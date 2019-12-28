78786823b8e3e2ae113c24029f63147f7baf5512
Otto Jung <otto.jung@kakao.com>
2019-12-28 13:13:12 +0100

    split common and pure

--- common.scm
+++ common.scm
@@ -1,25 +1,6 @@
 (define-module [my-guile-std common]
-  #:export
+  :export
   [
-   letin
-   defloop
-   apploop
-   reversed-args
-   fn
-   fn-list
-   with-return
-   monoids*
-   monoids-r*
-   monoid-r*
-   monoid*
-   letin-with-full
-   letin-parameterize
-   letin-with-full-parameterized
-   letin-with
-   dom
-   domid
-   domf
-   dom-default
    dom-print
    generate-prefixed-name
    gfunc/define
@@ -42,141 +23,39 @@
    mdict-keys
    with-bracket
    ]
+  :re-export
+  [
+   letin
+   defloop
+   apploop
+   reversed-args
+   fn
+   fn-list
+   with-return
+   monoids*
+   monoids-r*
+   monoid-r*
+   monoid*
+   letin-with-full
+   letin-parameterize
+   letin-with-full-parameterized
+   letin-with
+   dom
+   domid
+   domf
+   dom-default
+   ]
 
-  #:use-module [ice-9 format]
-  #:use-module [ice-9 textual-ports]
-  #:use-module [srfi srfi-1]
-  #:use-module [srfi srfi-13]
-  #:use-module [ice-9 hash-table]
-  #:use-module [srfi srfi-18]
-  #:use-module [srfi srfi-42]
-  #:use-module [srfi srfi-16]
+  :use-module [my-guile-std pure]
+  :use-module [ice-9 format]
+  :use-module [ice-9 textual-ports]
+  :use-module [srfi srfi-1]
+  :use-module [srfi srfi-13]
+  :use-module [ice-9 hash-table]
+  :use-module [srfi srfi-18]
+  :use-module [srfi srfi-42]
+  :use-module [srfi srfi-16]
   )
-
-(define-syntax letin
-  (syntax-rules ()
-    [(letin body) body]
-    [(letin ((a . as) b) body ...)
-     (let-values [[[a . as] b]] (letin body ...))]
-    [(letin (a b) body ...)
-     (let [[a b]] (letin body ...))]))
-
-(define-syntax defloop
-  (lambda (stx)
-    (syntax-case stx ()
-      [(defloop lambda-list . body)
-       (with-syntax ([name (datum->syntax #'body 'loop)])
-         #'(letrec ([name (lambda lambda-list . body)])
-             name))])))
-
-(define-syntax-rule [apploop args argv . body]
-  ((defloop args . body) . argv))
-
-(define-macro (reversed-args . body)
-  (reverse body))
-
-(define-syntax reversed-lambda
-  (syntax-rules ()
-    [(reversed-lambda body args) (lambda [] body)]
-    [(reversed-lambda body args next) (lambda (next . args) body)]
-    [(reversed-lambda body args x next ...) (reversed-lambda body (x . args) next ...)]))
-
-(define-syntax fn-start
-  (syntax-rules ()
-    [(fn-start args body) (reversed-lambda body () . args)]
-    [(fn-start args x body ...) (fn-start (x . args) body ...)]))
-
-(define-syntax-rule [fn . argv] (fn-start () . argv))
-
-(define-syntax fn-list-g
-  (syntax-rules ()
-    [(fn-list-g lst body) body]
-    [(fn-list-g lst x body ...)
-     (let [[x (car lst)]]
-       (fn-list-g
-        (cdr lst)
-        body
-        ...))]))
-
-(define-syntax-rule [fn-list . args]
-  (lambda [lst] (fn-list-g lst . args)))
-
-(define-syntax with-return
-  (lambda (stx)
-    (syntax-case stx ()
-      [(define-job . bodies)
-       (with-syntax [[return (datum->syntax #'bodies 'return)]]
-         #'(call/cc (lambda [return] (begin . bodies))))])))
-
-(define-syntax monoids*
-  (syntax-rules ()
-    [(monoids* x) x]
-    [(monoids* op x) (op x)]
-    [(monoids* a op b next-op ...)
-     (monoids* (op a b) next-op ...)]))
-
-(define-syntax monoids-r*
-  (syntax-rules ()
-    [(monoids-r* x) x]
-    [(monoids-r* op x) (op x)]
-    [(monoids-r* a op b next-op ...)
-     (op a (monoids-r* b next-op ...))]))
-
-(define-syntax monoid-r*
-  (syntax-rules ()
-    [(monoid-r* op a) a]
-    [(monoid-r* op a b ...)
-     (op a (monoid-r* op b ...))]))
-
-(define-syntax monoid*
-  (syntax-rules ()
-    [(monoid* op a) a]
-    [(monoid* op a b c ...)
-     (monoid* op (op a b) c ...)]))
-
-;; like do syntax in haskell
-(define-syntax letin-with-full
-  (syntax-rules ()
-    [(letin-with-full f body) body]
-    [(letin-with-full f (a b) body ...)
-     (f (quote a)
-        (quote b)
-        b
-        (lambda [a]
-          (letin-with-full f
-                           body
-                           ...)))]))
-
-(define letin-global-monad-parameter (make-parameter #f))
-(define-syntax-rule [letin-parameterize f . body]
-  (parameterize [[letin-global-monad-parameter f]]
-    (begin . body)))
-
-;; with parameterization
-(define-syntax-rule [letin-with-full-parameterized f . argv]
-  (let [[p (letin-global-monad-parameter)]]
-    (if p
-        (letin-with-full (p f (quote f)) . argv)
-        (letin-with-full f . argv))))
-
-(define-syntax-rule [letin-with f . argv]
-  (letin-with-full-parameterized (lambda [name result x cont] (f x cont)) . argv))
-
-(define-syntax-rule [letin-with-identity . argv]
-  (letin-with (fn x cont (cont x)) . argv))
-
-(define-syntax-rule [dom . argv] (letin-with . argv))
-
-(define-syntax-rule [domid . argv] (letin-with-identity . argv))
-
-(define-syntax-rule [domf . argv] (letin-with-full . argv))
-
-;; Short circuits with any predicate
-(define [dom-default default?]
-  (fn x cont
-      (if (default? x)
-          x
-          (cont x))))
 
 ;; Logs computations
 (define [dom-print name result x cont]
