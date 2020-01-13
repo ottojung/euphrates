7831e15f726e6680bb63b1aebc62bc5bf893a7b6
Otto Jung <otto.jung@kakao.com>
2020-01-13 15:55:43 +0100

    save@emacs

--- sharedtest/main.scm
+++ sharedtest/main.scm
@@ -22,10 +22,10 @@
 
 (display "Hello, Guile!\n")
 
-(println "tree:\n~a\n\n" (directory-tree "."))
+;; (println "tree:\n~a\n\n" (directory-tree "."))
 (println "files:\n~a\n\n" (directory-files "."))
-(println "files-rec:\n~a\n\n" (directory-files-rec "."))
-(println "rec only names:\n~a\n\n" (map cadr (directory-files-rec ".")))
+;; (println "files-rec:\n~a\n\n" (directory-files-rec "."))
+;; (println "rec only names:\n~a\n\n" (map cadr (directory-files-rec ".")))
 
 ;; scoping test
 (apploop [x] [20]
@@ -86,16 +86,6 @@
 ;; (format #t "hahahelo = ~a\n" hahahelo)
 
 (format #t "haha<int>(5) = ~a\n" (haha 5)) ;; TODO: fix
-
-(define zz (mdict 1 2 3 4))
-(printf "call(3) = ~a\n" (zz 3))
-(printf "set(3, 5) = ~a\n" (set! (zz) 5))
-(define z2 (mass zz 3 99))
-(printf "call(3) = ~a\n" (z2 3))
-(printf "set(3, 5) = ~a\n" (set! (z2) 5))
-(printf "has(3) = ~a\n" (mdict-has? z2 3))
-(printf "has(52) = ~a\n" (mdict-has? z2 52))
-(printf "keys(z2) = ~a\n" (mdict-keys z2))
 
 (println "~a" (list-fold 1 (range 1 5) *))
 (println "~a" (list-fold 1 (range 1 5) (lambda [acc x] (* acc x))))
