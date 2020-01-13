5eb45b0ca398e24e23e16ae9898d3d0822a49523
Otto Jung <otto.jung@kakao.com>
2020-01-13 14:17:01 +0100

    move other things

--- lib.scm
+++ lib.scm
@@ -283,61 +283,6 @@
   "
   (with-bracket-lf expr finally))
 
-;;;;;;;;;;;;;;;;;;;;
-;; HASHED RECORDS ;;
-;;;;;;;;;;;;;;;;;;;;
-
-(define [alist->hash-table lst]
-  (let [[h (make-hash-table)]]
-    (for-each
-     (lambda (pair)
-       (hash-set! h (car pair) (cdr pair)))
-     lst)
-    h))
-
-(define [hash->mdict h]
-  (letin
-   [unique (make-unique)]
-   (make-procedure-with-setter
-    (lambda [key]
-      (let [[g (hash-ref h key unique)]]
-        (if (unique g)
-            (throw 'mdict-key-not-found key h)
-            g)))
-    (lambda [new] h))))
-
-(define [alist->mdict alist]
-  (hash->mdict (alist->hash-table alist)))
-
-(define-syntax mdict-c
-  (syntax-rules ()
-    [(mdict-c carry) (alist->mdict carry)]
-    [(mdict-c carry key value . rest)
-     (mdict-c (cons (cons key value) carry) . rest)]))
-
-(define-syntax-rule [mdict . entries]
-  (mdict-c '() . entries))
-
-(define [mdict-has? h-func key]
-  (letin
-   [h (set! (h-func) 0)]
-   (hash-get-handle h key)))
-
-(define [mdict->alist h-func]
-  (letin
-   [h (set! (h-func) 0)]
-   (hash-map->list cons h)))
-
-(define [mass *mdict key value]
-  (letin
-   [new (alist->hash-table (mdict->alist *mdict))]
-   [new-f (hash->mdict new)]
-   (do (hash-set! new key value))
-   new-f))
-
-(define [mdict-keys h-func]
-  (map car (mdict->alist h-func)))
-
 ;;;;;;;;;;;;;;;;
 ;; SHORTHANDS ;;
 ;;;;;;;;;;;;;;;;
@@ -401,64 +346,6 @@
           (begin
             (put-u8 to byte)
             (lp (1+ count)))))))
-
-;;;;;;;;;;;;;;;;;;;;;;;
-;; GENERIC FUNCTIONS ;;
-;;;;;;;;;;;;;;;;;;;;;;;
-
-(define [check-list-contract check-list args]
-  (or (not check-list)
-      (and (= (length check-list) (length args))
-           (fold (lambda [p x c] (and c (p x))) #t check-list args))))
-
-(define-syntax-rule [generate-add-name name]
-  (generate-prefixed-name 'gfunc/instantiate- name))
-
-(define-syntax-rule [generate-param-name name]
-  (generate-prefixed-name 'gfunc/parameterize- name))
-
-(define-syntax gfunc/define
-  (lambda (stx)
-    (syntax-case stx ()
-      [[gfunc/define name]
-       (with-syntax ([add-name (generate-add-name name)]
-                     [param-name (generate-param-name name)])
-         #'(define-values [name add-name param-name]
-             (let [[internal-list (make-parameter '())]
-                   [sem (my-make-mutex)]]
-               (values
-                (lambda args
-                  (let [[m (find (lambda [p] (check-list-contract (car p) args)) (internal-list))]]
-                    (if m
-                        (apply (cdr m) args)
-                        (throw 'gfunc-no-instance-found
-                               (string-append "No gfunc instance of "
-                                              (symbol->string (syntax->datum #'name))
-                                              " accepts required arguments")))))
-                (lambda [args func]
-                  (call-with-blocked-asyncs
-                   (lambda []
-                     (my-mutex-lock! sem)
-                     (set! internal-list (make-parameter (append (internal-list) (list (cons args func)))))
-                     (my-mutex-unlock! sem))))
-                (lambda [args func body]
-                  (let [[new-list (cons (cons args func) (internal-list))]]
-                    (parameterize [[internal-list new-list]]
-                      (body))))))))])))
-
-(define-syntax gfunc/parameterize
-  (lambda (stx)
-    (syntax-case stx ()
-      [[gfunc/parameterize name check-list func . body]
-       (with-syntax [[param-name (generate-param-name name)]]
-         #'(param-name check-list func (lambda [] . body)))])))
-
-(define-syntax gfunc/instance
-  (lambda (stx)
-    (syntax-case stx ()
-      [[gfunc/instance name check-list func]
-       (with-syntax [[add-name (generate-add-name name)]]
-         #'(add-name (list . check-list) func))])))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; NON PREEMPTIVE THREADS ;;
