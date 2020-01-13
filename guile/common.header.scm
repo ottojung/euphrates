754665ffd31985263f5b83b80bcd47505bd82175
Otto Jung <otto.jung@kakao.com>
2020-01-13 13:50:45 +0100

    save

--- guile/common.header.scm
+++ guile/common.header.scm
@@ -90,17 +90,17 @@
    i-thread-critical-points
    i-thread-critical-points-print
 
-   process?
-   command:process
-   args:process
-   mode:process
-   pipe:process
-   pid:process
-   status:process
-   exited?:process
-   run-process
-   run-process-with-output-to
-   kill-process*
+   comprocess?
+   comprocess-command
+   comprocess-args
+   comprocess-mode
+   comprocess-pipe
+   comprocess-pid
+   comprocess-status
+   comprocess-exited?
+   run-comprocess
+   run-comprocess-with-output-to
+   kill-comprocess*
 
    define-rec
    ]
@@ -139,6 +139,7 @@
       (time-to-nanoseconds
        ((@ (srfi srfi-19) current-time) time-monotonic)))))
 
+(define call-with-new-sys-thread call-with-new-thread)
 
 ;;;;;;;;;;;;;;;;
 ;; FILESYSTEM ;;
@@ -293,3 +294,188 @@
    ()
    . fields))
 
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; PREEMPTIVE THREADS ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Enable asynchronous auto-yield
+;; making non-preemptive threads to preemptive ones (or `i-thread's - "interuptible threads")
+;;
+;; NOTE:
+;; * `my-mutex-lock!' is not interruptible
+;;   Use `i-thread-critical!' to ensure that no interrupt will happen before `my-mutex-unlock!'
+;;   Or use `np-thread-lockr' and `np-thread-unlockr' instead
+;; * `sleep' (`usleep') is not interruptible
+;;   use `np-thread-usleep' instead
+
+;; TODO: [variable interrupt frequency] use this somehow
+(define global-interrupt-frequency-p (make-parameter 1000000))
+
+(define-values
+  [i-thread-yield
+   i-thread-dont-yield]
+  (let [[interruptor-thread #f]
+        [lst (list)]
+        [interruptor-finished? #t]]
+
+    (define [interruptor-loop]
+      (if (null? lst)
+          (set! interruptor-finished? #t)
+          (begin
+            (map
+             (lambda [th]
+               (system-async-mark
+                np-thread-yield
+                th))
+             lst)
+            (usleep 900000) ;; TODO: [variable interrupt frequency]
+            (interruptor-loop))))
+
+    (values
+     (lambda [thread]
+
+       (when interruptor-finished?
+         (set! interruptor-thread
+           (call-with-new-thread interruptor-loop)))
+
+       (system-async-mark
+        (lambda []
+          (unless (member thread lst)
+            (set! lst (cons thread lst))))
+        interruptor-thread))
+
+     (lambda [thread]
+       (unless interruptor-finished?
+         (system-async-mark
+          (lambda []
+            (set! lst
+              (filter (lambda [th] (not (equal? th thread)))
+                      lst)))
+          interruptor-thread))))))
+
+(define [i-thread-yield-me]
+  (i-thread-yield ((@ [ice-9 threads] current-thread))))
+
+(define [i-thread-dont-yield-me]
+  (i-thread-dont-yield ((@ [ice-9 threads] current-thread))))
+
+(define-syntax-rule [i-thread-run! . thunk]
+  (np-thread-run!
+   (dynamic-wind
+     i-thread-yield-me
+     (lambda [] (begin . thunk))
+     i-thread-dont-yield-me)))
+
+;; For debug purposes
+(define-values
+  [i-thread-critical-points
+   i-thread-critical-points-append!
+   i-thread-critical-points-remove!
+   i-thread-critical-points-print]
+  (let [[lst (list)]
+        [mut (my-make-mutex)]]
+    (values
+     (lambda [] lst)
+     (lambda [st]
+       (my-mutex-lock! mut)
+       (set! lst (cons st lst))
+       (my-mutex-unlock! mut))
+     (lambda [st]
+       (my-mutex-lock! mut)
+       (set! lst
+         (filter (lambda [el] (not (equal? el st)))
+                 lst))
+       (my-mutex-unlock! mut))
+     (lambda []
+       (format #t "--- CRITICAL POINTS ---\n")
+       (for-each
+        (lambda [st]
+          (display-backtrace st (current-output-port)))
+        lst)
+       (format #t "--- END CRITICAL POINTS ---\n")))))
+
+(define-syntax-rule [i-thread-critical! . thunk]
+  "
+  Will not interrupt during execution of `thunk'
+  Unsafe: must finish quick!
+  "
+  (call-with-blocked-asyncs
+   (lambda []
+     (let [[st (make-stack #t)]]
+       (i-thread-critical-points-append! st)
+       (begin . thunk)
+       (i-thread-critical-points-remove! st)))))
+
+(define [i-thread-critical-b! thunk finally]
+  "
+  Same as `i-thread-critical' but also puts `thunk' and `finally' to `with-bracket' clause
+  "
+  (i-thread-critical! (with-bracket thunk finally)))
+
+;;;;;;;;;;;;;;;
+;; PROCESSES ;;
+;;;;;;;;;;;;;;;
+
+(define-rec comprocess
+  command
+  args
+  mode
+  pipe
+  pid
+  status
+  exited?
+  )
+
+(define [run-comprocess#private mode command . args]
+  "Run process in background
+   Input and Output ports are represented by `comprocess-pipe'
+   They are new, not related to `(current-input-port)' or `(current-ouput-porn)'
+
+   type ::= mode -> string -> list of string -> process
+   mode ::= OPEN_READ | OPEN_WRITE | OPEN_BOTH
+  "
+  (let [[p
+         (comprocess
+          command
+          args
+          mode
+          #f
+          #f
+          #f
+          #f)]]
+    (call-with-new-thread
+     (lambda []
+       (let* [[pipe (apply open-pipe*
+                           (cons* (comprocess-mode p)
+                                  (comprocess-command p)
+                                  (comprocess-args p)))]
+              [pid (hashq-ref port/pid-table pipe)]]
+         (set-comprocess-pipe p pipe)
+         (set-comprocess-pid p pid)
+         (set-comprocess-status p (cdr (waitpid pid))) ;; NOTE: unsafe because process could have ended by now, but probability is too small because processes take long time to start
+         (set-comprocess-exited? p #t))))
+
+    ;; wait for pipe to initialize
+    (let lp []
+      (unless (comprocess-pid p)
+        (usleep 100)
+        (lp)))
+    p))
+
+(define [run-comprocess command . args]
+  (apply run-comprocess#private (cons* OPEN_READ command args)))
+
+(define [run-comprocess-with-output-to output command . args]
+  "Run process in background
+   Input and Output ports are represented by `comprocess-pipe'
+   Output port will be redirected to `output', input is unchanged
+
+   type ::= port -> string -> list of string -> process
+  "
+  (let [[p (apply run-comprocess (cons* command args))]]
+    (call-with-new-thread
+     (lambda []
+       (port-redirect
+        (comprocess-pipe p)
+        output)))
+    p))
