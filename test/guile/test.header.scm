5ba1b7c09b587e21a712b6d82fe98e804efc55c9
Otto Jung <otto.jung@kakao.com>
2020-06-22 20:58:08 +0200

    guile: fix test header

--- test/guile/test.header.scm
+++ test/guile/test.header.scm
@@ -1,11 +1,7 @@
 
 (add-to-load-path "build/src/guile")
 
-(use-modules (euphrates common))
-
-(use-modules [ice-9 threads])
-(use-modules [ice-9 textual-ports])
-(use-modules [[srfi srfi-18]
-              #:select [make-mutex mutex-lock! mutex-unlock!]
-              #:prefix srfi::])
+(define-module (euphrates test)
+  #:use-module [euphrates common]
+  )
 
