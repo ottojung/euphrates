e5dc5bc062fc32c204a15a5945945cbdbf6694d0
Otto Jung <otto.jung@kakao.com>
2020-12-22 16:41:10 +0100

    improve atomic-box

--- src/atomic-box.scm
+++ src/atomic-box.scm
@@ -1,29 +1,35 @@
 
 %run guile
 
+%var make-atomic-box
+%var atomic-box?
+%var atomic-box-ref
+%var atomic-box-set!
+%var atomic-box-compare-and-set!
+
 %for (COMPILER "guile")
 
 (use-modules (ice-9 atomic))
 
-%var make-atomic-box
 (define make-atomic-box (@ (ice-9 atomic) make-atomic-box))
-
-%var atomic-box?
 (define atomic-box? (@ (ice-9 atomic) atomic-box?))
-
-%var atomic-box-ref
 (define atomic-box-ref (@ (ice-9 atomic) atomic-box-ref))
-
-%var atomic-box-set!
 (define atomic-box-set! (@ (ice-9 atomic) atomic-box-set!))
 
-;; for racket compatibility
-%var atomic-box-compare-and-set!
 (define (atomic-box-compare-and-set! box expected desired)
   (let ((ret (atomic-box-compare-and-swap! box expected desired)))
     (eq? ret expected)))
 
 %end
 
+%for (COMPILER "racket")
+
+(define make-atomic-box box)
+(define atomic-box? box?)
+(define atomic-box-ref unbox)
+(define atomic-box-set! set-box!)
+(define atomic-box-compare-and-set! box-cas!)
+
+%end
 
 
