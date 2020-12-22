73615405ee1dcec9f82b4f701c517f78313347c5
Otto Jung <otto.jung@kakao.com>
2020-12-22 01:05:28 +0100

    begin testing

--- test/main.scm
+++ test/main.scm
@@ -5,10 +5,23 @@
 %use (make-uni-spinlock) "./../src/uni-spinlock.scm"
 %use (debug) "./../src/debug.scm"
 %use (with-ignore-errors!) "./../src/with-ignore-errors.scm"
+%use (random-choice) "./../src/random-choice.scm"
+%use (printable#alphabet) "./../src/printable-alphabet.scm"
+%use (catch-any) "./../src/catch-any.scm"
+%use (assert) "./../src/assert.scm"
 
-(display "All good\n")
-(debug "All is ~s" 'debugy)
+(let ()
+  (catch-any
+   (lambda _ (assert (= (+ 3 2) (- 10 3))))
+   (lambda errors
+     (assert
+      (equal? errors
+              '((assertion-fail (test: (= 5 7)) (original: (= (+ 3 2) (- 10 3))))))))))
 
-(with-ignore-errors!
- (throw 'hello!))
+(let ()
+  (assert (equal? 5 (string-length (list->string (random-choice 5 printable#alphabet))))))
+
+
+
+
 
