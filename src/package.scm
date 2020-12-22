d82c5362d38c85d0736a9924361c2a19fa5a80bd
Otto Jung <otto.jung@kakao.com>
2020-12-22 22:06:17 +0100

    add package

---
+++ src/package.scm
@@ -0,0 +1,87 @@
+
+%run guile
+
+%use (make-hashmap hashmap-ref hashmap-set!) "./hash-table.scm"
+
+%var use-svars
+%var with-svars
+%var with-package
+%var make-package
+%var make-static-package
+
+;; This is for ad hoc polymorphism
+;; Faster than gfunc, but also more limited
+;; Similar to SML parametric modules
+
+(define-syntax with-svars-helper
+  (syntax-rules []
+    [[_ dict buf [] body]
+     (lambda dict
+       (let buf body))]
+    [[_ dict buf [name . names] body]
+     (with-svars-helper
+      dict
+      ((name
+        (let [[z (assq (quote name) dict)]]
+          (if z (cdr z) name))) . buf)
+      names
+      body)]))
+
+(define-syntax-rule [with-svars names body]
+  (with-svars-helper dd () names body))
+
+(define-syntax use-svars-helper
+  (syntax-rules []
+    [[_ [] [] f] (f)]
+    [[_ buf [] f] (f . buf)]
+    [[_ buf [[name value] . names] f]
+     (use-svars-helper
+      ((cons (quote name) value) . buf)
+      names
+      f)]))
+
+(define-syntax-rule [use-svars f . renames]
+  (use-svars-helper [] renames f))
+
+(define-syntax make-static-package-helper
+  (syntax-rules []
+    [[_ hh buf []]
+     (let [[hh (make-hashmap)]]
+       (begin . buf)
+       hh)]
+    [[_ hh buf [[name value] . definitions]]
+     (make-static-package-helper
+      hh
+      [(hashmap-set! hh
+                     (quote name)
+                     value) . buf]
+      definitions)]))
+
+(define-syntax-rule [make-static-package definitions]
+  (make-static-package-helper hh [] definitions))
+
+(define-syntax-rule [make-package inputs definitions]
+  (with-svars
+   inputs
+   (make-static-package definitions)))
+
+(define-syntax with-package-helper
+  (syntax-rules []
+    [[_ inst [] body] body]
+    [[_ inst [name . names] body]
+     (let [[name (hashmap-ref inst (quote name))]]
+       (with-package-helper
+        inst
+        names
+        body))]))
+
+(define-syntax with-package-renames-helper-pre
+  (syntax-rules []
+    [[_ [package . renames]]
+     (use-svars package . renames)]
+    [[_ package]
+     (package)]))
+
+(define-syntax-rule [with-package package-spec names body]
+  (let [[inst (with-package-renames-helper-pre package-spec)]]
+    (with-package-helper inst names body)))
--- test/main.scm
+++ test/main.scm
@@ -21,6 +21,7 @@
 %use (list->tree) "./src/list-to-tree.scm"
 %use (list-traverse) "./src/list-traverse.scm"
 %use (range) "./src/range.scm"
+%use (use-svars with-svars with-package make-package make-static-package) "./src/package.scm"
 
 (let ()
   (catch-any
@@ -170,6 +171,60 @@
       (if (< 5 x)
           (values #f x)
           (values #t (list)))))))
+
+;; package
+(let ()
+  (define already-defined 7)
+
+  (define x
+    (with-svars [already-defined]
+                (lambda (c) (list already-defined c))))
+
+  (define y
+    ((use-svars x (b 2) (already-defined 1)) 4))
+
+  (define z
+    ((x) 4))
+
+  (define p
+    (make-package [already-defined]
+                  [[foo (lambda (c) (list already-defined c))]
+                   [bar (lambda (c) (list c already-defined))]
+                   [baz 3]]))
+
+  (define p-inst (p (cons 'already-defined 22)))
+  (define foo-inst (hash-ref p-inst 'foo))
+
+  (assert= y (list 1 4))
+  (assert= z (list 7 4))
+
+  (assert=
+   (list 22 2)
+   (foo-inst 2))
+
+  (assert=
+   (list 3 2)
+   (with-package
+    [p [b 2] [already-defined 3]]
+    [foo bar baz]
+    (begin
+      (foo 2))))
+
+  (assert=
+   (list 7 2)
+   (with-package
+    p
+    [foo bar baz]
+    (begin
+      (foo 2))))
+
+  (assert=
+   (list 7 2)
+   (with-package
+    [p]
+    [foo bar baz]
+    (begin
+      (foo 2)))))
 
 (display "All good\n")
 
