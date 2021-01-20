d3eec1b4befdcd1aea57d37939103b20fd411bd5
Otto Jung <otto.jung@kakao.com>
2021-01-20 18:14:09 +0100

    add read-list

---
+++ src/read-list.scm
@@ -0,0 +1,15 @@
+
+%run guile
+
+%var read-list
+
+(define read-list
+  (case-lambda
+   (() (read-list (current-input-port)))
+   ((p)
+    (let lp ()
+      (let ((r (read p)))
+        (if (eof-object? r)
+            '()
+            (cons r (lp))))))))
+
