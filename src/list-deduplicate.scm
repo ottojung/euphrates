340479bdc58343f345fb9ecd979d28d96603337f
Otto Jung <otto.jung@kakao.com>
2021-01-22 18:22:53 +0100

    list-deduplicate: fix multithreaded use-case

--- src/list-deduplicate.scm
+++ src/list-deduplicate.scm
@@ -8,16 +8,14 @@
 
 ;; returns list in reverse order
 (define list-deduplicate
-  (let ((H/p (make-parameter (hashmap)))) ;; thread-local
-    (case-lambda
-     ((lst) (list-deduplicate lst equal?))
-     ((lst pred)
-      (let ((H (H/p)))
-        (hashmap-clear! H)
-        (let lp ((buf lst) (mem (list)))
-          (cond ((null? buf) mem)
-                ((hashmap-ref H (car buf) #f)
-                 (lp (cdr buf) mem))
-                (else
-                 (hashmap-set! H (car buf) #t)
-                 (lp (cdr buf) (cons (car buf) mem))))))))))
+  (case-lambda
+   ((lst) (list-deduplicate lst equal?))
+   ((lst pred)
+    (let ((H (hashmap)))
+      (let lp ((buf lst) (mem (list)))
+        (cond ((null? buf) mem)
+              ((hashmap-ref H (car buf) #f)
+               (lp (cdr buf) mem))
+              (else
+               (hashmap-set! H (car buf) #t)
+               (lp (cdr buf) (cons (car buf) mem)))))))))
