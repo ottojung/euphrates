251bcd1d388284a67788f9ed85175250833180d1
Otto Jung <otto.jung@kakao.com>
2021-01-22 17:25:19 +0100

    add string-null-or-whitespace

---
+++ src/string-null-or-whitespace-p.scm
@@ -0,0 +1,11 @@
+
+%run guile
+
+%var string-null-or-whitespace?
+
+(define (string-null-or-whitespace? str)
+  (let loop ((i (- (string-length str) 1)))
+    (if (< i 0) #t
+        (case (string-ref str i) ;; TODO: is this O(1)?
+          ((#\space #\tab #\newline) (loop (- i 1)))
+          (else #f)))))
