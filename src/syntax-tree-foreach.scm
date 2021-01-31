d2f2fae0a42eb8ccd8bae2629de642d08b142323
Otto Jung <otto.jung@kakao.com>
2021-01-31 13:19:32 +0100

    add syntax-tree-foreach

---
+++ src/syntax-tree-foreach.scm
@@ -0,0 +1,21 @@
+
+%run guile
+
+%var syntax-tree-foreach
+
+(define-syntax syntax-tree-foreach
+  (syntax-rules ()
+    [(_ f ((xs ...) ys ...))
+     (begin (syntax-tree-foreach f (xs ...))
+            (syntax-tree-foreach f (ys ...)))]
+    [(_ f (x xs ...))
+     (begin (f x)
+            (syntax-tree-foreach f (xs ...)))]
+    [(_ f ())
+     (begin)]))
+
+;; Example:
+;; (define-syntax-rule (defn m)
+;;   (define m #f))
+;; (syntax-tree-foreach defn (a (b (c d)) e)) ;; declares all identifiers
+
