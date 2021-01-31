d7825111eee9e712968b7152b30fab1fa121679b
Otto Jung <otto.jung@kakao.com>
2021-01-31 12:24:41 +0100

    get-command-line-arguments: fix brackets

--- src/get-command-line-arguments.scm
+++ src/get-command-line-arguments.scm
@@ -6,20 +6,21 @@
 %var get-command-line-arguments
 
 (define get-command-line-arguments
-  (let default
-      (lambda _
+  (let ((default
+          (lambda _
 
 %for (COMPILER "guile")
-        (let ((ret (command-line)))
-          (if (< (length ret) 2)
-              '()
-              (cdr ret))))
+            (let ((ret (command-line)))
+              (if (< (length ret) 2)
+                  '()
+                  (cdr ret))))
 %end
 %for (COMPILER "racket")
-        (vector->list
-         (current-command-line-arguments)))
+                  (vector->list
+                   (current-command-line-arguments)))
 %end
+          ))
 
-   (lambda _
-     (or (command-line-argumets/p)
-         (default))))
+    (lambda _
+      (or (command-line-argumets/p)
+          (default)))))
