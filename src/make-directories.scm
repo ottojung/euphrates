bf4c845aeb500031f0f0f05280e318b18bc25b87
Otto Jung <otto.jung@kakao.com>
2021-02-02 13:20:12 +0100

    fix make-directories.scm

--- src/make-directories.scm
+++ src/make-directories.scm
@@ -5,32 +5,25 @@
 %use (list-fold) "./list-fold.scm"
 %use (file-or-directory-exists?) "./file-or-directory-exists-q.scm"
 %use (append-posix-path) "./append-posix-path.scm"
+%use (string-null-or-whitespace?) "./string-null-or-whitespace-p.scm"
 
-%var make-directory
+%var make-directories
+
+(define (make-directories path)
 
 %for (COMPILER "guile")
-
-(define (make-directories path)
-  (define parts (string-split/simple path #\/))
-  (list-fold
-   (acc #f)
-   (i parts)
-   (let* ((path (if acc (append-posix-path acc i) i)))
-     (unless (file-or-directory-exists? path)
-       (mkdir path))
-     path)))
-
+  (define mk-single-dir mkdir)
 %end
 %for (COMPILER "racket")
+  (define mk-single-dir make-directory)
+%end
 
-(define (make-directories path)
   (define parts (string-split/simple path #\/))
   (list-fold
    (acc #f)
    (i parts)
-   (let* ((path (if acc (append-posix-path acc i) i)))
+   (let* ((path0 (if acc (append-posix-path acc i) i))
+          (path (if (string-null? path0) "/" path0)))
      (unless (file-or-directory-exists? path)
-       (make-directory path))
+       (mk-single-dir path))
      path)))
-
-%end
