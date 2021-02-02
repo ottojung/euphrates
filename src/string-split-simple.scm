5d5694797ad5cef41e4342737eb3c31dcecf1290
Otto Jung <otto.jung@kakao.com>
2021-02-02 13:15:35 +0100

    rename string-split-simple

--- src/string-split-simple.scm
+++ src/string-split-simple.scm
@@ -1,17 +1,16 @@
 
 %run guile
 
-%var string-split#simple
+%var string-split/simple
 
 %for (COMPILER "guile")
 
-(define string-split#simple string-split)
+(define string-split/simple string-split)
 
 %end
-
 %for (COMPILER "racket")
 
-(define [string-split#simple str delim]
+(define [string-split/simple str delim]
   (if (char? delim)
       (string-split str (string delim) #:trim? #f)
       (string-split str delim #:trim? #f)))
