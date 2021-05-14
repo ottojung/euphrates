e1be42fe3a49dd4e89958e3a995cd1a93489e5a9
Otto Jung <otto.jung@kakao.com>
2021-05-14 17:43:34 +0200

    fix sleep-until.scm

--- src/sleep-until.scm
+++ src/sleep-until.scm
@@ -3,6 +3,8 @@
 
 %var sleep-until
 
+%use (dynamic-thread-get-delay-procedure) "./dynamic-thread-get-delay-procedure.scm"
+
 (define-syntax-rule [sleep-until condi . body]
   (let ((sleep (dynamic-thread-get-delay-procedure)))
     (do ()
