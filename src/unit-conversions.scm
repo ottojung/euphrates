798a930c88f1b2a65067dd720549302a11d5c067
Otto Jung <otto.jung@kakao.com>
2021-05-14 23:20:47 +0200

    extend unit-conversions

--- src/unit-conversions.scm
+++ src/unit-conversions.scm
@@ -1,27 +1,61 @@
 
 %run guile
 
+%var normal->milli/unit
 %var normal->micro/unit
-%var micro->nano/unit
 %var normal->nano/unit
-%var nano->micro/unit
+
+%var milli->normal/unit
+%var milli->micro/unit
+%var milli->nano/unit
+
 %var micro->normal/unit
+%var micro->milli/unit
+%var micro->nano/unit
+
 %var nano->normal/unit
+%var nano->milli/unit
+%var nano->micro/unit
+
+(define (normal->milli/unit s)
+  (* 1000 s))
 
 (define (normal->micro/unit s)
   (* 1000000 s))
 
-(define (micro->nano/unit ms)
-  (* 1000 ms))
-
 (define (normal->nano/unit s)
-  (micro->nano/unit (normal->micro/unit s)))
+  (* 1000000000 s))
 
-(define (nano->micro/unit ns)
-  (/ ns 1000))
+
+
+
+(define (milli->normal/unit u)
+  (/ u 1000))
+
+(define (milli->micro/unit u)
+  (* u 1000))
+
+(define (milli->nano/unit u)
+  (* 1000000 u))
+
+
 
 (define (micro->normal/unit u)
   (/ u 1000000))
 
-(define (nano->normal/unit n)
-  (/ n 1000000000))
+(define (micro->milli/unit u)
+  (/ u 1000))
+
+(define (micro->nano/unit u)
+  (* 1000 u))
+
+
+
+(define (nano->normal/unit ns)
+  (/ ns 1000000000))
+
+(define (nano->milli/unit ns)
+  (/ ns 1000000))
+
+(define (nano->micro/unit ns)
+  (/ ns 1000))
