ba3c915fa23167ae4dab9cc65d1eba6914babd58
Otto Jung <otto.jung@kakao.com>
2021-05-14 21:18:19 +0200

    directory-mtime-state: ignore errors

--- src/directory-mtime-state.scm
+++ src/directory-mtime-state.scm
@@ -6,12 +6,16 @@
 %use (directory-files-rec) "./directory-files-rec.scm"
 %use (file-mtime) "./file-mtime.scm"
 %use (list->hashset) "./ihashset.scm"
+%use (with-ignore-errors!) "./with-ignore-errors.scm"
 
 ;; returns set of all files from the directory and their modification times
 
 (define (directory-mtime-state dir)
   (define files (map car (directory-files-rec dir)))
 
-  (define with-mtimes (map cons files (map file-mtime files)))
+  (define (safe-mtime file)
+    (with-ignore-errors!
+     (file-mtime file)))
+  (define with-mtimes (map cons files (map safe-mtime files)))
 
   (list->hashset with-mtimes))
