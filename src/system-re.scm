f5dd642a958f7d9b00b9fa6518451f4639ca1c69
Otto Jung <otto.jung@kakao.com>
2021-08-06 23:40:26 +0200

    quote all arguments of system-re

--- src/system-re.scm
+++ src/system-re.scm
@@ -9,11 +9,17 @@
 %use (file-delete) "./file-delete.scm"
 %use (shell-quote) "./shell-quote.scm"
 
+;; Escapes all args, so variable substitution won't work!
+;; i.e. (system-re "cat ~a" "$HOME/.bashrc") => will invoke `cat '$HOME/.bashrc'` and will not work :(
+;; correct usage:
+;;      (system-re "cat \"$HOME\"/~a" ".bashrc") => will invoke `cat \"$HOME\"'/.bashrc'` :)
+;; or:
+;;      (system-re "cat ~a" (string-append (getenv "HOME") "/.bashrc") => will invoke `cat '/home/user/.bashrc'` :)
 %var system-re
 
 (define (system-re fmt . args)
   "Like `system', but returns (output, exit status)"
-  (let* ((command (apply stringf (cons fmt args)))
+  (let* ((command (apply stringf (cons fmt (map shell-quote args))))
          (temp (make-temporary-filename))
          (p (system*/exit-code
              "/bin/sh" "-c"
