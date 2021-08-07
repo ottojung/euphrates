7a05f07738db723d73a732e8403acc1adedf5ad5
Otto Jung <otto.jung@kakao.com>
2021-08-07 13:59:34 +0200

    unify alphabets

--- src/alpha-alphabet.scm
+++ src/alpha-alphabet.scm
@@ -3,11 +3,13 @@
 
 %var alpha/alphabet
 
+;; starts off like base64 table
 (define alpha/alphabet
-  #(#\a #\b #\c #\d #\e #\f #\g #\h #\i #\j
-    #\k #\l #\m #\n #\o #\p #\q #\r #\s #\t
-    #\u #\v #\w #\x #\y #\z #\A #\B #\C #\D
-    #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N
-    #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X
-    #\Y #\Z))
+  #(#\A #\B #\C #\D #\E #\F #\G #\H
+    #\I #\J #\K #\L #\M #\N #\O #\P
+    #\Q #\R #\S #\T #\U #\V #\W #\X
+    #\Y #\Z #\a #\b #\c #\d #\e #\f
+    #\g #\h #\i #\j #\k #\l #\m #\n
+    #\o #\p #\q #\r #\s #\t #\u #\v
+    #\w #\x #\y #\z))
 
