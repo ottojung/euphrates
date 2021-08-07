bc88c8db41cef82e52b1395c0271054e420ef44c
Otto Jung <otto.jung@kakao.com>
2021-08-07 13:47:58 +0200

    fix base64 situation

--- src/base64-alphabet-minusunderscore.scm
+++ src/base64-alphabet-minusunderscore.scm
@@ -1,10 +1,10 @@
 
 %run guile
 
-%var base64/alphabet
+%var base64/alphabet/minusunderscore
 
-;; Specified by RFC 1421, 2045, 2152, 4648 $4, 4880
-(define base64/alphabet
+;; Specified by RFC 4648
+(define base64/alphabet/minusunderscore
   #(#\A #\B #\C #\D #\E #\F #\G #\H
     #\I #\J #\K #\L #\M #\N #\O #\P
     #\Q #\R #\S #\T #\U #\V #\W #\X
@@ -12,4 +12,4 @@
     #\g #\h #\i #\j #\k #\l #\m #\n
     #\o #\p #\q #\r #\s #\t #\u #\v
     #\w #\x #\y #\z #\0 #\1 #\2 #\3
-    #\4 #\5 #\6 #\7 #\8 #\9 #\+ #\/))
+    #\4 #\5 #\6 #\7 #\8 #\9 #\- #\_))
