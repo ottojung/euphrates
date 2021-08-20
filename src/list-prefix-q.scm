fe84887c1b5955ade648812ed81f11628ac5bf99
Otto Jung <otto.jung@kakao.com>
2021-08-21 00:00:26 +0200

    add list-prefix?

---
+++ src/list-prefix-q.scm
@@ -0,0 +1,27 @@
+;;;; Copyright (C) 2021  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+%var list-prefix?
+
+(define (list-prefix? prefix-lst target-lst)
+  (let loop ((prefix-lst prefix-lst)
+             (target-lst target-lst))
+    (cond
+     ((null? prefix-lst) #t)
+     ((null? target-lst) #f)
+     (else
+      (and (equal? (car prefix-lst) (car target-lst))
+           (loop (cdr prefix-lst) (cdr target-lst)))))))
