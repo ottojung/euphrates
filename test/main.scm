8e50ce1148fa0d8dcb408a50a614b52bfa0be53f
Otto Jung <otto.jung@kakao.com>
2021-08-20 19:42:10 +0200

    cfg-machine: add more tests

--- test/main.scm
+++ test/main.scm
@@ -579,6 +579,36 @@
 
     (assert=HS
      '((k . 2) (x . 1) (z . 1) (m . 2) (y . 7) (i 8 9 3))
+     (immutable-hashmap->alist H)))
+
+  (let ()
+    (define m (make-cfg-machine
+               '((main (and (any x z)
+                            (or (= 3) (= 2 m k))
+                            (call save-i)
+                            (any y)))
+                 (save-i (or (and (any* i) (call save-i))
+                             (epsilon))))))
+    (define-values (H sucess?) (m (list 1 2 3 9 8 7)))
+    (assert sucess?)
+
+    (assert=HS
+     '((k . 2) (x . 1) (z . 1) (m . 2) (y . 7) (i 8 9 3))
+     (immutable-hashmap->alist H)))
+
+  (let ()
+    (define m (make-cfg-machine
+               '((main (and (any x z)
+                            (or (= 3) (= 2 m k))
+                            (call save-i)
+                            (any y)))
+                 (save-i (or (and (any* i) (call save-i))
+                             (epsilon))))))
+    (define-values (H sucess?) (m (list 1 2 7)))
+    (assert sucess?)
+
+    (assert=HS
+     '((k . 2) (x . 1) (z . 1) (m . 2) (y . 7))
      (immutable-hashmap->alist H))))
 
 ;; parse-cli
