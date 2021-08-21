7c9aaefbd382712018eabb7c086f68066b539e35
Otto Jung <otto.jung@kakao.com>
2021-08-21 20:31:56 +0200

    add system-environment.scm

---
+++ src/system-environment.scm
@@ -0,0 +1,42 @@
+;;;; Copyright (C) 2021  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+%var system-environment-get
+%var system-environment-set!
+
+%for (COMPILER "guile")
+
+(define (system-environment-get s)
+  (getenv s))
+
+(define (system-environment-set! s v)
+  (setenv s v))
+
+%end
+%for (COMPILER "racket")
+
+(define (system-environment-get s)
+  (getenv s))
+
+(define (system-environment-set! s v)
+  (if v
+      (putenv s v) ;; Guile's procedure is called `setenv', except that it accepts #f
+      (environment-variables-set!
+       (current-environment-variables)
+       (string->bytes/locale s (char->integer #\?))
+       #f)))
+
+%end
