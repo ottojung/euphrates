fc6e08899211bb3ecb06e05ac9b7547f762ef481
Otto Jung <otto.jung@kakao.com>
2021-08-21 14:07:11 +0200

    add system-fmt

---
+++ src/system-fmt.scm
@@ -0,0 +1,33 @@
+;;;; Copyright (C) 2021  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+;; Escapes all args, so variable substitution won't work!
+;; i.e. (system-re "cat ~a" "$HOME/.bashrc") => will invoke `cat '$HOME/.bashrc'` and will not work :(
+;; correct usage:
+;;      (system-re "cat \"$HOME\"/~a" ".bashrc") => will invoke `cat \"$HOME\"'/.bashrc'` :)
+;; or:
+;;      (system-re "cat ~a" (string-append (getenv "HOME") "/.bashrc") => will invoke `cat '/home/user/.bashrc'` :)
+;;
+;; Returns exit code, which is a number.
+%var system-fmt
+
+%use (system*/exit-code) "./system-star-exit-code.scm"
+%use (stringf) "./stringf.scm"
+%use (shell-quote) "./shell-quote.scm"
+
+(define (system-fmt fmt . args)
+  (let* ((command (apply stringf (cons fmt (map shell-quote args)))))
+    (system*/exit-code "/bin/sh" "-c" command)))
