4e4b66c891796c6a6a38c234e063acbd0e4d3db7
Otto Jung <otto.jung@kakao.com>
2021-08-22 21:55:59 +0200

    shell-quote: fix when to quote

--- src/shell-quote.scm
+++ src/shell-quote.scm
@@ -5,22 +5,20 @@
 ;; so the string $'b is quoted as '$'"'"'b'
 %var shell-quote
 
+%use (alphanum/alphabet/index) "./alpha-alphabet.scm"
+
 (define (shell-quote str)
   (define lst (string->list str))
 
-  (if (null? (filter (lambda (c) (equal? #\' c)) lst))
+  (if (null? (filter (negate alphanum/alphabet/index) lst))
       str
-      (let ()
-
-        (define escaped/reversed
-          (cons #\'
-                (let loop ((lst lst))
-                  (if (null? lst)
-                      '(#\')
-                      (let ((x (car lst)))
-                        (if (equal? #\' x)
-                            (cons #\' (cons #\" (cons #\' (cons #\" (cons #\' (loop (cdr lst)))))))
-                            (cons x (loop (cdr lst)))))))))
-
-        (list->string escaped/reversed))))
+      (list->string
+       (cons #\'
+             (let loop ((lst lst))
+               (if (null? lst)
+                   '(#\')
+                   (let ((x (car lst)))
+                     (if (equal? #\' x)
+                         (cons #\' (cons #\" (cons #\' (cons #\" (cons #\' (loop (cdr lst)))))))
+                         (cons x (loop (cdr lst)))))))))))
 
