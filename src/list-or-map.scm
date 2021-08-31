6828c98eba581ea56a2f80534aded08b24699243
Otto Jung <otto.jung@kakao.com>
2021-08-31 23:37:34 +0200

    add list-or-map.scm

---
+++ src/list-or-map.scm
@@ -0,0 +1,11 @@
+
+%run guile
+
+%var list-or-map
+
+(define (list-or-map f lst)
+  (let loop ((buf lst))
+    (if (null? buf) #f
+        (if (f (car buf))
+            #t
+            (loop (cdr buf))))))
