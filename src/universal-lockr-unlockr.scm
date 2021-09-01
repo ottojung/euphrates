31a6cb9459f29a7f4a9e20b5bcc29f26cae5c506
Otto Jung <otto.jung@kakao.com>
2021-09-01 18:50:39 +0200

    add universal-lockr-unlockr.scm

---
+++ src/universal-lockr-unlockr.scm
@@ -0,0 +1,36 @@
+
+%run guile
+
+%var universal-lockr!
+%var universal-unlockr!
+
+%use (hashmap) "./hashmap.scm"
+%use (hashmap-ref hashmap-set!) "./ihashmap.scm"
+%use (dynamic-thread-get-delay-procedure) "./dynamic-thread-get-delay-procedure.scm"
+%use (with-critical) "./with-critical.scm"
+%use (make-uni-spinlock-critical) "./uni-spinlock.scm"
+
+;; Like uni-spinlock but use arbitary variables as lock target
+;; and do sleep when wait
+(define-values (universal-lockr! universal-unlockr!)
+  (let ((critical (make-uni-spinlock-critical))
+        (h (hashmap)))
+    (values
+     (lambda (resource)
+       (let ((sleep (dynamic-thread-get-delay-procedure)))
+         (let lp ()
+           (when
+               (with-critical
+                critical
+                (let ((r (hashmap-ref h resource #f)))
+                  (if r
+                      #t
+                      (begin
+                        (hashmap-set! h resource #t)
+                        #f))))
+             (sleep)
+             (lp)))))
+     (lambda (resource)
+       (with-critical
+        critical
+        (hashmap-set! h resource #f))))))
