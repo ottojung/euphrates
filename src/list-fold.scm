a7a364cba27cb84dd8e90c1bca445f830590e804
Otto Jung <otto.jung@kakao.com>
2021-09-02 23:56:52 +0200

    list-fold: allow defines

--- src/list-fold.scm
+++ src/list-fold.scm
@@ -1,13 +1,16 @@
 
 %run guile
 
+;; Almost like racket's `for/list'
 %var list-fold
 
-(define-syntax-rule (list-fold (acc-name acc-value)
-                               (i-name i-value)
-                               . bodies)
-  (let loop ((acc-name acc-value) (i-all i-value))
-    (if (null? i-all) acc-name
-        (let ((i-name (car i-all)))
-          (let ((new-acc (begin . bodies)))
-            (loop new-acc (cdr i-all)))))))
+(define-syntax list-fold
+  (syntax-rules ()
+    ((_ (acc-name acc-value)
+        (i-name i-value)
+        . bodies)
+     (let loop ((acc-name acc-value) (i-all i-value))
+       (if (null? i-all) acc-name
+           (let ((i-name (car i-all)))
+             (let ((new-acc (let () . bodies)))
+               (loop new-acc (cdr i-all)))))))))
