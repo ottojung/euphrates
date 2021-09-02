d0f750d74b309dc7d19f9501393c8aaf8ba7de91
Otto Jung <otto.jung@kakao.com>
2021-09-02 21:03:36 +0200

    dynamic-thread: critical-make-p-default: switch to np-thread version

--- src/dynamic-thread-critical-make-p-default.scm
+++ src/dynamic-thread-critical-make-p-default.scm
@@ -3,23 +3,7 @@
 
 %var dynamic-thread-critical-make#p-default
 
-%use (dynamic-thread-mutex-make) "./dynamic-thread-mutex-make.scm"
-%use (dynamic-thread-mutex-lock!#p) "./dynamic-thread-mutex-lock-p.scm"
-%use (dynamic-thread-mutex-unlock!#p) "./dynamic-thread-mutex-unlock-p.scm"
-%use (dynamic-thread-disable-cancel#p) "./dynamic-thread-disable-cancel-p.scm"
-%use (dynamic-thread-enable-cancel#p) "./dynamic-thread-enable-cancel-p.scm"
+%use (np-thread-global-critical-make) "./np-thread.scm"
 
-(define (dynamic-thread-critical-make#p-default)
-  (let* ((mut (dynamic-thread-mutex-make))
-         (lock! (dynamic-thread-mutex-lock!#p))
-         (unlock! (dynamic-thread-mutex-unlock!#p))
-         (disable (dynamic-thread-disable-cancel#p))
-         (enable (dynamic-thread-enable-cancel#p)))
-    (lambda (thunk)
-      (disable)
-      (lock! mut)
-      (call-with-values thunk
-        (lambda vals
-          (unlock! mut)
-          (enable)
-          (apply values vals))))))
+(define dynamic-thread-critical-make#p-default
+  np-thread-global-critical-make)
