535872b39d9c567ad5564bfc95a732045f512e3b
Otto Jung <otto.jung@kakao.com>
2021-09-02 22:57:13 +0200

    dynamic-thread: fix "dynamic-thread-sleep#p" import

--- src/dynamic-thread-get-delay-procedure-default.scm
+++ src/dynamic-thread-get-delay-procedure-default.scm
@@ -1,7 +1,7 @@
 
 %run guile
 
-%use (dynamic-thread-sleep#p) "./dynamic-thread-sleep.scm"
+%use (dynamic-thread-sleep#p) "./dynamic-thread-sleep-p.scm"
 %use (dynamic-thread-get-wait-delay) "./dynamic-thread-get-wait-delay.scm"
 
 %var dynamic-thread-get-delay-procedure#p-default
