93d92cba033713737d49d5c2a1443b22e1a54e2a
Otto Jung <otto.jung@kakao.com>
2021-09-03 16:23:02 +0200

    dynamic-thread-async: use mutexes instead of sleep-until

--- src/dynamic-thread-async-thunk.scm
+++ src/dynamic-thread-async-thunk.scm
@@ -3,8 +3,10 @@
 
 %use (dynamic-thread-spawn) "./dynamic-thread-spawn.scm"
 %use (catch-any) "./catch-any.scm"
-%use (sleep-until) "./sleep-until.scm"
 %use (raisu) "./raisu.scm"
+%use (dynamic-thread-mutex-make) "./dynamic-thread-mutex-make.scm"
+%use (dynamic-thread-mutex-lock!) "./dynamic-thread-mutex-lock.scm"
+%use (dynamic-thread-mutex-unlock!) "./dynamic-thread-mutex-unlock.scm"
 
 ;; This is like futures.
 ;; Use this when need to join a thread.
@@ -13,24 +15,30 @@
 (define (dynamic-thread-async-thunk thunk)
   (define status #f) ;; \in { #f, 'ok, 'fail }
   (define results #f)
+  (define lock (dynamic-thread-mutex-make))
   (define thread
-    (dynamic-thread-spawn
-     (lambda ()
-       (catch-any
-        (lambda ()
-          (call-with-values thunk
-            (lambda vals
-              (set! results vals)
-              (set! status 'ok))))
-        (lambda errors
-          (set! results errors)
-          (set! status 'fail))))))
+    (begin
+      (dynamic-thread-mutex-lock! lock)
+      (dynamic-thread-spawn
+       (lambda ()
+         (catch-any
+          (lambda ()
+            (call-with-values thunk
+              (lambda vals
+                (set! results vals)
+                (set! status 'ok)
+                (dynamic-thread-mutex-unlock! lock))))
+          (lambda errors
+            (set! results errors)
+            (set! status 'fail)
+            (dynamic-thread-mutex-unlock! lock)))))))
 
   (define (wait/no-throw)
-    (sleep-until status))
+    (dynamic-thread-mutex-lock! lock)
+    (dynamic-thread-mutex-unlock! lock))
 
   (define (wait)
-    (sleep-until status)
+    (wait/no-throw)
     (when (eq? 'fail status)
       (raisu 'dynamic-thread-run-async-failed results))
     (apply values results))
