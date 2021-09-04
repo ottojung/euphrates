c57b9e9bfa1de1b712d9b683bdea86863fa9bd8f
Otto Jung <otto.jung@kakao.com>
2021-09-04 11:30:40 +0200

    cartesian-product-g: change singleton set case

--- src/cartesian-product-g.scm
+++ src/cartesian-product-g.scm
@@ -8,12 +8,15 @@
 %var cartesian-product/g
 
 (define (cartesian-product/g/reversed lists)
-  (if (or (null? lists) (null? (cdr lists))) lists
-      (let loop ((ret (cartesian-map list (cadr lists) (car lists)))
-                 (lists (cddr lists)))
-        (if (null? lists) ret
-            (loop (cartesian-product (car lists) ret)
-                  (cdr lists))))))
+  (cond
+   ((null? lists) '())
+   ((null? (cdr lists)) (map list (car lists)))
+   (else
+    (let loop ((ret (cartesian-map list (cadr lists) (car lists)))
+               (lists (cddr lists)))
+      (if (null? lists) ret
+          (loop (cartesian-product (car lists) ret)
+                (cdr lists)))))))
 
 (define (cartesian-product/g lists)
   (cartesian-product/g/reversed (reverse lists)))
