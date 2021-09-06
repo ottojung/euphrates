4cb49ef01afa8dcefa41fc77d4b911aad3a4197a
Otto Jung <otto.jung@kakao.com>
2021-09-06 15:37:28 +0200

    add define-pair

---
+++ src/define-pair.scm
@@ -0,0 +1,11 @@
+
+%run guile
+
+%var define-pair
+
+(define-syntax define-pair
+  (syntax-rules ()
+    ((_ (a b) value)
+     (define-values (a b)
+       (let ((v value))
+         (values (car v) (cdr v)))))))
