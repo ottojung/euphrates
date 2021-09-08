66079c5e81c23b757c6ac0c4eb41f930265363be
Otto Jung <otto.jung@kakao.com>
2021-09-08 10:06:34 +0200

    add linear-interpolation.scm

---
+++ src/linear-interpolation.scm
@@ -0,0 +1,29 @@
+
+%run guile
+
+%var linear-interpolate-1d
+%var linear-interpolate-2d
+
+%use (raisu) "./raisu.scm"
+
+(define (linear-interpolate-1d start end t)
+  (unless (and (number? t) (<= 0 t) (<= t 1))
+    (raisu 't-must-be-a-number-in-range-0-1 start end t))
+
+  (+ (* (- 1 t) start) (* end t)))
+
+
+(define (linear-interpolate-2d start end t)
+  (define x1 (car start))
+  (define x2 (car end))
+  (define y1 (cdr start))
+  (define y2 (cdr end))
+
+  (unless (and (number? t) (<= 0 t) (<= t 1))
+    (raisu 't-must-be-a-number-in-range-0-1 start end t))
+
+  (cons (linear-interpolate-1d x1 x2 t)
+        (linear-interpolate-1d y1 y2 t)))
+
+
+
