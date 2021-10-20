121b1b0e683c044a04ad77b265d7ca9777cf8fe1
Otto Jung <otto.jung@kakao.com>
2021-10-20 21:42:40 +0200

    memconst: dont use define-syntax-rule

--- src/memconst.scm
+++ src/memconst.scm
@@ -3,13 +3,15 @@
 
 %var memconst
 
-; memoized constant function
-(define-syntax-rule (memconst x)
-  (let ((memory #f)
-        (evaled? #f))
-    (lambda argv
-      (unless evaled?
-        (set! evaled? #t)
-        (set! memory x))
-      memory)))
+;; memoized constant function
+(define-syntax memconst
+  (syntax-rules ()
+    ((_ x)
+     (let ((memory #f)
+           (evaled? #f))
+       (lambda argv
+         (unless evaled?
+           (set! evaled? #t)
+           (set! memory x))
+         memory)))))
 
