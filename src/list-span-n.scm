e97e55a36cb8b657745bc54eb25df5d9abba3990
Otto Jung <otto.jung@kakao.com>
2021-11-08 15:38:10 +0100

    add list-span-n.scm

---
+++ src/list-span-n.scm
@@ -0,0 +1,12 @@
+
+%run guile
+
+%var list-span-n
+
+;; equivalent to (list-take-n n lst, list-drop-n n lst)
+;; list may be shorter than n
+(define (list-span-n n lst)
+  (let loop ((n n) (lst lst) (buf '()))
+    (if (or (zero? n) (null? lst))
+        (values (reverse buf) lst)
+        (loop (- n 1) (cdr lst) (cons (car lst) buf)))))
