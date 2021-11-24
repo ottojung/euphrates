a4b7f6d09dd4b5dd1039109f455143ab3bf3ce40
Otto Jung <otto.jung@kakao.com>
2021-11-24 23:27:29 +0100

    add compose-under-par

---
+++ src/compose-under-par.scm
@@ -0,0 +1,26 @@
+
+%run guile
+
+%var compose-under-par
+
+%use (syntax-reverse) "./syntax-reverse.scm"
+
+(define-syntax compose-under-par-cont
+  (syntax-rules ()
+    ((_ op buf) (op . buf))))
+
+(define-syntax compose-under-par-helper
+  (syntax-rules ()
+    [(_ args op buf ())
+     (lambda args
+       (syntax-reverse (compose-under-par-cont op) buf))]
+    [(_ args op buf (f . fs))
+     (compose-under-par-helper
+      (x . args) op
+      ((f x) . buf)
+      fs)]))
+
+(define-syntax compose-under-par
+  (syntax-rules ()
+    ((_ operation . composites)
+     (compose-under-par-helper () operation () composites))))
