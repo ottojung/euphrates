8320858a3c10c58525b8218b055c97728922cd63
Otto Jung <otto.jung@kakao.com>
2021-11-27 22:55:47 +0100

    add rtree

---
+++ src/rtree.scm
@@ -0,0 +1,19 @@
+
+%run guile
+
+%var rtree
+%var rtree?
+%var rtree-ref
+%var set-rtree-ref!
+%var rtree-value
+%var rtree-children
+
+%use (define-type9) "./define-type9.scm"
+
+;; recursive tree structure
+(define-type9 <rtree>
+  (rtree ref value children) rtree?
+  (ref rtree-ref set-rtree-ref!)
+  (value rtree-value)
+  (children rtree-children)
+  )
