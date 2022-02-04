e61d5f8586a0eee088bc063418755869e15c3685
Otto Jung <otto.jung@kakao.com>
2022-02-04 15:28:48 +0100

    assoc-set-value.scm: fix logical bug

--- src/assoc-set-value.scm
+++ src/assoc-set-value.scm
@@ -3,11 +3,11 @@
 
 %var assoc-set-value
 
-(define (assoc-set-value key value alist)
-  (let loop ((alist alist))
+(define (assoc-set-value key value alist0)
+  (let loop ((alist alist0))
     (if (null? alist)
         `((,key . ,value))
         (let ((x (car alist)))
           (if (equal? key (car x))
-              (cons `(,key . ,value) (loop (cdr alist)))
-              (cons x (cdr alist)))))))
+              (cons `(,key . ,value) (cdr alist))
+              (cons x (loop (cdr alist))))))))
