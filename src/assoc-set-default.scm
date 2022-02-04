01a39cda0b749f47d5e65fd0d53be304f08cebe6
Otto Jung <otto.jung@kakao.com>
2022-02-04 15:11:09 +0100

    assoc-set-default.scm: fix imports

--- src/assoc-set-default.scm
+++ src/assoc-set-default.scm
@@ -3,6 +3,8 @@
 
 %var assoc-set-default
 
+%use (assoc-set-value) "./assoc-set-value.scm"
+
 (define (assoc-set-default key value alist)
   (define got (assoc key alist))
   (define current (and got (cdr got)))
