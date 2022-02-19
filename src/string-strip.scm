7f68629449c1666a7ca327aa123a6e49acf3e851
Otto Jung <otto.jung@kakao.com>
2022-02-19 16:16:18 +0100

    string-strip: strip \r as well

--- src/string-strip.scm
+++ src/string-strip.scm
@@ -7,5 +7,5 @@
 
 (define string-strip
   (case-lambda
-   ((str) (string-trim-chars str "\n \t" 'both))
+   ((str) (string-trim-chars str "\r\n \t" 'both))
    ((str chars) (string-trim-chars str chars 'both))))
