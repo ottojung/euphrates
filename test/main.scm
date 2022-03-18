47380142d3be62d5870ec99186eb3ecde6b11df5
Otto Jung <otto.jung@kakao.com>
2022-03-18 22:59:11 +0100

    list-split-on: add a test

--- test/main.scm
+++ test/main.scm
@@ -1142,6 +1142,8 @@
 (let ()
   (assert= '((1) (3) (5) (7))
            (list-split-on even? (list 1 2 3 4 5 6 7)))
+  (assert= '((1 3 5 7) (9))
+           (list-split-on even? (list 1 3 5 7 2 9)))
   (assert= '((1 3 5 7))
            (list-split-on even? (list 1 3 5 7)))
   (assert= '((1 3) (5 7))
