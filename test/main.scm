25f53ffbac95f97fda313ee728d806062d23815d
Otto Jung <otto.jung@vauplace.com>
2022-07-25 16:14:42 +0200

    add catchu-case

--- test/main.scm
+++ test/main.scm
@@ -142,6 +142,7 @@
 %use (url-decompose) "./src/url-decompose.scm"
 %use (url-goto) "./src/url-goto.scm"
 %use (path-normalize) "./src/path-normalize.scm"
+%use (catchu-case) "./src/catchu-case.scm"
 
 (let ()
   (catch-any
@@ -3020,6 +3021,43 @@
 
   (assert= " "
            (path-normalize " "))
+
+  )
+
+(let () ;; catchu-case
+
+  (define-syntax test
+    (syntax-rules ()
+      ((_ body)
+       (assert=
+        "OK: x y z\n"
+        (with-output-to-string
+          (lambda _ body))))))
+
+  (test
+   (catchu-case
+    (raisu 'test-exception-type-1)
+    (('test-exception-type-1)
+     (dprintln "OK: x y z"))))
+
+  (test
+   (catchu-case
+    (raisu 'test-exception-type-1 'x 'y 'z)
+    (('test-exception-type-1 arg1 . args)
+     (apply dprintln (cons "OK: ~s ~s ~s" (cons arg1 args))))))
+
+  (test
+   (catchu-case
+    (raisu 'test-exception-type-1 'x 'y 'z)
+
+    (('some-bad-exception-type arg1 arg2 arg3)
+     (raisu 'bad-catch!))
+
+    (('test-exception-type-1 arg1 . args)
+     (apply dprintln (cons "OK: ~s ~s ~s" (cons arg1 args))))
+
+    (('some-bad-exception-type-2 arg1 arg2 arg3)
+     (raisu 'bad-catch-2!))))
 
   )
 
