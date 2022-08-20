ddb4779d06d4db9cea97dd8cd576122b790fb90f
Otto Jung <otto.jung@vauplace.com>
2022-08-20 17:07:33 +0200

    big-random-int: base on srfi-27 implementation

--- src/big-random-int.scm
+++ src/big-random-int.scm
@@ -3,26 +3,10 @@
 
 %var big-random-int
 
-%for (COMPILER "guile")
+%use (get-current-random-source) "./get-current-random-source.scm"
+%use (random-source-make-integers) "./srfi-27-generic.scm"
 
 (define big-random-int
-  (let ((initialized? #f))
-    (lambda (max)
-      (unless initialized?
-        ;; NOTE: in guile, random is deterministic by default
-        ;; while rackets is non-deterministic by default
-        ;; It would be ok if they both were deterministic,
-        ;; but it is easier to change guile, so there it is.
-        ;; TODO: deterministic random at will
-        (set! initialized? #t)
-        (set! *random-state* (random-state-from-platform)))
-      (random max))))
-
-%end
-
-%for (COMPILER "racket")
-
-(define [big-random-int max]
-  (exact-floor (* (random) max))) ;; random in racket is bounded to 4294967087
-
-%end
+  (lambda (max)
+    ((random-source-make-integers
+      (get-current-random-source)) max)))
