e742a0ddf525c9baeb0e474a8692aba83cb698cb
Otto Jung <otto.jung@vauplace.com>
2022-08-20 16:24:10 +0200

    srfi-27: implement random-source

---
+++ src/srfi-27-backbone-generator.scm
@@ -0,0 +1,80 @@
+;;;; Copyright (C) 2022  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Originally taken from: https://github.com/scheme-requests-for-implementation/srfi-27/blob/master/reference/mrg32k3a-a.scm
+
+%run guile
+
+%var mrg32k3a-pack-state
+%var mrg32k3a-unpack-state
+%var mrg32k3a-random-range
+%var mrg32k3a-random-integer
+%var mrg32k3a-random-real
+
+;;
+;; 54-BIT INTEGER IMPLEMENTATION OF THE "MRG32K3A"-GENERATOR
+;; =========================================================
+;;
+;; Sebastian.Egner@philips.com, Mar-2002.
+;;
+;; This file is an implementation of Pierre L'Ecuyer's MRG32k3a
+;; pseudo random number generator. Please refer to 'mrg32k3a.scm'
+;; for more information.
+;;
+;; compliance:
+;;   Scheme R5RS with integers covering at least {-2^53..2^53-1}.
+;;
+;; history of this file:
+;;   SE, 18-Mar-2002: initial version
+;;   SE, 22-Mar-2002: comments adjusted, range added
+;;   SE, 25-Mar-2002: pack/unpack just return their argument
+
+;; the actual generator
+
+(define (mrg32k3a-random-m1 state)
+  (let ((x11 (vector-ref state 0))
+        (x12 (vector-ref state 1))
+        (x13 (vector-ref state 2))
+        (x21 (vector-ref state 3))
+        (x22 (vector-ref state 4))
+        (x23 (vector-ref state 5)))
+    (let ((x10 (modulo (- (* 1403580 x12) (* 810728 x13)) 4294967087))
+          (x20 (modulo (- (* 527612 x21) (* 1370589 x23)) 4294944443)))
+      (vector-set! state 0 x10)
+      (vector-set! state 1 x11)
+      (vector-set! state 2 x12)
+      (vector-set! state 3 x20)
+      (vector-set! state 4 x21)
+      (vector-set! state 5 x22)
+      (modulo (- x10 x20) 4294967087))))
+
+;; interface to the generic parts of the generator
+
+(define (mrg32k3a-pack-state unpacked-state)
+  unpacked-state)
+
+(define (mrg32k3a-unpack-state state)
+  state)
+
+(define (mrg32k3a-random-range) ; m1
+  4294967087)
+
+(define (mrg32k3a-random-integer state range) ; rejection method
+  (let* ((q (quotient 4294967087 range))
+         (qn (* q range)))
+    (do ((x (mrg32k3a-random-m1 state) (mrg32k3a-random-m1 state)))
+        ((< x qn) (quotient x q)))))
+
+(define (mrg32k3a-random-real state) ; normalization is 1/(m1+1)
+  (* 0.0000000002328306549295728 (+ 1.0 (mrg32k3a-random-m1 state))))
---
+++ src/srfi-27-random-source-obj.scm
@@ -0,0 +1,57 @@
+;;;; Copyright (C) 2022  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+;; For the use exclusively in srfi-27-base-generator.scm
+
+%var :random-source-make
+%var :random-source?
+%var :random-source-state-ref
+%var :random-source-state-set!
+%var :random-source-randomize!
+%var :random-source-pseudo-randomize!
+%var :random-source-make-integers
+%var :random-source-make-reals
+%var :random-source-current-time
+
+%use (time-get-monotonic-nanoseconds-timestamp) "./time-get-monotonic-nanoseconds-timestamp.scm"
+
+(define (:random-source-make a0 a1 a2 a3 a4 a5)
+  (vector a0 a1 a2 a3 a4 a5))
+
+(define (:random-source? s)
+  (and (vector? s)
+       (= 6 (vector-length s))))
+
+(define (:random-source-state-ref s)
+  (vector-ref s 0))
+
+(define (:random-source-state-set! s)
+  (vector-ref s 1))
+
+(define (:random-source-randomize! s)
+  (vector-ref s 2))
+
+(define (:random-source-pseudo-randomize! s)
+  (vector-ref s 3))
+
+(define (:random-source-make-integers s)
+  (vector-ref s 4))
+
+(define (:random-source-make-reals s)
+  (vector-ref s 5))
+
+(define (:random-source-current-time)
+  (time-get-monotonic-nanoseconds-timestamp))
