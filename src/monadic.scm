6faaeca5246b64340050df4cf02e84dccf012bee
Otto Jung <otto.jung@vauplace.com>
2022-10-28 16:00:44 +0200

    monad: code cleanup

--- src/monadic.scm
+++ src/monadic.scm
@@ -4,6 +4,7 @@
 %var monadic-bare
 %var monadic
 
+%use (identity*) "./identity-star.scm"
 %use (memconst) "./memconst.scm"
 %use (monadarg monadarg-cont monadarg-lval monadarg-qvar) "./monadarg.scm"
 %use (monadfin monadfin-lval) "./monadfin.scm"
@@ -11,14 +12,7 @@
 %use (raisu) "./raisu.scm"
 %use (range) "./range.scm"
 
-(define-syntax monadic-bare-handle-tags
-  (syntax-rules ()
-    ((monadic-bare-handle-tags ())
-     (list))
-    ((monadic-bare-handle-tags . tags)
-     (list . tags))))
-
-(define (rethunkify-list m)
+(define (monadic-bare-rethunkify m)
   (define thunk (monadarg-lval m))
   (define qvar (monadarg-qvar m))
   (define len (if (list? qvar) (length qvar) 1))
@@ -35,59 +29,48 @@
    (else
     (raisu 'bad-thunk-type thunk))))
 
+(define (monadic-bare-continue m)
+  (apply (monadarg-cont m) (monadic-bare-rethunkify m)))
+
+(define-syntax monadic-bare-single
+  (syntax-rules ()
+    ((_ (f var val tags) arg cont)
+     (monadic-bare-continue
+      (f
+       (monadarg
+        (memconst arg)
+        cont
+        (quote var)
+        (quote val)
+        (list . tags)))))))
+
 ;; This is something like "do syntax" from Haskell
 (define-syntax monadic-bare-helper
   (syntax-rules ()
 
-    ((_ f ((a . as) b . tags) . ())
-     (call-with-values
-         (lambda _
-           (f
-            (monadarg
-             (memconst (call-with-values (lambda _ b) (lambda x x)))
-             (lambda args (apply values args))
-             (quote (a . as))
-             (quote b)
-             (monadic-bare-handle-tags . tags))))
-       (lambda (m)
-         (apply (monadarg-cont m) (rethunkify-list m)))))
+    ((_ f ((x . xs) y . tags) . ())
+     (monadic-bare-single
+      (f (x . xs) y tags)
+      (call-with-values (lambda _ y) list)
+      identity*))
 
-    ((_ f ((a . as) b . tags) . bodies)
-     (call-with-values
-         (lambda _
-           (f (monadarg
-               (memconst (call-with-values (lambda _ b) (lambda x x)))
-               (lambda (a . as)
-                 (monadic-bare-helper f . bodies))
-               (quote (a . as))
-               (quote b)
-               (monadic-bare-handle-tags . tags))))
-       (lambda (m)
-         (apply (monadarg-cont m) (rethunkify-list m)))))
+    ((_ f ((x . xs) y . tags) . bodies)
+     (monadic-bare-single
+      (f (x . xs) y tags)
+      (call-with-values (lambda _ y) list)
+      (lambda (x . xs) (monadic-bare-helper f . bodies))))
 
-    ((_ f (a b . tags) . ())
-     (call-with-values
-         (lambda _
-           (f (monadarg
-               (memconst (list b))
-               identity
-               (quote a)
-               (quote b)
-               (monadic-bare-handle-tags . tags))))
-       (lambda (m)
-         (apply (monadarg-cont m) (rethunkify-list m)))))
+    ((_ f (x y . tags) . ())
+     (monadic-bare-single
+      (f x y tags)
+      (list y)
+      identity))
 
-    ((_ f (a b . tags) . bodies)
-     (call-with-values
-         (lambda _
-           (f (monadarg
-               (memconst (list b))
-               (lambda (a) (monadic-bare-helper f . bodies))
-               (quote a)
-               (quote b)
-               (monadic-bare-handle-tags . tags))))
-       (lambda (m)
-         (apply (monadarg-cont m) (rethunkify-list m)))))))
+    ((_ f (x y . tags) . bodies)
+     (monadic-bare-single
+      (f x y tags)
+      (list y)
+      (lambda (x) (monadic-bare-helper f . bodies))))))
 
 (define-syntax monadic-bare
   (syntax-rules ()
