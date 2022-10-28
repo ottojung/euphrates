7cec184e0fe7c8e37e59e3129e11f76aa0875af5
Otto Jung <otto.jung@vauplace.com>
2022-10-28 13:12:33 +0200

    monad-lazy: simplify return values handling

--- src/monad-lazy.scm
+++ src/monad-lazy.scm
@@ -3,29 +3,18 @@
 
 %var monad-lazy
 
-%use (monadarg-qvar monadarg-qtags monadarg-lval) "./monadarg.scm"
-%use (monad-arg monad-ret/thunk) "./monad.scm"
-%use (monadfin?) "./monadfin.scm"
 %use (dynamic-thread-async) "./dynamic-thread-async.scm"
-%use (range) "./range.scm"
 %use (memconst) "./memconst.scm"
+%use (monad-arg monad-ret/thunk) "./monad.scm"
+%use (monadarg-lval monadarg-qtags) "./monadarg.scm"
+%use (monadfin?) "./monadfin.scm"
 
 ;; Provides lazy evaluation, with "async" feature
 (define monad-lazy
   (lambda (monad-input)
     (if (monadfin? monad-input) monad-input
-        (let* ((qvar (monadarg-qvar monad-input))
-               (len (if (list? qvar) (length qvar) 1))
-               (single? (< len 2))
-               (result
+        (let* ((result
                 (if (memq 'async (monadarg-qtags monad-input))
                     (dynamic-thread-async (monad-arg monad-input))
-                    (monadarg-lval monad-input)))
-               (choose
-                (lambda (i)
-                  (memconst
-                   (list-ref (result) i))))
-               (return
-                (if single? result
-                    (map choose (range len)))))
-          (monad-ret/thunk monad-input return)))))
+                    (monadarg-lval monad-input))))
+          (monad-ret/thunk monad-input result)))))
