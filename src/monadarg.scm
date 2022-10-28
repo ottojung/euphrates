bc1958d9da745594a4af8d4a5eedc77c51becf05
Otto Jung <otto.jung@vauplace.com>
2022-10-28 17:28:20 +0200

    monadarg: make mutable

--- src/monadarg.scm
+++ src/monadarg.scm
@@ -4,17 +4,22 @@
 %var monadarg
 %var monadarg?
 %var monadarg-lval
+%var set-monadarg-lval!
 %var monadarg-cont
+%var set-monadarg-cont!
 %var monadarg-qvar
+%var set-monadarg-qvar!
 %var monadarg-qval
+%var set-monadarg-qval!
 %var monadarg-qtags
+%var set-monadarg-qtags!
 
 %use (define-type9) "./define-type9.scm"
 
 (define-type9 <monadarg>
   (monadarg lval cont qvar qval qtags) monadarg?
-  (lval monadarg-lval)
-  (cont monadarg-cont)
-  (qvar monadarg-qvar)
-  (qval monadarg-qval)
-  (qtags monadarg-qtags))
+  (lval monadarg-lval set-monadarg-lval!)
+  (cont monadarg-cont set-monadarg-cont!)
+  (qvar monadarg-qvar set-monadarg-qvar!)
+  (qval monadarg-qval set-monadarg-qval!)
+  (qtags monadarg-qtags set-monadarg-qtags!))
