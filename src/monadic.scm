718af3203d6637cee4ace17475546b3cebb5bab1
Otto Jung <otto.jung@vauplace.com>
2022-10-28 19:08:26 +0200

    add with-monad

--- src/monadic.scm
+++ src/monadic.scm
@@ -5,12 +5,9 @@
 %var monadic
 
 %use (identity*) "./identity-star.scm"
-%use (monad-current-arg/p) "./monad-current-arg-p.scm"
 %use (monad-current/p) "./monad-current-p.scm"
 %use (monad-do/generic) "./monad-do.scm"
-%use (monadarg-make-empty) "./monadarg.scm"
-%use (monadfin monadfin-lval) "./monadfin.scm"
-%use (monadic-global/p) "./monadic-global-p.scm"
+%use (with-monad) "./with-monad.scm"
 
 ;; This is something like "do syntax" from Haskell
 (define-syntax monadic-bare-helper
@@ -36,26 +33,7 @@
       (f x y (list . tags))
       (lambda (x) (monadic-bare-helper f . bodies))))))
 
-(define-syntax monadic-bare
-  (syntax-rules ()
-    ((_ m . args)
-     (apply
-      values
-      (call-with-values
-          (lambda _
-            (monadic-bare-helper m . args))
-        (lambda results
-          ((monadfin-lval
-            (m (monadfin
-                (lambda _ (map (lambda (f) (f)) results))))))))))))
-
-;; NOTE: uses parameterization
 (define-syntax monadic
   (syntax-rules ()
     ((_ fexpr . argv)
-     (let* ((p (monadic-global/p))
-            (f fexpr)
-            (m (if p (p f (quote fexpr)) f)))
-       (parameterize ((monad-current/p m)
-                      (monad-current-arg/p (monadarg-make-empty)))
-         (monadic-bare m . argv))))))
+     (with-monad fexpr (monadic-bare-helper (monad-current/p) . argv)))))
---
+++ src/with-monad.scm
@@ -0,0 +1,40 @@
+;;;; Copyright (C) 2022  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+%var with-monad
+
+%use (monad-current-arg/p) "./monad-current-arg-p.scm"
+%use (monad-current/p) "./monad-current-p.scm"
+%use (monadarg-make-empty) "./monadarg.scm"
+%use (monadfin monadfin-lval) "./monadfin.scm"
+%use (monadic-global/p) "./monadic-global-p.scm"
+
+(define-syntax with-monad
+  (syntax-rules ()
+    ((_ fexpr body . bodies)
+     (let* ((p (monadic-global/p))
+            (f fexpr)
+            (m (if p (p f (quote fexpr)) f)))
+       (parameterize ((monad-current/p m)
+                      (monad-current-arg/p (monadarg-make-empty)))
+         (apply
+          values
+          (call-with-values
+              (lambda _ body . bodies)
+            (lambda results
+              ((monadfin-lval
+                (m (monadfin
+                    (lambda _ (map (lambda (f) (f)) results))))))))))))))
