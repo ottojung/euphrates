0208027a993d1943ca56d2ec5724ffc1aa57a5ef
Otto Jung <otto.jung@vauplace.com>
2022-10-28 18:28:31 +0200

    add monad-do

---
+++ src/monad-do.scm
@@ -0,0 +1,77 @@
+;;;; Copyright (C) 2020, 2021, 2022  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+%var monad-do
+%var monad-do/generic
+
+%use (memconst) "./memconst.scm"
+%use (monad-current-arg/p) "./monad-current-arg-p.scm"
+%use (monad-current/p) "./monad-current-p.scm"
+%use (monadarg-cont monadarg-lval monadarg-qvar set-monadarg-cont! set-monadarg-lval! set-monadarg-qtags! set-monadarg-qval! set-monadarg-qvar!) "./monadarg.scm"
+%use (raisu) "./raisu.scm"
+%use (range) "./range.scm"
+
+(define (monad-do/rethunkify m)
+  (define thunk (monadarg-lval m))
+  (define qvar (monadarg-qvar m))
+  (define len (if (list? qvar) (length qvar) 1))
+
+  (cond
+   ((list? thunk) thunk)
+   ((procedure? thunk)
+    (let ((result (memconst (thunk))))
+      (map
+       (lambda (i)
+         (memconst
+          (list-ref (result) i)))
+       (range len))))
+   (else
+    (raisu 'bad-thunk-type thunk))))
+
+(define (monad-do/continue m)
+  (apply (monadarg-cont m) (monad-do/rethunkify m)))
+
+(define-syntax monad-do/generic
+  (syntax-rules ()
+    ((_ (f var val qtags) cont)
+     (let ((arg (monad-current-arg/p)))
+       (set-monadarg-lval! arg (memconst (call-with-values (lambda _ val) list)))
+       (set-monadarg-cont! arg cont)
+       (let* ((qvar0 (quote var))
+              (qvar (if (equal? qvar0 (quote #f)) #f qvar0)))
+         (set-monadarg-qvar! arg qvar))
+       (set-monadarg-qval! arg (quote val))
+       (set-monadarg-qtags! arg qtags)
+       (monad-do/continue (f arg))))))
+
+(define-syntax monad-do
+  (syntax-rules ()
+    ((_ (var val qtags) cont)
+     (let ((f (monad-current/p)))
+       (monad-do/generic (f var val qtags) cont)))
+    ((_ (var val qtags))
+     (call-with-current-continuation
+      (lambda (cont)
+        (let ((f (monad-current/p)))
+          (monad-do/generic (f var val qtags) cont)))))
+    ((_ (val qtags) cont)
+     (let ((f (monad-current/p)))
+       (monad-do/generic (f #f val qtags) cont)))
+    ((_ (val qtags))
+     (call-with-current-continuation
+      (lambda (cont)
+        (let ((f (monad-current/p)))
+          (monad-do/generic (f #f val qtags) cont)))))))
--- src/monadic.scm
+++ src/monadic.scm
@@ -5,68 +5,35 @@
 %var monadic
 
 %use (identity*) "./identity-star.scm"
-%use (memconst) "./memconst.scm"
 %use (monad-current-arg/p) "./monad-current-arg-p.scm"
 %use (monad-current/p) "./monad-current-p.scm"
-%use (monadarg-cont monadarg-lval monadarg-make-empty monadarg-qvar set-monadarg-cont! set-monadarg-lval! set-monadarg-qtags! set-monadarg-qval! set-monadarg-qvar!) "./monadarg.scm"
+%use (monad-do/generic) "./monad-do.scm"
+%use (monadarg-make-empty) "./monadarg.scm"
 %use (monadfin monadfin-lval) "./monadfin.scm"
 %use (monadic-global/p) "./monadic-global-p.scm"
-%use (raisu) "./raisu.scm"
-%use (range) "./range.scm"
-
-(define (monadic-bare-rethunkify m)
-  (define thunk (monadarg-lval m))
-  (define qvar (monadarg-qvar m))
-  (define len (if (list? qvar) (length qvar) 1))
-
-  (cond
-   ((list? thunk) thunk)
-   ((procedure? thunk)
-    (let ((result (memconst (thunk))))
-      (map
-       (lambda (i)
-         (memconst
-          (list-ref (result) i)))
-       (range len))))
-   (else
-    (raisu 'bad-thunk-type thunk))))
-
-(define (monadic-bare-continue m)
-  (apply (monadarg-cont m) (monadic-bare-rethunkify m)))
-
-(define-syntax monadic-bare-single
-  (syntax-rules ()
-    ((_ (f var val tags) cont)
-     (let ((arg (monad-current-arg/p)))
-       (set-monadarg-lval! arg (memconst (call-with-values (lambda _ val) list)))
-       (set-monadarg-cont! arg cont)
-       (set-monadarg-qvar! arg (quote var))
-       (set-monadarg-qval! arg (quote val))
-       (set-monadarg-qtags! arg (list . tags))
-       (monadic-bare-continue (f arg))))))
 
 ;; This is something like "do syntax" from Haskell
 (define-syntax monadic-bare-helper
   (syntax-rules ()
 
     ((_ f ((x . xs) y . tags) . ())
-     (monadic-bare-single
-      (f (x . xs) y tags)
+     (monad-do/generic
+      (f (x . xs) y (list . tags))
       identity*))
 
     ((_ f ((x . xs) y . tags) . bodies)
-     (monadic-bare-single
-      (f (x . xs) y tags)
+     (monad-do/generic
+      (f (x . xs) y (list . tags))
       (lambda (x . xs) (monadic-bare-helper f . bodies))))
 
     ((_ f (x y . tags) . ())
-     (monadic-bare-single
-      (f x y tags)
+     (monad-do/generic
+      (f x y (list . tags))
       identity))
 
     ((_ f (x y . tags) . bodies)
-     (monadic-bare-single
-      (f x y tags)
+     (monad-do/generic
+      (f x y (list . tags))
       (lambda (x) (monadic-bare-helper f . bodies))))))
 
 (define-syntax monadic-bare
