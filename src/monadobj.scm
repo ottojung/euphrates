90d0ae971d2c90318435ca0823712398f8520b47
Otto Jung <otto.jung@vauplace.com>
2022-10-29 12:49:41 +0200

    add monadobj

---
+++ src/monadobj.scm
@@ -0,0 +1,30 @@
+;;;; Copyright (C) 2022  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+%var monadobj-constructor
+%var monadobj?
+%var monadobj-procedure
+%var monadobj-uses-continuations?
+%var monadobj-handles-fin?
+
+%use (define-type9) "./define-type9.scm"
+
+(define-type9 <monadobj>
+  (monadobj-constructor procedure uses-continuations? handles-fin?) monadobj?
+  (procedure monadobj-procedure)
+  (uses-continuations? monadobj-uses-continuations?)
+  (handles-fin? monadobj-handles-fin?)
+  )
