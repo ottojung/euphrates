134f8507df9d8f4305063c9615b174714db5fddc
Otto Jung <otto.jung@vauplace.com>
2022-10-30 14:25:31 +0100

    test: add catchu-case case

--- test/main.scm
+++ test/main.scm
@@ -3184,42 +3184,49 @@
 
 ;;   )
 
-;; (let () ;; catchu-case
+(let () ;; catchu-case
 
-;;   (define-syntax test
-;;     (syntax-rules ()
-;;       ((_ body)
-;;        (assert=
-;;         "OK: x y z\n"
-;;         (with-output-to-string
-;;           (lambda _ body))))))
+  (define-syntax test
+    (syntax-rules ()
+      ((_ body)
+       (assert=
+        "OK: x y z\n"
+        (with-output-to-string
+          (lambda _ body))))))
 
-;;   (test
-;;    (catchu-case
-;;     (raisu 'test-exception-type-1)
-;;     (('test-exception-type-1)
-;;      (dprintln "OK: x y z"))))
+  (assert=
+   10
+   (catchu-case
+    10
+    (('test-no-exept x y)
+     (raisu 'impossible))))
 
-;;   (test
-;;    (catchu-case
-;;     (raisu 'test-exception-type-1 'x 'y 'z)
-;;     (('test-exception-type-1 arg1 . args)
-;;      (apply dprintln (cons "OK: ~s ~s ~s" (cons arg1 args))))))
+  (test
+   (catchu-case
+    (raisu 'test-exception-type-1)
+    (('test-exception-type-1)
+     (dprintln "OK: x y z"))))
 
-;;   (test
-;;    (catchu-case
-;;     (raisu 'test-exception-type-1 'x 'y 'z)
+  (test
+   (catchu-case
+    (raisu 'test-exception-type-1 'x 'y 'z)
+    (('test-exception-type-1 arg1 . args)
+     (apply dprintln (cons "OK: ~s ~s ~s" (cons arg1 args))))))
 
-;;     (('some-bad-exception-type arg1 arg2 arg3)
-;;      (raisu 'bad-catch!))
+  (test
+   (catchu-case
+    (raisu 'test-exception-type-1 'x 'y 'z)
 
-;;     (('test-exception-type-1 arg1 . args)
-;;      (apply dprintln (cons "OK: ~s ~s ~s" (cons arg1 args))))
+    (('some-bad-exception-type arg1 arg2 arg3)
+     (raisu 'bad-catch!))
 
-;;     (('some-bad-exception-type-2 arg1 arg2 arg3)
-;;      (raisu 'bad-catch-2!))))
+    (('test-exception-type-1 arg1 . args)
+     (apply dprintln (cons "OK: ~s ~s ~s" (cons arg1 args))))
 
-;;   )
+    (('some-bad-exception-type-2 arg1 arg2 arg3)
+     (raisu 'bad-catch-2!))))
+
+  )
 
 ;; (let () ;; random-variable-name
 ;;   (define n1 (random-variable-name 20))
