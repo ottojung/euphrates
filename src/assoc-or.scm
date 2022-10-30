cb589c6f2ee4bcb25a247ed23bbc86c4e80e9192
Otto Jung <otto.jung@vauplace.com>
2022-10-30 22:51:43 +0100

    assoc-or: make it a macro

--- src/assoc-or.scm
+++ src/assoc-or.scm
@@ -3,12 +3,12 @@
 
 %var assoc-or
 
-(define assoc-or
-  (case-lambda
-   ((key alist default)
-    (let ((got (assoc key alist)))
-      (if got (cdr got)
-          default)))
-   ((key alist)
-    (let ((got (assoc key alist)))
-      (and got (cdr got))))))
+(define-syntax assoc-or
+  (syntax-rules ()
+    ((_ key alist default)
+     (let ((got (assoc key alist)))
+       (if got (cdr got)
+           default)))
+    ((_ key alist)
+     (let ((got (assoc key alist)))
+       (and got (cdr got))))))
