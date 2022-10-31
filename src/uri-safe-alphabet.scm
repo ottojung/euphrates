55561324ab99b4799c1cdc00a9c97b98f19f3408
Otto Jung <otto.jung@vauplace.com>
2022-10-31 12:28:07 +0100

    add uri-safe-alphabet

---
+++ src/uri-safe-alphabet.scm
@@ -0,0 +1,98 @@
+;;;; Copyright (C) 2022  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+%var uri-safe/alphabet
+%var uri-safe/alphabet/index
+
+;; RFC 3986 section 2.3 Unreserved Characters
+(define uri-safe/alphabet
+  #(#\A #\B #\C #\D #\E #\F #\G #\H #\I #\J
+    #\K #\L #\M #\N #\O #\P #\Q #\R #\S #\T
+    #\U #\V #\W #\X #\Y #\Z #\a #\b #\c #\d
+    #\e #\f #\g #\h #\i #\j #\k #\l #\m #\n
+    #\o #\p #\q #\r #\s #\t #\u #\v #\w #\x
+    #\y #\z #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7
+    #\8 #\9 #\- #\_ #\. #\~))
+
+(define (uri-safe/alphabet/index c)
+  (case c
+    ((#\A) 0)
+    ((#\B) 1)
+    ((#\C) 2)
+    ((#\D) 3)
+    ((#\E) 4)
+    ((#\F) 5)
+    ((#\G) 6)
+    ((#\H) 7)
+    ((#\I) 8)
+    ((#\J) 9)
+    ((#\K) 10)
+    ((#\L) 11)
+    ((#\M) 12)
+    ((#\N) 13)
+    ((#\O) 14)
+    ((#\P) 15)
+    ((#\Q) 16)
+    ((#\R) 17)
+    ((#\S) 18)
+    ((#\T) 19)
+    ((#\U) 20)
+    ((#\V) 21)
+    ((#\W) 22)
+    ((#\X) 23)
+    ((#\Y) 24)
+    ((#\Z) 25)
+    ((#\a) 26)
+    ((#\b) 27)
+    ((#\c) 28)
+    ((#\d) 29)
+    ((#\e) 30)
+    ((#\f) 31)
+    ((#\g) 32)
+    ((#\h) 33)
+    ((#\i) 34)
+    ((#\j) 35)
+    ((#\k) 36)
+    ((#\l) 37)
+    ((#\m) 38)
+    ((#\n) 39)
+    ((#\o) 40)
+    ((#\p) 41)
+    ((#\q) 42)
+    ((#\r) 43)
+    ((#\s) 44)
+    ((#\t) 45)
+    ((#\u) 46)
+    ((#\v) 47)
+    ((#\w) 48)
+    ((#\x) 49)
+    ((#\y) 50)
+    ((#\z) 51)
+    ((#\0) 52)
+    ((#\1) 53)
+    ((#\2) 54)
+    ((#\3) 55)
+    ((#\4) 56)
+    ((#\5) 57)
+    ((#\6) 58)
+    ((#\7) 59)
+    ((#\8) 60)
+    ((#\9) 61)
+    ((#\-) 62)
+    ((#\_) 63)
+    ((#\.) 64)
+    ((#\~) 65)
+    (else #f)))
