5779d9ac0c41a2fe22d77dc61e6a82580c912112
Otto Jung <otto.jung@vauplace.com>
2022-10-31 11:58:43 +0100

    string-plus-encode: more compression

--- src/string-plus-encode.scm
+++ src/string-plus-encode.scm
@@ -20,32 +20,36 @@
 %use (alphanum/alphabet alphanum/alphabet/index) "./alphanum-alphabet.scm"
 %use (convert-number-base/generic) "./convert-number-base.scm"
 
-;; NOTE: this is like the uri-encoding, but simpler (I think) and compresses better.
+;; NOTE: this is like the uri-encoding, but compresses better.
 (define-values (string-plus-encode/generic string-plus-encode)
   (let ()
-    (define (generic s alphabet in-alphabet? maxcode plus-char)
+    (define (generic s alphabet in-alphabet? maxcode open-char)
       (define len (string-length s))
       (define alen (vector-length alphabet))
-      (define counter 0)
+      (define last-opened? #f)
+      (define close-sequence
+        (string open-char (vector-ref alphabet 0)))
 
       (if (string-index s (negate in-alphabet?))
           (call-with-output-string
            (lambda (port)
              (string-for-each
               (lambda (ch)
-                (set! counter (+ 1 counter))
                 (if (in-alphabet? ch)
-                    (display ch port)
                     (begin
-                      (display plus-char port)
+                      (when last-opened?
+                        (set! last-opened? #f)
+                        (display close-sequence port))
+                      (display ch port))
+                    (begin
+                      (set! last-opened? #t)
+                      (display open-char port)
                       (display
                        (list->string
                         (convert-number-base/generic
                          alphabet alen
                          (tointeger alphabet alen maxcode ch)))
-                       port)
-                      (unless (= len counter)
-                        (display plus-char port)))))
+                       port))))
               s)))
           s))
 
@@ -61,7 +65,7 @@
              (- i 1)))))
 
     (define (tointeger alphabet alen maxcode ch)
-      (define code (char->integer ch))
+      (define code (+ 1 (char->integer ch)))
       (cond
        ((<= code maxcode) code)
        ((<= code (+ maxcode alen))
--- test/main.scm
+++ test/main.scm
@@ -3351,9 +3351,11 @@
     (assert= a (string-plus-encode b)))
 
   (test1 "1234" "1234")
-  (test1 "abcd" "abcd")
+  (test1 "hello" "hello")
   (test1 "" "")
-  (test1 "+5gK++5Wv++5bq++7Nk" "你好世界")
+  (test1 "+5gL+5Ww+5br+7Nl" "你好世界")
+  (test1 "hello+J+x+5gL+5Ww+5br+7Nl" "hello, 你好世界")
+  (test1 "hello+J+x+5gL+5Ww+5br+7Nl+J+x+0c+3e+x+0va+12" "hello, 你好世界, cą va?")
   )
 
 (display "All good\n")
