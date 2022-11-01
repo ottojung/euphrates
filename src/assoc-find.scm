4128d4b7f6142d80092a77be70a6ef6d5ec1d88e
Otto Jung <otto.jung@vauplace.com>
2022-11-01 18:59:56 +0100

    assoc-find: fix compilation

--- src/assoc-find.scm
+++ src/assoc-find.scm
@@ -3,13 +3,20 @@
 
 %var assoc/find
 
-(define (assoc/find predicate alist)
-  (let loop ((alist alist))
-    (if (null? alist) default
-        (let* ((x (car alist))
-               (k (car x)))
-          (if (predicate k keys)
-              x
-              (loop (cdr alist)))))))
+%use (raisu) "./raisu.scm"
+
+(define-syntax assoc/find
+  (syntax-rules ()
+    ((_ predicate alist)
+     (assoc-find predicate alist (raisu 'could-not-find-the-element alist)))
+    ((_ predicate0 alist default)
+     (let ((predicate predicate0))
+       (let loop ((alist alist))
+         (if (null? alist) default
+             (let* ((x (car alist))
+                    (k (car x)))
+               (if (predicate k)
+                   x
+                   (loop (cdr alist))))))))))
 
 
