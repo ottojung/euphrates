ad81dab705dc62c202550187e58bbf214824e462
Otto Jung <otto.jung@vauplace.com>
2022-11-02 00:36:47 +0100

    test: better reporting

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,11 +1,25 @@
 #! /bin/sh
 
+TESTCOUNT=$(ls test/test-*.scm | wc -l)
+INDEX=0
+
 for FILE in test/test-*.scm
 do
-	printf "> %s ..." "$(basename "$FILE")"
+	INDEX=$((INDEX + 1))
+	SHORT="$(basename "$FILE")"
+	SHORTLEN=$(echo "$SHORT" | wc -c)
+
+	LEFT=$((40 - SHORTLEN))
+	if test "$LEFT" -lt 0
+	then LEFT=0
+	fi
+
+	# printf '%0.s-' $(seq 1 $LEFT)
+	printf '(%s/%s)' "$INDEX" "$TESTCOUNT"
+	printf ' %s ... ' "$SHORT"
 
 	if build/czempak run "$FILE" 2>&1
-	then printf " ✓"
-	else printf " X"
+	then printf '✓'
+	else printf 'X'
 	fi | grep -v -e '^;;; '
 done
