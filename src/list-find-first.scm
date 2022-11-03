10e5e6019a8c790c976b34c426197965256cbcb3
Otto Jung <otto.jung@vauplace.com>
2022-11-03 14:29:18 +0100

    list-find-first: make it a macro

--- src/list-find-first.scm
+++ src/list-find-first.scm
@@ -5,17 +5,16 @@
 
 %use (raisu) "./raisu.scm"
 
-(define list-find-first
-  (case-lambda
-   ((f lst)
-    (let loop ((lst lst))
-      (if (null? lst) (raisu 'no-first-element-to-satisfy-predicate f)
-          (let ((x (car lst)))
-            (if (f x) x
-                (loop (cdr lst)))))))
-   ((f default lst)
-    (let loop ((lst lst))
-      (if (null? lst) default
-          (let ((x (car lst)))
-            (if (f x) x
-                (loop (cdr lst)))))))))
+(define-syntax list-find-first
+  (syntax-rules ()
+    ((_ f lst)
+     (list-find-first
+      f (raisu 'no-first-element-to-satisfy-predicate f)
+      lst))
+    ((_ f0 default lst0)
+     (let ((f f0) (lst lst0))
+       (let loop ((lst lst))
+         (if (null? lst) default
+             (let ((x (car lst)))
+               (if (f x) x
+                   (loop (cdr lst))))))))))
