1dd1d75485c5a62f64b94da9d82c0b632d53d9f2
Otto Jung <otto.jung@vauplace.com>
2022-11-13 16:11:41 +0100

    profun: op-divisible: use RFC

--- src/profun-op-divisible.scm
+++ src/profun-op-divisible.scm
@@ -17,22 +17,32 @@
 %var profun-op-divisible
 
 %use (bool->profun-result) "./bool-to-profun-result.scm"
+%use (make-profun-RFC) "./profun-RFC.scm"
 %use (profun-ctx-set profun-set) "./profun-accept.scm"
 %use (profun-op-lambda) "./profun-op-lambda.scm"
-%use (profun-bound-value?) "./profun-value.scm"
+%use (profun-bound-value? profun-unbound-value?) "./profun-value.scm"
+%use (raisu) "./raisu.scm"
 
 (define profun-op-divisible
   (profun-op-lambda
    (ctx (y x) (x-name y-name))
    (define last (or ctx 1))
-   (bool->profun-result
-    (if (profun-bound-value? x)
-        (and (not (= 0 x))
-             (= 0 (remainder y x)))
-        (and (< last y)
-             (let loop ((i last) (cnt 0))
-               (if (= 0 (remainder y i))
-                   (profun-set
-                    (y-name <- i)
-                    (profun-ctx-set (+ 1 i)))
-                   (loop (+ 1 i) cnt))))))))
+
+   (if (profun-unbound-value? y)
+       (make-profun-RFC #f `((what ,y-name)))
+       (and
+        (or (number? y)
+            (raisu 'non-number-in-divisible y-name y))
+        (bool->profun-result
+         (if (profun-bound-value? x)
+             (and (or (number? x)
+                      (raisu 'non-number-in-divisible x-name x))
+                  (not (= 0 x))
+                  (= 0 (remainder y x)))
+             (and (< last y)
+                  (let loop ((i last) (cnt 0))
+                    (if (= 0 (remainder y i))
+                        (profun-set
+                         (y-name <- i)
+                         (profun-ctx-set (+ 1 i)))
+                        (loop (+ 1 i) cnt))))))))))
