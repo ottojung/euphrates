f62293ea5d14ea53177e7629a4d287136789b6a1
Otto Jung <otto.jung@vauplace.com>
2022-11-23 18:29:47 +0100

    profun: op: add value operation

---
+++ src/profun-op-value.scm
@@ -0,0 +1,50 @@
+;;;; Copyright (C) 2022  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+%var profun-op-value
+
+%use (profun-set) "./profun-accept.scm"
+%use (profun-answer-join/and profun-answer-join/any profun-answer-join/or) "./profun-answer-join.scm"
+%use (profun-op-lambda) "./profun-op-lambda.scm"
+%use (profun-reject) "./profun-reject.scm"
+%use (raisu) "./raisu.scm"
+
+(define (profun-op-value alist)
+  (profun-op-lambda
+   (ctx (x) (x-name))
+   (let loop ((x x))
+     (cond
+      ((symbol? x)
+       (let ((val/p (assq x alist)))
+         (if val/p
+             (profun-set (x <- (cdr val/p)))
+             (profun-reject))))
+      ((and (pair? x)
+            (list? (cdr x)))
+       (let ((composer
+              (case (car x)
+                ((any) profun-answer-join/any)
+                ((or) profun-answer-join/or)
+                ((and) profun-answer-join/and)
+                (else (raisu 'uknown-value-composer (car x))))))
+         (let lp2 ((buf (cdr x))
+                   (ret (profun-reject)))
+           (if (null? buf) ret
+               (let* ((u (car buf))
+                      (y (loop u)))
+                 (lp2 (cdr buf) (composer ret y)))))))
+      (else
+       (raisu 'uknown-value-format x))))))
--- test/test-profune-communications.scm
+++ test/test-profune-communications.scm
@@ -2,17 +2,12 @@
 %run guile
 
 %use (assert=) "./src/assert-equal.scm"
-%use (debug) "./src/debug.scm"
-%use (debugv) "./src/debugv.scm"
 %use (lines->string) "./src/lines-to-string.scm"
 %use (printf) "./src/printf.scm"
-%use (profun-set) "./src/profun-accept.scm"
-%use (profun-answer-join/and profun-answer-join/any profun-answer-join/or) "./src/profun-answer-join.scm"
 %use (profun-make-handler) "./src/profun-make-handler.scm"
 %use (profun-op-divisible) "./src/profun-op-divisible.scm"
 %use (profun-op-equals) "./src/profun-op-equals.scm"
 %use (profun-op-false) "./src/profun-op-false.scm"
-%use (profun-op-lambda) "./src/profun-op-lambda.scm"
 %use (profun-op-less) "./src/profun-op-less.scm"
 %use (profun-op*) "./src/profun-op-mult.scm"
 %use (profun-op+) "./src/profun-op-plus.scm"
@@ -20,12 +15,11 @@
 %use (profun-op-sqrt) "./src/profun-op-sqrt.scm"
 %use (profun-op-true) "./src/profun-op-true.scm"
 %use (profun-op-unify) "./src/profun-op-unify.scm"
-%use (profun-reject) "./src/profun-reject.scm"
+%use (profun-op-value) "./src/profun-op-value.scm"
 %use (profun-create-database) "./src/profun.scm"
 %use (profune-communications-hook/p) "./src/profune-communications-hook-p.scm"
 %use (profune-communications) "./src/profune-communications.scm"
 %use (make-profune-communicator) "./src/profune-communicator.scm"
-%use (raisu) "./src/raisu.scm"
 %use (~a) "./src/tilda-a.scm"
 %use (words->string) "./src/words-to-string.scm"
 
@@ -55,32 +49,7 @@
    (< profun-op-less)
    (divisible profun-op-divisible)
    (equals profun-op-equals)
-   (value
-    (profun-op-lambda
-     (ctx (x) (x-name))
-     (let loop ((x x))
-       (cond
-        ((symbol? x)
-         (case x
-           ((X) (profun-set ('X <- 9)))
-           ((Y) (profun-set ('Y <- 16)))
-           (else (profun-reject))))
-        ((and (pair? x)
-              (list? (cdr x)))
-         (let ((composer
-                (case (car x)
-                  ((any) profun-answer-join/any)
-                  ((or) profun-answer-join/or)
-                  ((and) profun-answer-join/and)
-                  (else (raisu 'uknown-value-composer (car x))))))
-           (let lp2 ((buf (cdr x))
-                     (ret (profun-reject)))
-             (if (null? buf) ret
-                 (let* ((u (car buf))
-                        (y (loop u)))
-                   (lp2 (cdr buf) (composer ret y)))))))
-        (else
-         (raisu 'uknown-value-format x))))))
+   (value (profun-op-value '((X . 9) (Y . 16))))
    ))
 
 (define definitions1
