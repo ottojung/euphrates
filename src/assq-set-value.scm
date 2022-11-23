593b76ad0c6f1e023652e242059fa3827eaf0d3f
Otto Jung <otto.jung@vauplace.com>
2022-11-23 19:56:40 +0100

    add assq-set-value

---
+++ src/assq-set-value.scm
@@ -0,0 +1,13 @@
+
+%run guile
+
+%var assq-set-value
+
+(define (assq-set-value key value alist0)
+  (let loop ((alist alist0))
+    (if (null? alist)
+        `((,key . ,value))
+        (let ((x (car alist)))
+          (if (eq? key (car x))
+              (cons `(,key . ,value) (cdr alist))
+              (cons x (loop (cdr alist))))))))
