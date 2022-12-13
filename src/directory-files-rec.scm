d9ddde199443e302e431014e894ca2e03a92b541
Otto Jung <otto.jung@vauplace.com>
2022-12-13 23:23:52 +0100

    directory-files-rec: base on directory-files-depth-iter

--- src/directory-files-rec.scm
+++ src/directory-files-rec.scm
@@ -9,36 +9,22 @@
 ;;
 ;;  where dirname1 is the parent dir of the file
 
+%use (directory-files-depth-iter) "./directory-files-depth-iter.scm"
+
 %for (COMPILER "guile")
 
-(use-modules (ice-9 ftw))
+(define directory-files-rec
+  (case-lambda
+   ((directory) (directory-files-rec #f directory))
+   ((include-directories? directory)
+    (define iter
+      (directory-files-depth-iter include-directories? +inf.0 directory))
 
-(define [directory-files-rec directory]
-  ;; Don't skip anything
-  (define (enter? name stat result)
-    #t)
-
-  (define current '())
-
-  (define (leaf name stat result)
-    (cons (cons* name (basename name) current)
-          result))
-
-  (define (down name stat result)
-    (set! current (cons name current))
-    result)
-  (define (up name stat result)
-    (set! current (cdr current))
-    result)
-
-  (define (skip name stat result) result)
-
-  ;; ignore errors
-  (define (error name stat errno result) result)
-
-  (file-system-fold enter? leaf down up skip error
-                    '()
-                    directory))
+    (let loop ((buf '()))
+      (define x (iter))
+      (if x
+          (loop (cons x buf))
+          buf)))))
 
 %end
 
--- test/test-directory-files-rec.scm
+++ test/test-directory-files-rec.scm
@@ -6,5 +6,5 @@
 %use (directory-files-rec) "./src/directory-files-rec.scm"
 
 (let ()
-  (assert=HS '(("test/filetests/dir2/file1" "file1" "test/filetests/dir2" "test/filetests") ("test/filetests/dir1/dir1/zzz" "zzz" "test/filetests/dir1/dir1" "test/filetests/dir1" "test/filetests") ("test/filetests/dir1/ccc" "ccc" "test/filetests/dir1" "test/filetests") ("test/filetests/dir1/ab" "ab" "test/filetests/dir1" "test/filetests") ("test/filetests/b" "b" "test/filetests") ("test/filetests/a" "a" "test/filetests") ("test/filetests/cdefg" "cdefg" "test/filetests"))
+  (assert=HS '(("test/filetests/cdefg" "cdefg" "test/filetests") ("test/filetests/b" "b" "test/filetests") ("test/filetests/a" "a" "test/filetests") ("test/filetests/dir2/file1" "file1" "test/filetests/dir2" "test/filetests") ("test/filetests/dir1/ab" "ab" "test/filetests/dir1" "test/filetests") ("test/filetests/dir1/ccc" "ccc" "test/filetests/dir1" "test/filetests") ("test/filetests/dir1/dir1/zzz" "zzz" "test/filetests/dir1/dir1" "test/filetests/dir1" "test/filetests"))
              (directory-files-rec "test/filetests")))
