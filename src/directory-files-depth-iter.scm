f0a3655e77092c66c2d15d185df146b3a70b1161
Otto Jung <otto.jung@vauplace.com>
2022-12-13 22:55:36 +0100

    directory-files-depth-iter: add skip-directories flag

--- src/directory-files-depth-iter.scm
+++ src/directory-files-depth-iter.scm
@@ -40,68 +40,76 @@
   (stack dirlevel-stack)
   )
 
-(define (directory-files-depth-iter depth directory)
-  (define norm (path-normalize directory))
-  (define full-name0 norm)
-  (define file-name0 full-name0)
+(define directory-files-depth-iter
+  (case-lambda
+   ((depth directory) (directory-files-depth-iter #f depth directory))
+   ((skip-directories? depth directory)
+    (define norm (path-normalize directory))
+    (define full-name0 norm)
+    (define file-name0 full-name0)
 
-  (define current-stack #f)
-  (define current-prefix #f)
-  (define current-depth #f)
-  (define current-stream #f)
+    (define current-stack #f)
+    (define current-prefix #f)
+    (define current-depth #f)
+    (define current-stream #f)
 
-  (define todo-dirs (make-queue))
+    (define todo-dirs (make-queue))
 
-  (define (push-todo-dir! full-name file-name current-stack)
-    (define prev-prefix (if (null? current-stack) "" (car current-stack)))
-    (define new-prefix (append-posix-path prev-prefix file-name))
-    (define stack (cons new-prefix current-stack))
-    (define level (make-dirlevel full-name file-name stack))
-    (queue-push! todo-dirs level))
+    (define (push-todo-dir! full-name file-name current-stack)
+      (define prev-prefix (if (null? current-stack) "" (car current-stack)))
+      (define new-prefix (append-posix-path prev-prefix file-name))
+      (define stack (cons new-prefix current-stack))
+      (define level (make-dirlevel full-name file-name stack))
+      (queue-push! todo-dirs level))
 
-  (define (pop-todo-dir!)
-    (define level (queue-pop! todo-dirs))
-    (set! current-stack (dirlevel-stack level))
-    (set! current-prefix (car current-stack))
-    (set! current-depth (+ 1 (length current-stack)))
-    (set! current-stream (opendir (dirlevel-full-name level)))
-    )
+    (define (pop-todo-dir!)
+      (define level (queue-pop! todo-dirs))
+      (set! current-stack (dirlevel-stack level))
+      (set! current-prefix (car current-stack))
+      (set! current-depth (+ 1 (length current-stack)))
+      (set! current-stream (opendir (dirlevel-full-name level)))
+      )
 
-  (define (next)
-    (define file-name
-      (and current-stream (readdir current-stream)))
-    (cond
-     ((equal? #f file-name)
-      (if (queue-empty? todo-dirs)
-          #f
-          (begin
-            (pop-todo-dir!)
-            (next))))
-     ((eof-object? file-name)
-      (closedir current-stream)
-      (set! current-stream #f)
-      (if (queue-empty? todo-dirs)
-          #f
-          (begin
-            (pop-todo-dir!)
-            (next))))
-     ((or (string=? "." file-name)
-          (string=? ".." file-name))
-      (next))
-     (else
-      (let ((full-name (if (= current-depth 1)
-                           file-name
-                           (string-append current-prefix "/" file-name))))
-        (when (< current-depth depth)
-          (let ((st (false-if-exception (stat full-name))))
-            (when (and st (equal? 'directory (stat:type st)))
-              (push-todo-dir! full-name file-name current-stack))))
+    (define (next)
+      (define file-name
+        (and current-stream (readdir current-stream)))
+      (cond
+       ((equal? #f file-name)
+        (if (queue-empty? todo-dirs)
+            #f
+            (begin
+              (pop-todo-dir!)
+              (next))))
+       ((eof-object? file-name)
+        (closedir current-stream)
+        (set! current-stream #f)
+        (if (queue-empty? todo-dirs)
+            #f
+            (begin
+              (pop-todo-dir!)
+              (next))))
+       ((or (string=? "." file-name)
+            (string=? ".." file-name))
+        (next))
+       (else
+        (let* ((full-name (if (= current-depth 1)
+                              file-name
+                              (string-append current-prefix "/" file-name)))
+               (recurse? (< current-depth depth))
+               (dir? (and (or skip-directories? recurse?)
+                          (let ((st (false-if-exception (stat full-name))))
+                            (and st (equal? 'directory (stat:type st)))))))
 
-        (cons full-name (cons file-name current-stack))))))
+          (when dir?
+            (push-todo-dir! full-name file-name current-stack))
 
-  (push-todo-dir! full-name0 file-name0 '())
+          (if (and dir? skip-directories?)
+              (next)
+              (cons full-name (cons file-name current-stack)))))))
 
-  next
-  )
+    (push-todo-dir! full-name0 file-name0 '())
+
+    next
+    )))
 
 %end
--- test/test-directory-files-depth-iter.scm
+++ test/test-directory-files-depth-iter.scm
@@ -14,3 +14,14 @@
 
 (assert=HS '(("test/filetests/cdefg" "cdefg" "test/filetests") ("test/filetests/dir2" "dir2" "test/filetests") ("test/filetests/dir1" "dir1" "test/filetests") ("test/filetests/b" "b" "test/filetests") ("test/filetests/a" "a" "test/filetests") ("test/filetests/dir2/file1" "file1" "test/filetests/dir2" "test/filetests") ("test/filetests/dir1/ab" "ab" "test/filetests/dir1" "test/filetests") ("test/filetests/dir1/ccc" "ccc" "test/filetests/dir1" "test/filetests") ("test/filetests/dir1/dir1" "dir1" "test/filetests/dir1" "test/filetests") ("test/filetests/dir1/dir1/zzz" "zzz" "test/filetests/dir1/dir1" "test/filetests/dir1" "test/filetests"))
            collect)
+
+(define iter/no-dirs
+  (directory-files-depth-iter #t 9 "test/filetests"))
+
+(define collect/no-dirs
+  (let loop ()
+    (define x (iter/no-dirs))
+    (if x (cons x (loop)) '())))
+
+(assert=HS '(("test/filetests/cdefg" "cdefg" "test/filetests") ("test/filetests/b" "b" "test/filetests") ("test/filetests/a" "a" "test/filetests") ("test/filetests/dir2/file1" "file1" "test/filetests/dir2" "test/filetests") ("test/filetests/dir1/ab" "ab" "test/filetests/dir1" "test/filetests") ("test/filetests/dir1/ccc" "ccc" "test/filetests/dir1" "test/filetests") ("test/filetests/dir1/dir1/zzz" "zzz" "test/filetests/dir1/dir1" "test/filetests/dir1" "test/filetests"))
+           collect/no-dirs)
