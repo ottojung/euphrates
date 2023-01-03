28689e534af2e98e2e6c4de9032d9dadd082203a
Otto Jung <otto.jung@vauplace.com>
2023-01-03 16:09:31 +0100

    add assq-set-default

---
+++ src/assq-set-default.scm
@@ -0,0 +1,28 @@
+;;;; Copyright (C) 2023  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+%var assq-set-default
+
+%use (assq-set-value) "./assq-set-value.scm"
+
+(define-syntax assq-set-default
+  (syntax-rules ()
+    ((_ key/0 value alist/0)
+     (let ()
+       (define key key/0)
+       (define alist alist/0)
+       (define got (assq key alist))
+       (if got alist (assq-set-value key value alist))))))
