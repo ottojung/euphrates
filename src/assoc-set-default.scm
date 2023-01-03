0451c0b5c19abee659840ef62458355d56d43f27
Otto Jung <otto.jung@vauplace.com>
2023-01-03 16:07:52 +0100

    assoc-set-default: dont evaluate default

--- src/assoc-set-default.scm
+++ src/assoc-set-default.scm
@@ -5,7 +5,12 @@
 
 %use (assoc-set-value) "./assoc-set-value.scm"
 
-(define (assoc-set-default key value alist)
-  (define got (assoc key alist))
-  (define current (and got (cdr got)))
-  (if current alist (assoc-set-value key value alist)))
+(define-syntax assoc-set-default
+  (syntax-rules ()
+    ((_ key/0 value alist/0)
+     (let ()
+       (define key key/0)
+       (define alist alist/0)
+       (define got (assoc key alist))
+       (define current (and got (cdr got)))
+       (if current alist (assoc-set-value key value alist))))))
