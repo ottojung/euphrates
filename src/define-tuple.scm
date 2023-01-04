8fe1a7194d843c30c6e962b17ed76a32deda2732
Otto Jung <otto.jung@vauplace.com>
2023-01-04 13:53:34 +0100

    define-tuple: handler zero arguments case

--- src/define-tuple.scm
+++ src/define-tuple.scm
@@ -5,6 +5,8 @@
 
 (define-syntax define-tuple
   (syntax-rules ()
+    ((_ () value)
+     (begin value (values)))
     ((_ (v . vs) value)
      (define-values (v . vs)
        (apply values value)))
