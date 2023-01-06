8bb2644178f7ede68e2e86b596c2c7df597ce610
Otto Jung <otto.jung@vauplace.com>
2023-01-06 14:10:33 +0100

    add url-get-query

---
+++ src/url-get-query.scm
@@ -0,0 +1,23 @@
+;;;; Copyright (C) 2023  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+%var url-get-query
+
+%use (url-decompose) "./url-decompose.scm"
+
+(define (url-get-query url)
+  (define v (url-decompose url))
+  (vector-ref v 3))
