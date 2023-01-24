0ebb9ab5c2e7f083366a384cc7a6d77fd9319a94
Otto Jung <otto.jung@vauplace.com>
2023-01-24 15:33:49 +0100

    add syntax-map/noeval

---
+++ src/syntax-map-noeval.scm
@@ -0,0 +1,17 @@
+
+%run guile
+
+%var syntax-map/noeval
+
+%use (syntax-reverse) "./syntax-reverse.scm"
+
+(define-syntax syntax-map/noeval/buf
+  (syntax-rules ()
+    ((_ cont buf fun) (syntax-reverse cont buf))
+    ((_ cont buf (fun ctxarg) x . xs) (syntax-map/noeval/buf cont ((fun ctxarg x) . buf) (fun ctxarg) . xs))
+    ((_ cont buf fun x . xs) (syntax-map/noeval/buf cont ((fun x) . buf) fun . xs))))
+
+(define-syntax syntax-map/noeval
+  (syntax-rules ()
+    ((_ cont fun args-to-map)
+     (syntax-map/noeval/buf cont () fun . args-to-map))))
