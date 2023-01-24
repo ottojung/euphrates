11c6342649c2b6f5e819d5171c4360234311411e
Otto Jung <otto.jung@vauplace.com>
2023-01-24 14:20:55 +0100

    syntax-append: support multiple args

--- src/syntax-append.scm
+++ src/syntax-append.scm
@@ -13,6 +13,13 @@
     ((_ cont buf as (b . bs))
      (syntax-append/buf cont (b . buf) as bs))))
 
+(define-syntax syntax-append/cont
+  (syntax-rules ()
+    ((_ (cont a) b)
+     (syntax-append cont a b))))
+
 (define-syntax syntax-append
   (syntax-rules ()
-    ((_ cont a b) (syntax-append/buf cont () a b))))
+    ((_ cont a b) (syntax-append/buf cont () a b))
+    ((_ cont a b . bs)
+     (syntax-append (syntax-append/cont (cont a)) b . bs))))
--- test/test-syntax-append.scm
+++ test/test-syntax-append.scm
@@ -8,5 +8,12 @@
   (define-syntax cont
     (syntax-rules () ((_ arg buf) (list arg . buf))))
 
-  (assert= (list 'arg 2 3 4 5 6 7)
-           (syntax-append (cont 'arg) (2 3) (4 5 6 7))))
+  (assert= (list 'arg 1 2 3 4 5 6 7 8 9)
+           (syntax-append (cont 'arg) (1 2 3) (4 5 6 7) (8 9))))
+
+(let ()
+  (define-syntax cont
+    (syntax-rules () ((_ arg buf) (list arg . buf))))
+
+  (assert= (list 'arg 1 2 3 4 5 6 7 8 9)
+           (syntax-append (cont 'arg) (1 2 3) (4 5) (6 7) (8 9))))
