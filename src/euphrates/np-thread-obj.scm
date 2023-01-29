8c3eac995aa49093457591012e3eb1fd11bf61b8
Otto Jung <otto.jung@vauplace.com>
2023-01-29 20:41:22 +0100

    np-thread-obj: fix exports

--- src/euphrates/np-thread-obj.scm
+++ src/euphrates/np-thread-obj.scm
@@ -2,7 +2,7 @@
 (cond-expand
  (guile
   (define-module (euphrates np-thread-obj)
-    :export (np-thread-obj np-thread-obj? np-thread-obj-continuation set-np-thread-obj-continuation! np-thread-obj-cancel-scheduled? set-np-thread-obj-cancel-scheduled?! np-thread-obj-cancel-enabled?)
+    :export (np-thread-obj np-thread-obj? np-thread-obj-continuation set-np-thread-obj-continuation! np-thread-obj-cancel-scheduled? set-np-thread-obj-cancel-scheduled?! np-thread-obj-cancel-enabled? set-np-thread-obj-cancel-enabled?!)
     :use-module ((euphrates define-type9) :select (define-type9)))))
 
 
