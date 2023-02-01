f0f743167f87860670322515680aeab2d4ec1b4d
Otto Jung <otto.jung@vauplace.com>
2023-02-01 11:20:19 +0100

    add run-syncproc/re*

---
+++ src/euphrates/run-syncproc-re-star.scm
@@ -0,0 +1,31 @@
+;;;; Copyright (C) 2023  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(cond-expand
+ (guile
+  (define-module (euphrates run-syncproc-re-star)
+    :export (run-syncproc/re*)
+    :use-module ((euphrates asyncproc) :select (asyncproc-status))
+    :use-module ((euphrates run-syncproc) :select (run-syncproc))
+    )))
+
+(define (run-syncproc/re* command . args)
+  (define status #f)
+  (define text
+    (with-output-to-string
+      (lambda _
+        (define p
+          (apply run-syncproc (cons command args)))
+        (set! status (asyncproc-status p)))))
+  (values text status))
