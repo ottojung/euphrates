beafec34642790e5f5c45be6415c45cca3f02312
Otto Jung <otto.jung@vauplace.com>
2023-02-12 02:47:55 +0100

    make: fix syncproc test

--- test/test-run-syncproc.scm
+++ test/test-run-syncproc.scm
@@ -5,22 +5,22 @@
     :use-module ((euphrates assert-equal) :select (assert=))
     :use-module ((euphrates asyncproc-input-text-p) :select (asyncproc-input-text/p))
     :use-module ((euphrates asyncproc) :select (asyncproc-status))
-    :use-module ((euphrates run-syncproc) :select (run-syncproc))
+    :use-module ((euphrates run-syncproc-star) :select (run-syncproc*))
     )))
 
 (let ()
-  (define p (run-syncproc "true"))
+  (define p (run-syncproc* "true"))
   (assert= 0 (asyncproc-status p)))
 
 (let ()
-  (define p (run-syncproc "false"))
+  (define p (run-syncproc* "false"))
   (assert= 1 (asyncproc-status p)))
 
 (let ()
   (define s
     (with-output-to-string
       (lambda _
-        (define p (run-syncproc "echo" "hello" "there euphrates testcase"))
+        (define p (run-syncproc* "echo" "hello" "there euphrates testcase"))
         (assert= 0 (asyncproc-status p)))))
   (assert= s "hello there euphrates testcase\n"))
 
@@ -30,6 +30,6 @@
       (lambda _
         (define p
           (parameterize ((asyncproc-input-text/p "hello there"))
-            (run-syncproc "cat")))
+            (run-syncproc* "cat")))
         (assert= 0 (asyncproc-status p)))))
   (assert= s "hello there"))
