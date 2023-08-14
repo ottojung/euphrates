ab220a8f5dbc0da9822e1c8628b6e632f8bab1b9
Otto Jung <otto.jung@vauplace.com>
2023-08-13 22:07:58 -0700

    test: add lalr-parser-standardcases test

---
+++ test/test-lalr-parser-standardcases.scm
@@ -0,0 +1,101 @@
+
+(define hidden-imports
+  '(glr-driver lr-driver
+               vector vector-ref let* list-ref
+               lexical-token?
+               force-output
+               lexical-token-value
+               lexical-token-source))
+
+(define (make-lexer)
+  (lambda ()
+    (letrec ((read-number
+              (lambda (l)
+                (let ((c (peek-char)))
+                  (if (and (char? c) (char-numeric? c))
+                      (read-number (cons (read-char) l))
+                      (string->number (apply string (reverse l)))))))
+             (read-id
+              (lambda (l)
+                (let ((c (peek-char)))
+                  (if (and (char? c) (char-alphabetic? c))
+                      (read-id (cons (read-char) l))
+                      (apply string (reverse l)))))))
+
+      ;; -- read the next token
+      (let loop ()
+        (let* ((location (make-source-location "*stdin*" 'unknown-line 'unknown-column -1 -1))
+               (c (read-char)))
+          (cond ((eof-object? c)      '*eoi*)
+                ((char=? c #\newline) (make-lexical-token 'NEWLINE location 'NEWLINE))
+                ((char-whitespace? c) (make-lexical-token 'SPACE   location 'SPACE))
+                ((char=? c #\+)       (make-lexical-token '+       location (string c)))
+                ((char=? c #\-)       (make-lexical-token '-       location (string c)))
+                ((char=? c #\*)       (make-lexical-token '*       location (string c)))
+                ((char=? c #\/)       (make-lexical-token '/       location (string c)))
+                ((char=? c #\=)       (make-lexical-token '=       location (string c)))
+                ((char=? c #\,)       (make-lexical-token 'COMMA   location (string c)))
+                ((char=? c #\()       (make-lexical-token 'LPAREN  location (string c)))
+                ((char=? c #\))       (make-lexical-token 'RPAREN  location (string c)))
+                ((char-numeric? c)    (make-lexical-token 'NUM     location (read-number (list c))))
+                ((char-alphabetic? c) (make-lexical-token 'ID      location (read-id (list c))))
+                (else
+                 (raisu
+                  'lex-error
+                  (stringf "PARSE ERROR : illegal character: ~s" c)
+                  c)
+                 (loop))))))))
+
+(define (error-procedure . args)
+  (raisu
+   'parse-error
+   (stringf "PARSE ERROR : ~s" args)
+   args))
+
+(define id-list-parser
+  (lalr-parser
+   ;; --- tokens
+   (ID NUM = + - * / LPAREN RPAREN SPACE NEWLINE COMMA)
+
+   ;; --- rules
+   (exprs    (exprs expr) : (list 'exprs $1 $2)
+             (expr) : (list 'exprs $1))
+   (expr     (SPACE) : (list 'expr $1)
+             (ID) : (list 'expr $1))))
+
+
+(define multiline-id-list-parser
+  (lalr-parser
+
+   (expect: 3)
+
+   ;; --- tokens
+   (ID NUM = + - * / LPAREN RPAREN SPACE NEWLINE COMMA)
+
+   ;; --- rules
+   (lines    (line lines) : (list 'lines $1 $2)
+             (line)       : (list 'lines $1)
+             (exprs)      : (list 'lines $1))
+   (line     (exprs line) : (list 'line $1 $2)
+             (NEWLINE)    : (list 'line $1))
+   (exprs    (exprs expr) : (list 'exprs $1 $2)
+             (expr) : (list 'exprs $1))
+   (expr     (SPACE) : (list 'expr $1)
+             (ID) : (list 'expr $1))))
+
+(define (run-input parser input)
+  (with-string-as-input
+   input (parser (make-lexer) error-procedure)))
+
+(assert=
+ '(exprs (exprs (exprs (exprs (exprs (exprs (exprs (expr "ida")) (expr SPACE)) (expr "idb")) (expr SPACE)) (expr "idc")) (expr SPACE)) (expr "longerid"))
+ (run-input
+  id-list-parser
+  "ida idb idc longerid"))
+
+(assert=
+ '(lines (line (exprs (exprs (exprs (exprs (exprs (exprs (exprs (expr "idala")) (expr SPACE)) (expr "idbla")) (expr SPACE)) (expr "idcla")) (expr SPACE)) (expr "longeridla")) (line NEWLINE)) (lines (exprs (exprs (exprs (exprs (exprs (expr "idblb")) (expr SPACE)) (expr "idclb")) (expr SPACE)) (expr "longeridlb"))))
+ (run-input
+  multiline-id-list-parser
+  "idala idbla idcla longeridla
+idblb idclb longeridlb"))
---
+++ test/test-lalr-parser-standardcases.sld
@@ -0,0 +1,62 @@
+
+(define-library
+  (test-lalr-parser-standardcases)
+  (import (only (euphrates assert-equal) assert=))
+  (import
+    (only (euphrates lalr-parser)
+          glr-driver
+          lalr-parser
+          lexical-token-source
+          lexical-token-value
+          lexical-token?
+          lr-driver
+          make-lexical-token
+          make-source-location))
+  (import (only (euphrates raisu) raisu))
+  (import (only (euphrates stringf) stringf))
+  (import
+    (only (euphrates with-string-as-input)
+          with-string-as-input))
+  (import
+    (only (scheme base)
+          *
+          +
+          -
+          /
+          =
+          and
+          apply
+          begin
+          char=?
+          char?
+          cond
+          cons
+          define
+          else
+          eof-object?
+          if
+          lambda
+          let
+          let*
+          letrec
+          list
+          list-ref
+          peek-char
+          quote
+          read-char
+          reverse
+          string
+          string->number
+          vector
+          vector-ref))
+  (import
+    (only (scheme char)
+          char-alphabetic?
+          char-numeric?
+          char-whitespace?))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin
+             (include-from-path
+               "test-lalr-parser-standardcases.scm")))
+    (else (include "test-lalr-parser-standardcases.scm"))))
