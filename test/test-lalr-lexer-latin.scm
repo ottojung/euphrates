0696a3767a52d93a5c8c99230b35d31a6cba53c6
Otto Jung <otto.jung@vauplace.com>
2023-08-15 20:34:50 -0700

    test: add cases for lalr-lexer/latin

---
+++ test/test-lalr-lexer-latin.scm
@@ -0,0 +1,274 @@
+
+(define make-lexer make-lalr-lexer/latin)
+
+(define (error-procedure type/subtype message-fmt token)
+  (raisu* :type (car type/subtype)
+          :message (stringf message-fmt token)
+          :args (list (cdr type/subtype) token)))
+
+(define (make-test-parser parser-rules)
+   (lalr-parser
+    `(:tokens ,lalr-lexr/latin-tokens
+      :rules ,parser-rules)))
+
+(define (test-parser parser-rules input expected-output)
+  (define parser
+    (make-test-parser parser-rules))
+
+  (define result
+    (run-input parser input))
+
+  (assert= result expected-output))
+
+(define (test-parser-error parser-rules input)
+  (define parser
+    (make-test-parser parser-rules))
+
+  (assert-throw
+   'parse-error
+   (run-input parser input)))
+
+(define (run-input parser input)
+  (with-string-as-input
+   input (parser (make-lexer) error-procedure)))
+
+(define save list)
+
+;;;;;;;;;;;;;;;;
+;; TEST CASES ;;
+;;;;;;;;;;;;;;;;
+
+
+
+
+(test-parser
+ `((expr     (term add expr) : (,save 'expr $1 $2 $3)
+             (term) : (,save 'expr $1))
+   (add      (<+) : (,save 'add $1))
+   (term     (num) : (,save 'term $1))
+   (num      (dig num) : (,save 'num $1 $2)
+             (dig) : (,save 'num $1))
+   (dig      (<0) : $1 (<1) : $1 (<2) : $1 (<3) : $1 (<4) : $1
+             (<5) : $1 (<6) : $1 (<7) : $1 (<8) : $1 (<9) : $1))
+
+ "5+3"
+
+ '(expr (term (num #\5)) (add #\+) (expr (term (num #\3)))))
+
+
+
+
+
+(test-parser
+ `((expr     (term add expr) : (,save 'expr $1 $2 $3)
+             (term) : (,save 'expr $1))
+   (add      (<+) : (,save 'add $1))
+   (term     (num) : (,save 'term $1))
+   (num      (dig num) : (,save 'num $1 $2)
+             (dig) : (,save 'num $1))
+   (dig      ,@lalr-lexer/latin/digits))
+
+ "5+3"
+
+ '(expr (term (num #\5)) (add #\+) (expr (term (num #\3)))))
+
+
+
+
+
+(test-parser
+ `((expr     (term add expr) : (,save 'expr $1 $2 $3)
+             (term) : (,save 'expr $1))
+   (add      (<+) : (,save 'add $1))
+   (term     (num) : (,save 'term $1))
+   (num      (dig num) : (,save 'num $1 $2)
+             (dig) : (,save 'num $1))
+   (dig      ,@lalr-lexer/latin/digits))
+
+ "47+3732"
+
+ '(expr (term (num #\4 (num #\7))) (add #\+) (expr (term (num #\3 (num #\7 (num #\3 (num #\2))))))))
+
+
+
+
+(test-parser
+ `((expr     (term add expr) : (,save 'expr $1 $2 $3)
+             (term) : (,save 'expr $1))
+   (add      (<+) : (,save 'add $1))
+   (term     (num) : (,save 'term $1)
+             (id) : (,save 'term $1))
+   (num      (dig num) : (,save 'num $1 $2)
+             (dig) : (,save 'num $1))
+   (id       (alpha idcont) : (,save 'id $1 $2))
+   (idcont   (idcont alphanum) : (,save 'idcont $1 $2)
+             (alphanum) : (,save 'idcont $1))
+   (alphanum (alpha) : $1
+             (dig) : $1)
+   (alpha    ,@lalr-lexer/latin/letters)
+   (dig      ,@lalr-lexer/latin/digits))
+
+ "47+3732"
+
+ '(expr (term (num #\4 (num #\7))) (add #\+) (expr (term (num #\3 (num #\7 (num #\3 (num #\2))))))))
+
+
+
+
+
+
+(test-parser
+ `((expr     (term add expr) : (,save 'expr $1 $2 $3)
+             (term) : (,save 'expr $1))
+   (add      (<+) : (,save 'add $1))
+   (term     (num) : (,save 'term $1)
+             (id) : (,save 'term $1))
+   (num      (dig num) : (,save 'num $1 $2)
+             (dig) : (,save 'num $1))
+   (id       (alpha idcont) : (,save 'id $1 $2))
+   (idcont   (idcont alphanum) : (,save 'idcont $1 $2)
+             (alphanum) : (,save 'idcont $1))
+   (alphanum (alpha) : $1
+             (dig) : $1)
+   (alpha    ,@lalr-lexer/latin/letters)
+   (dig      ,@lalr-lexer/latin/digits))
+
+ "47+x3"
+
+ '(expr (term (num #\4 (num #\7))) (add #\+) (expr (term (id #\x (idcont #\3))))))
+
+
+
+
+(test-parser
+ `((expr     (term add expr) : (,save 'expr $1 $2 $3)
+             (term) : (,save 'expr $1))
+   (add      (<+) : (,save 'add $1))
+   (term     (num) : (,save 'term $1)
+             (id) : (,save 'term $1))
+   (num      (dig num) : (,save 'num $1 $2)
+             (dig) : (,save 'num $1))
+   (id       (alpha idcont) : (,save 'id $1 $2))
+   (idcont   (idcont alphanum) : (,save 'idcont $1 $2)
+             (alphanum) : (,save 'idcont $1))
+   (alphanum (alpha) : $1
+             (dig) : $1)
+   (alpha    ,@lalr-lexer/latin/letters)
+   (dig      ,@lalr-lexer/latin/digits)
+   (space    (<SPACE) : $1
+             (<TAB) : $1))
+
+ "47+x3"
+
+ '(expr (term (num #\4 (num #\7))) (add #\+) (expr (term (id #\x (idcont #\3))))))
+
+
+
+
+(test-parser
+ `((expr     (term add expr) : (,save 'expr $1 $2 $3)
+             (term) : (,save 'expr $1))
+   (add      (<+) : (,save 'add $1))
+   (term     (num) : (,save 'term $1)
+             (id) : (,save 'term $1)
+             (space term) : (,save 'term $1 $2)
+             (term space) : (,save 'term $1 $2))
+   (num      (dig num) : (,save 'num $1 $2)
+             (dig) : (,save 'num $1))
+   (id       (alpha idcont) : (,save 'id $1 $2))
+   (idcont   (idcont alphanum) : (,save 'idcont $1 $2)
+             (alphanum) : (,save 'idcont $1))
+   (alphanum (alpha) : $1
+             (dig) : $1)
+   (alpha    ,@lalr-lexer/latin/letters)
+   (dig      ,@lalr-lexer/latin/digits)
+   (space    (<SPACE) : $1
+             (<TAB) : $1))
+
+ "47+x3"
+
+ '(expr (term (num #\4 (num #\7))) (add #\+) (expr (term (id #\x (idcont #\3))))))
+
+
+
+
+
+(test-parser
+ `((expr     (term add expr) : (,save 'expr $1 $2 $3)
+             (term) : (,save 'expr $1))
+   (add      (<+) : (,save 'add $1))
+   (term     (num) : (,save 'term $1)
+             (id) : (,save 'term $1)
+             (space term) : (,save 'term $1 $2)
+             (term space) : (,save 'term $1 $2))
+   (num      (dig num) : (,save 'num $1 $2)
+             (dig) : (,save 'num $1))
+   (id       (alpha idcont) : (,save 'id $1 $2))
+   (idcont   (idcont alphanum) : (,save 'idcont $1 $2)
+             (alphanum) : (,save 'idcont $1))
+   (alphanum (alpha) : $1
+             (dig) : $1)
+   (alpha    ,@lalr-lexer/latin/letters)
+   (dig      ,@lalr-lexer/latin/digits)
+   (space    (<SPACE) : $1
+             (<TAB) : $1))
+
+ "47 + x3"
+
+ '(expr (term (term (num #\4 (num #\7))) #\space) (add #\+) (expr (term #\space (term (id #\x (idcont #\3)))))))
+
+
+
+
+(test-parser
+ `((expr     (term add expr) : (,save 'expr $1 $2 $3)
+             (term) : (,save 'expr $1))
+   (add      (<+) : (,save 'add $1))
+   (term     (num) : (,save 'term $1)
+             (id) : (,save 'term $1)
+             (space term) : (,save 'term $1 $2)
+             (term space) : (,save 'term $1 $2))
+   (num      (dig num) : (,save 'num $1 $2)
+             (dig) : (,save 'num $1))
+   (id       (alpha idcont) : (,save 'id $1 $2))
+   (idcont   (idcont alphanum) : (,save 'idcont $1 $2)
+             (alphanum) : (,save 'idcont $1))
+   (alphanum (alpha) : $1
+             (dig) : $1)
+   (alpha    ,@lalr-lexer/latin/letters)
+   (dig      ,@lalr-lexer/latin/digits)
+   (space    (<SPACE) : $1
+             (<TAB) : $1))
+
+ "   47 +x3   "
+
+ '(expr (term #\space (term #\space (term #\space (term (term (num #\4 (num #\7))) #\space)))) (add #\+) (expr (term (term (term (term (id #\x (idcont #\3))) #\space) #\space) #\space))))
+
+
+
+
+(test-parser
+ `((expr     (term add expr) : (,save 'expr $1 $2 $3)
+             (term) : (,save 'expr $1))
+   (add      (<+) : (,save 'add $1))
+   (term     (num) : (,save 'term $1)
+             (id) : (,save 'term $1)
+             (space* term space*) : (,save 'term $1 $2))
+   (num      (dig num) : (,save 'num $1 $2)
+             (dig) : (,save 'num $1))
+   (id       (alpha idcont) : (,save 'id $1 $2))
+   (idcont   (idcont alphanum) : (,save 'idcont $1 $2)
+             (alphanum) : (,save 'idcont $1))
+   (alphanum (alpha) : $1
+             (dig) : $1)
+   (alpha    ,@lalr-lexer/latin/letters)
+   (dig      ,@lalr-lexer/latin/digits)
+   (space*   (space space*) : $1 ())
+   (space    (<SPACE) : $1
+             (<TAB) : $1))
+
+ "   47 +  x3   "
+
+ '(expr (term #\space (term (num #\4 (num #\7)))) (add #\+) (expr (term #\space (term (id #\x (idcont #\3)))))))
+
+
---
+++ test/test-lalr-lexer-latin.sld
@@ -0,0 +1,41 @@
+
+(define-library
+  (test-lalr-lexer-latin)
+  (import (only (euphrates assert-equal) assert=))
+  (import
+    (only (euphrates assert-throw) assert-throw))
+  (import
+    (only (euphrates lalr-lexer-latin-digits)
+          lalr-lexer/latin/digits))
+  (import
+    (only (euphrates lalr-lexer-latin-letters)
+          lalr-lexer/latin/letters))
+  (import
+    (only (euphrates lalr-lexer-latin)
+          make-lalr-lexer/latin))
+  (import
+    (only (euphrates lalr-lexr-latin-tokens)
+          lalr-lexr/latin-tokens))
+  (import
+    (only (euphrates lalr-parser) lalr-parser))
+  (import (only (euphrates raisu-star) raisu*))
+  (import (only (euphrates stringf) stringf))
+  (import
+    (only (euphrates with-string-as-input)
+          with-string-as-input))
+  (import
+    (only (scheme base)
+          begin
+          car
+          cdr
+          define
+          list
+          quasiquote
+          quote
+          unquote
+          unquote-splicing))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin
+             (include-from-path "test-lalr-lexer-latin.scm")))
+    (else (include "test-lalr-lexer-latin.scm"))))
