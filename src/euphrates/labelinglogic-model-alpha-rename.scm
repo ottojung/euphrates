1b290b2fff79bbd076c87d09bf0465a949f198af
Otto Jung <otto.jung@vauplace.com>
2023-10-16 22:37:45 -0700

    labelinglogic:model:alpha-rename: exclude model names

--- src/euphrates/labelinglogic-model-alpha-rename.scm
+++ src/euphrates/labelinglogic-model-alpha-rename.scm
@@ -2,8 +2,18 @@
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define (labelinglogic:model:alpha-rename taken-names-set model)
+  (define all-taken-names
+    (append
+     (map (lambda (model-component)
+            (define-tuple (class predicate) model-component)
+            class)
+          model)
+     (if (hashset? taken-names-set)
+         (hashset->list taken-names-set)
+         taken-names-set)))
+
   (with-unique-identifier-context
-   :existing-names taken-names-set
+   :existing-names all-taken-names
 
    (labelinglogic:model:replace-constants
     model
--- src/euphrates/labelinglogic-model-alpha-rename.sld
+++ src/euphrates/labelinglogic-model-alpha-rename.sld
@@ -3,6 +3,10 @@
   (euphrates labelinglogic-model-alpha-rename)
   (export labelinglogic:model:alpha-rename)
   (import
+    (only (euphrates define-tuple) define-tuple))
+  (import
+    (only (euphrates hashset) hashset->list hashset?))
+  (import
     (only (euphrates labelinglogic-model-replace-constants)
           labelinglogic:model:replace-constants))
   (import
@@ -13,7 +17,13 @@
           unique-identifier?
           with-unique-identifier-context))
   (import
-    (only (scheme base) begin define if lambda))
+    (only (scheme base)
+          append
+          begin
+          define
+          if
+          lambda
+          map))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
