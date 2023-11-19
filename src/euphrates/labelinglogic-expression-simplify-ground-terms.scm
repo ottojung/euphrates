b14037d5182f4eb4854d18eb5562b421447eeada
Otto Jung <otto.jung@vauplace.com>
2023-11-19 12:10:23 -0800

    save

--- src/euphrates/labelinglogic-expression-simplify-ground-terms.scm
+++ src/euphrates/labelinglogic-expression-simplify-ground-terms.scm
@@ -2,6 +2,7 @@
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define (labelinglogic:expression:simplify-ground-terms expr)
+  (define expr0 expr)
   (define type (labelinglogic:expression:type expr))
   (define args (labelinglogic:expression:args expr))
 
@@ -21,14 +22,14 @@
               :type 'not-a-ground-term
               :message (stringf "Expression type ~s is not a ground term"
                                 (~a type))
-              :args (list type expr)))
+              :args (list type expr expr0)))
 
      (else
       (raisu* :from "labelinglogic:expression:simplify-ground-terms"
               :type 'unknown-expr-type
               :message (stringf "Expression type ~s not recognized"
                                 (~a type))
-              :args (list type expr)))))
+              :args (list type expr expr0)))))
 
   (cond
    ((equal? type 'and)
