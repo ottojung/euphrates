ee907c4b7efeb03b8b03f38da703102a0893c088
Otto Jung <otto.jung@vauplace.com>
2023-11-19 12:09:07 -0800

    save

--- src/euphrates/labelinglogic-expression-simplify-ground-terms.scm
+++ src/euphrates/labelinglogic-expression-simplify-ground-terms.scm
@@ -2,6 +2,30 @@
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define (labelinglogic:expression:simplify-ground-terms expr)
+  (define type (labelinglogic:expression:type expr))
+  (define args (labelinglogic:expression:args expr))
+
+  (define (ensure-ground expr)
+    (define type (labelinglogic:expression:type expr))
+    (define args (labelinglogic:expression:args expr))
+
+    (cond
+     ((equal? type (list 'and 'or))
+      0)
+
+     ((member type (list 'tuple 'not))
+      (for-each ensure-ground args))
+
+     ((member type (list '= 'r7rs 'constant 'not 'tuple))
+      expr)
+
+     (else
+      (raisu* :from "labelinglogic:expression:simplify-ground-terms"
+              :type 'unknown-expr-type
+              :message (stringf "Expression type ~s not recognized"
+                                (~a type))
+              :args (list type expr)))))
+
   (cond
    ((equal? type 'and)
     (let ()
@@ -16,14 +40,14 @@
 
       0))
 
-   ((equal? type 'tuple)
-    (cons 'tuple (map loop args)))
+   ((member type (list 'tuple 'not))
+    (for-each ensure-ground args))
 
    ((member type (list '= 'r7rs 'constant 'not 'tuple))
     expr)
 
    (else
-    (raisu* :from "labelinglogic:expression->mod2-expression"
+    (raisu* :from "labelinglogic:expression:simplify-ground-terms"
             :type 'unknown-expr-type
             :message (stringf "Expression type ~s not recognized"
                               (~a type))
