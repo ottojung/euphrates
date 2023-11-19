b1f7c501e7ff0b19396c40379e08e0643ff34562
Otto Jung <otto.jung@vauplace.com>
2023-11-19 12:06:46 -0800

    save

--- src/euphrates/labelinglogic-expression-simplify-ground-terms.scm
+++ src/euphrates/labelinglogic-expression-simplify-ground-terms.scm
@@ -1,9 +1,36 @@
 ;;;; Copyright (C) 2023  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define (labelinglogic:expression:simplify-ground-terms
-         A-expr B-expr)
+(define (labelinglogic:expression:simplify-ground-terms expr)
+  (cond
+   ((equal? type 'and)
+    (let ()
+      (define rec (map loop args))
+      `(* ,@rec)))
 
+   ((equal? type 'not)
+    (let ()
+      (define-tuple (arg) args)
+      (define rec (loop arg))
+      `(+ 1 ,rec)))
 
-  0)
+   ((equal? type 'or)
+    (let ()
+      (define-tuple (A B) args)
 
+      
+
+      0))
+
+   ((equal? type 'tuple)
+    (cons 'tuple (map loop args)))
+
+   ((member type (list '= 'r7rs 'constant))
+    expr)
+
+   (else
+    (raisu* :from "labelinglogic:expression->mod2-expression"
+            :type 'unknown-expr-type
+            :message (stringf "Expression type ~s not recognized"
+                              (~a type))
+            :args (list type expr)))))
--- src/euphrates/labelinglogic-expression-simplify-ground-terms.sld
+++ src/euphrates/labelinglogic-expression-simplify-ground-terms.sld
@@ -4,7 +4,36 @@
     labelinglogic-expression-simplify-ground-terms)
   (export
     labelinglogic:expression:simplify-ground-terms)
-  (import (only (scheme base) begin define))
+  (import
+    (only (euphrates define-tuple) define-tuple))
+  (import (only (euphrates raisu-star) raisu*))
+  (import (only (euphrates stringf) stringf))
+  (import (only (euphrates tilda-a) ~a))
+  (import
+    (only (scheme base)
+          *
+          +
+          =
+          and
+          begin
+          cond
+          cons
+          define
+          else
+          equal?
+          let
+          list
+          map
+          member
+          not
+          or
+          quasiquote
+          quote
+          unquote
+          unquote-splicing))
+  (cond-expand
+    (guile (import (only (srfi srfi-31) rec)))
+    (else (import (only (srfi 31) rec))))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
