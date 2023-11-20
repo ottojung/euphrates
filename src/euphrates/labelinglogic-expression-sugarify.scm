0daba5d4c988e561d657d8809583335f1bb96a31
Otto Jung <otto.jung@vauplace.com>
2023-11-19 16:46:58 -0800

    save

--- src/euphrates/labelinglogic-expression-sugarify.scm
+++ src/euphrates/labelinglogic-expression-sugarify.scm
@@ -2,6 +2,7 @@
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define (labelinglogic:expression:sugarify/functor expr)
+  (debugs (list "FUNCTOR" expr))
   (define type0 (labelinglogic:expression:type expr))
   (define stack (stack-make))
 
@@ -15,6 +16,8 @@
 
   (define linearized
     (reverse (stack->list stack)))
+
+  (debugs linearized)
 
   (labelinglogic:expression:make
    type0 linearized))
