db600fdd0f11d64abefbe3f8aaf715db9c718b98
Otto Jung <otto.jung@vauplace.com>
2023-11-20 20:56:01 -0800

    save

--- test/test-labelinglogic.scm
+++ test/test-labelinglogic.scm
@@ -383,15 +383,6 @@
      (uid_3 (= #\4))
      (uid_4 (= #\5)))
 
-   ;; `((t_n (or (r7rs (lambda (c)
-   ;;                    (and (char? c)
-   ;;                         (char-numeric? c))))
-   ;;            uid_1))
-   ;;   (t_4 (tuple uid_1 uid_2 uid_3))
-   ;;   (uid_1 (= #\3))
-   ;;   (uid_2 (= #\4))
-   ;;   (uid_3 (= #\5)))
-
    ;; `((t_n (r7rs (lambda (c)
    ;;                (and (char? c)
    ;;                     (char-numeric? c)))))
