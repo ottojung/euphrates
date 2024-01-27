8bf6e1f290a6a45de37a596f9eddd48319e6f69c
Otto Jung <otto.jung@vauplace.com>
2024-01-27 10:24:45 -0800

    save

---
+++ src/euphrates/labelinglogic-expression-to-dnf.scm
@@ -0,0 +1,49 @@
+;;;; Copyright (C) 2024  Otto Jung
+;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define (labelinglogic:expression:to-dnf expr)
+  (define expr*
+    (labelinglogic:expression:desugar
+     (labelinglogic:expression:move-nots-down expr)))
+
+  (define (make type args)
+    (labelinglogic:expression:make type args))
+
+  (let loop ((expr expr*))
+    (define type (labelinglogic:expression:type expr))
+    (define args (labelinglogic:expression:args expr))
+
+    (cond
+     ((equal? type 'constant) expr)
+     ((equal? type 'not) expr)
+     ((equal? type 'or) (make type (map loop args)))
+     ((equal? type 'and)
+      (if (null? args) expr
+          (let ()
+            (define args* (map loop args))
+            (define c (car args*))
+            (define rest (cdr args*))
+            (if (null? rest) expr
+                (let ()
+                  (define next (car rest))
+                  (define next-type (labelinglogic:expression:type next))
+                  (define next-args (labelinglogic:expression:args next))
+                  (define c-type (labelinglogic:expression:type c))
+                  (cond
+                   ((equal? next-type 'or)
+                    (loop
+                     (make 'or
+                       (map
+                        (lambda (x) (make 'and (list c x)))
+                        next-args))))
+                   ((equal? c-type 'or)
+                    (loop (make type (list next c))))
+                   (else
+                    (make 'and args*))))))))
+
+     (else
+      (raisu* :from "labelinglogic:expression:to-dnf"
+              :type 'unknown-expr-type
+              :message (stringf "Expression type ~s not recognized"
+                                (~a type))
+              :args (list type expr))))))
---
+++ src/euphrates/labelinglogic-expression-to-dnf.sld
@@ -0,0 +1,48 @@
+
+(define-library
+  (euphrates labelinglogic-expression-to-dnf)
+  (export labelinglogic:expression:to-dnf)
+  (import
+    (only (euphrates labelinglogic-expression-args)
+          labelinglogic:expression:args))
+  (import
+    (only (euphrates labelinglogic-expression-desugar)
+          labelinglogic:expression:desugar))
+  (import
+    (only (euphrates labelinglogic-expression-make)
+          labelinglogic:expression:make))
+  (import
+    (only (euphrates
+            labelinglogic-expression-move-nots-down)
+          labelinglogic:expression:move-nots-down))
+  (import
+    (only (euphrates labelinglogic-expression-type)
+          labelinglogic:expression:type))
+  (import (only (euphrates raisu-star) raisu*))
+  (import (only (euphrates stringf) stringf))
+  (import (only (euphrates tilda-a) ~a))
+  (import
+    (only (scheme base)
+          and
+          begin
+          car
+          cdr
+          cond
+          define
+          else
+          equal?
+          if
+          lambda
+          let
+          list
+          map
+          not
+          null?
+          or
+          quote))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin
+             (include-from-path
+               "euphrates/labelinglogic-expression-to-dnf.scm")))
+    (else (include "labelinglogic-expression-to-dnf.scm"))))
