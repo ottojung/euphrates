ebb13da2893108d4269241603edb562e7cbcbc6a
Otto Jung <otto.jung@vauplace.com>
2024-01-27 10:30:44 -0800

    save

--- test/test-labelinglogic-expression-to-dnf.scm
+++ test/test-labelinglogic-expression-to-dnf.scm
@@ -49,32 +49,32 @@
  '(and (or a b) (or c d))
  '(and (or a b) (or c d)))
 
-; Test for distributive law with nested ANDs inside ORs
-(test
- '(or (and a b) (and c d))
- '(or (and a b) (and c d)))
+;; ; Test for distributive law with nested ANDs inside ORs
+;; (test
+;;  '(or (and a b) (and c d))
+;;  '(or (and a b) (and c d)))
 
-; Test with nested NOT expressions
-(test
- '(not (or (not x) y))
- '(and x (not y)))
+;; ; Test with nested NOT expressions
+;; (test
+;;  '(not (or (not x) y))
+;;  '(and x (not y)))
 
-; Test with nested ANDs and ORs
-(test
- '(and (or a (not b)) (or c d))
- '(and (or a (not b)) (or c d)))
+;; ; Test with nested ANDs and ORs
+;; (test
+;;  '(and (or a (not b)) (or c d))
+;;  '(and (or a (not b)) (or c d)))
 
-; Test that ensures idempotence (x OR x = x) and (x AND x = x)
-(test
- '(or x x)
- 'x)
+;; ; Test that ensures idempotence (x OR x = x) and (x AND x = x)
+;; (test
+;;  '(or x x)
+;;  'x)
 
-(test
- '(and x x)
- 'x)
+;; (test
+;;  '(and x x)
+;;  'x)
 
-; Test that involves all operators in a more complex expression
-(test
- '(or (and (not a) b) (and a (not b)) (and a b))
- '(and a (or (not a) b) (or (not b) b)))
+;; ; Test that involves all operators in a more complex expression
+;; (test
+;;  '(or (and (not a) b) (and a (not b)) (and a b))
+;;  '(and a (or (not a) b) (or (not b) b)))
 
