8e2c5ef003152405de0f0f17f94e1e93fa703299
Otto Jung <otto.jung@vauplace.com>
2024-01-27 10:26:10 -0800

    save

--- test/test-labelinglogic-expression-to-dnf.scm
+++ test/test-labelinglogic-expression-to-dnf.scm
@@ -4,3 +4,9 @@
  (labelinglogic:expression:sugarify
   (labelinglogic:expression:to-dnf
    '(and x (not y) (or x y)))))
+
+(assert=
+ '(or (and x (not y) x) (and x (not y) y))
+ (labelinglogic:expression:sugarify
+  (labelinglogic:expression:to-dnf
+   '(and x (not y) (or x y)))))
