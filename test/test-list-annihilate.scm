844df07073bddf18d7979b3e174912cb8103704a
Otto Jung <otto.jung@vauplace.com>
2024-02-16 21:25:47 -0800

    save

--- test/test-list-annihilate.scm
+++ test/test-list-annihilate.scm
@@ -60,7 +60,7 @@
          (list 'true 'true 'true 'true))
 
 ;; Test with characters
-(assert= (list-annihilate char=? 'char-same (list #\a #\b #\a))
+(assert= (list-annihilate equal? 'char-same (list #\a #\b #\a))
          (list 'char-same #\b 'char-same))
 
 ;; Test with vectors
--- test/test-list-annihilate.sld
+++ test/test-list-annihilate.sld
@@ -17,7 +17,6 @@
           abs
           and
           begin
-          char=?
           cons
           define
           equal?
