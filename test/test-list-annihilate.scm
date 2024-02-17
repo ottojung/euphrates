5d523ed7e9c3d8aedd73a1a4fb9a92a8afdd4e8a
Otto Jung <otto.jung@vauplace.com>
2024-02-16 21:24:06 -0800

    save

--- test/test-list-annihilate.scm
+++ test/test-list-annihilate.scm
@@ -11,3 +11,26 @@
 ;; Test with pairs (cons cells) and equal?
 (assert= (list-annihilate equal? 'same (list (cons 1 2) (cons 1 2) (cons 3 4)))
          (list 'same 'same (cons 3 4)))
+
+
+
+
+;; Test with an empty list
+(assert= (list-annihilate equal? 'c '())
+         '())
+
+;; Test list with a single element
+(assert= (list-annihilate equal? 'c (list 1))
+         (list 1))
+
+;; Test list with only identical elements
+(assert= (list-annihilate equal? 'annihilated (list 1 1 1 1))
+         (list 'annihilated 'annihilated 'annihilated 'annihilated))
+
+;; Test list with no matching elements
+(assert= (list-annihilate equal? 'n (list 1 2 3 4))
+         (list 1 2 3 4))
+
+;; Test list with non-numeric elements
+(assert= (list-annihilate equal? 'same (list 'a 'b 'c 'd))
+         (list 'a 'b 'c 'd))
