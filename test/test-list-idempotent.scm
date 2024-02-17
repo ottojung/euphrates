60e8dd902fcd36d9eae8c5cd306c775db3a2dbee
Otto Jung <otto.jung@vauplace.com>
2024-02-16 20:11:32 -0800

    save

--- test/test-list-idempotent.scm
+++ test/test-list-idempotent.scm
@@ -5,5 +5,5 @@
 (assert= (list-idempotent (lambda (x y)
                             (or (= (remainder x y) 0)
                                 (= (remainder y x) 0)))
-                          (list 1 2 1 4 1))
-         (list 1 2 4))
+                          (list 2 5 4 3 10 15 7))
+         (list 1))
