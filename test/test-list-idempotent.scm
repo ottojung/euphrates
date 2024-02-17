f9b6476ea6ac982dd737b8cde3670b7b7d3d1f6d
Otto Jung <otto.jung@vauplace.com>
2024-02-16 20:11:41 -0800

    save

--- test/test-list-idempotent.scm
+++ test/test-list-idempotent.scm
@@ -6,4 +6,4 @@
                             (or (= (remainder x y) 0)
                                 (= (remainder y x) 0)))
                           (list 2 5 4 3 10 15 7))
-         (list 1))
+         (list 2 5 3 7))
