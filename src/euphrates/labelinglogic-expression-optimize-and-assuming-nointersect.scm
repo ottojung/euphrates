241e74ee36a20bf716e1d2fcd247df068434061a
Otto Jung <otto.jung@vauplace.com>
2024-02-24 10:34:14 -0800

    save

--- src/euphrates/labelinglogic-expression-optimize-and-assuming-nointersect.scm
+++ src/euphrates/labelinglogic-expression-optimize-and-assuming-nointersect.scm
@@ -2,7 +2,7 @@
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define (labelinglogic:expression:optimize/and-assuming-nointersect expr)
-  ;; This function optimizes an and expression by removing contradicting, redundant and non-intersecting components.
+  ;; This function optimizes an 'and' expression by removing contradicting, redundant and non-intersecting components.
   ;;
   ;; The optimization process includes:
   ;;   - Removal of duplicate parts of the expression
