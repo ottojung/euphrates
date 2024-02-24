3953a33d8c37004445e2dde738a32980e1310929
Otto Jung <otto.jung@vauplace.com>
2024-02-24 11:37:07 -0800

    save

--- test/test-labelinglogic-expression-optimize-and-assuming-nointersect.scm
+++ test/test-labelinglogic-expression-optimize-and-assuming-nointersect.scm
@@ -20,6 +20,11 @@
   '(and (= 0) (= 0))))
 
 (assert=
+ '(and (= 0))
+ (labelinglogic:expression:optimize/and-assuming-nointersect
+  '(and (= 0) (= 0) (= 0) (= 0) (= 0))))
+
+(assert=
  '(or)
  (labelinglogic:expression:optimize/and-assuming-nointersect
   '(and (= 0) (= 1))))
