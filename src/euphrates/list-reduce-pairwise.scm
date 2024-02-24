4bd50092b2be97b9d51572fb9b2c9294501bdfdf
Otto Jung <otto.jung@vauplace.com>
2024-02-23 21:51:59 -0800

    save

---
+++ src/euphrates/list-reduce-pairwise.scm
@@ -0,0 +1,37 @@
+;;;; Copyright (C) 2024  Otto Jung
+;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define (list-reduce/pairwise projection lst)
+  ;; The `list-annihilate` function takes three arguments:
+  ;;   pred: a binary predicate function that defines when two elements should be "annihilated" and replaced by `constant`
+  ;;   constant: the value to be used as a replacement for the annihilated elements
+  ;;   lst: a list of elements to be processed
+  ;;
+  ;; It returns a new list where, starting from the head of the list,
+  ;; every time an element satisfies the predicate `pred` with any preceding element, both get replaced by `constant`.
+  ;; The result list maintains the original structure but with elements replaced by `constant` as defined by the predicate.
+  ;;
+  ;; Example usage:
+  ;; (list-annihilate equal? 'c (list 1 2 1 4 1))
+  ;; will return
+  ;; (list 'c 2 'c 4 'c)
+  ;; replacing all appearances of the elements that have been "annihilated" with 'c.
+
+  (define input (list->vector lst))
+  (define output (vector-copy input))
+  (define n (vector-length input))
+
+  (let loop ((x 0) (y 0))
+    (when (< x n)
+
+      (when (< x y)
+        (when (pred (vector-ref input x)
+                    (vector-ref input y))
+          (vector-set! output x constant)
+          (vector-set! output y constant)))
+
+      (if (< y (- n 1))
+          (loop x (+ 1 y))
+          (loop (+ 1 x) (+ 1 x)))))
+
+  (vector->list output))
---
+++ src/euphrates/list-reduce-pairwise.sld
@@ -0,0 +1,29 @@
+
+(define-library
+  (euphrates list-reduce-pairwise)
+  (export list-reduce/pairwise)
+  (import
+    (only (euphrates list-annihilate)
+          list-annihilate))
+  (import
+    (only (scheme base)
+          +
+          -
+          <
+          begin
+          define
+          if
+          let
+          list->vector
+          vector->list
+          vector-copy
+          vector-length
+          vector-ref
+          vector-set!
+          when))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin
+             (include-from-path
+               "euphrates/list-reduce-pairwise.scm")))
+    (else (include "list-reduce-pairwise.scm"))))
