1ae42bb641c07930ecec126768cc30e4a85a4ca6
Otto Jung <otto.jung@vauplace.com>
2024-02-24 11:06:17 -0800

    save

--- test/test-labelinglogic-expression-optimize-and-assuming-nointersect.scm
+++ test/test-labelinglogic-expression-optimize-and-assuming-nointersect.scm
@@ -24,43 +24,43 @@
  (labelinglogic:expression:optimize/and-assuming-nointersect
   '(and (= 0) (= 1))))
 
-;;Optimizing non-intersecting negated ground terms
+;; Optimizing non-intersecting negated ground terms
 (assert=
  '(or)
  (labelinglogic:expression:optimize/and-assuming-nointersect
   '(and (not (= 1)) (= 1))))
 
-;;Optimizing non-intersecting r7rs terms
+;; Optimizing non-intersecting r7rs terms
 (assert=
  '(or)
  (labelinglogic:expression:optimize/and-assuming-nointersect
   '(and (r7rs even?) (r7rs odd?))))
 
-;;Negated and non-negated expressions
+;; Negated and non-negated expressions
 (assert=
  '(or)
  (labelinglogic:expression:optimize/and-assuming-nointersect
   '(and (= 1) (not (= 1)))))
 
-;;Different non-intersecting tuples
+;; Different non-intersecting tuples
 (assert=
  '(and (tuple 1 2) (tuple 3 4))
  (labelinglogic:expression:optimize/and-assuming-nointersect
   '(and (tuple 1 2) (tuple 3 4))))
 
-;;The same tuples
+;; The same tuples
 (assert=
  '(and (tuple 1 2))
  (labelinglogic:expression:optimize/and-assuming-nointersect
   '(and (tuple 1 2) (tuple 1 2))))
 
-;;Tuples with top expressions
+;; Tuples with top expressions
 (assert=
  '(and (tuple 1 2))
  (labelinglogic:expression:optimize/and-assuming-nointersect
   '(and (tuple 1 2) (tuple 1 2) (and))))
 
-;;Tuples with bottom expressions
+;; Tuples with bottom expressions
 (assert=
  '(or)
  (labelinglogic:expression:optimize/and-assuming-nointersect
