77d3c4f825029f4d56a8ee4d962b21950d012299
Otto Jung <otto.jung@vauplace.com>
2024-02-23 21:52:17 -0800

    save

--- src/euphrates/list-reduce-pairwise.scm
+++ src/euphrates/list-reduce-pairwise.scm
@@ -1,7 +1,7 @@
 ;;;; Copyright (C) 2024  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define (list-reduce/pairwise pred projection lst)
+(define (list-reduce/pairwise projection default-value lst)
   ;; The `list-annihilate` function takes three arguments:
   ;;   pred: a binary predicate function that defines when two elements should be "annihilated" and replaced by `constant`
   ;;   constant: the value to be used as a replacement for the annihilated elements
