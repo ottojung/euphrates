8e96543a1390ee9cf07fb8309316b730302da531
Otto Jung <otto.jung@vauplace.com>
2024-02-25 11:27:06 -0800

    save

--- test/test-labelinglogic-expression-optimize-and-assuming-nointersect.scm
+++ test/test-labelinglogic-expression-optimize-and-assuming-nointersect.scm
@@ -147,7 +147,7 @@
 
 ;; A complex case combining 'not =' and 'r7rs [7]
 (assert=
- '(and (not (= 4)) (r7rs even?) (not (= 2)))
+ 0
  (labelinglogic:expression:optimize/and-assuming-nointersect
   '(and (not (= 3)) (not (= 4)) (r7rs even?) (not (= 2)))))
 
