af023ef5a95c36a7d952da8edea96e66601fdfa7
Otto Jung <otto.jung@vauplace.com>
2024-02-25 15:01:42 -0800

    save

--- test/test-labelinglogic-expression-optimize-and-assuming-nointersect.scm
+++ test/test-labelinglogic-expression-optimize-and-assuming-nointersect.scm
@@ -1,71 +1,71 @@
 
-(assert=
- '(and)
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and)))
+;; (assert=
+;;  '(and)
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and)))
 
-(assert=
- '(and (= 0))
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (= 0) (= 0))))
+;; (assert=
+;;  '(and (= 0))
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (= 0) (= 0))))
 
-(assert=
- '(and (= 0))
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (= 0) (= 0) (= 0) (= 0) (= 0))))
+;; (assert=
+;;  '(and (= 0))
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (= 0) (= 0) (= 0) (= 0) (= 0))))
 
-(assert=
- '(or)
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (= 0) (= 1))))
+;; (assert=
+;;  '(or)
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (= 0) (= 1))))
 
-;; Optimizing non-intersecting negated ground terms
-(assert=
- '(or)
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (not (= 1)) (= 1))))
+;; ;; Optimizing non-intersecting negated ground terms
+;; (assert=
+;;  '(or)
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (not (= 1)) (= 1))))
 
-;; Optimizing non-intersecting r7rs terms
-(assert=
- '(or)
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (r7rs even?) (r7rs odd?))))
+;; ;; Optimizing non-intersecting r7rs terms
+;; (assert=
+;;  '(or)
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (r7rs even?) (r7rs odd?))))
 
-;; Negated and non-negated expressions
-(assert=
- '(or)
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (= 1) (not (= 1)))))
+;; ;; Negated and non-negated expressions
+;; (assert=
+;;  '(or)
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (= 1) (not (= 1)))))
 
-;; Different non-intersecting tuples
-(assert=
- '(and (tuple (= 1) (= 2)) (tuple (= 3) (= 4)))
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (tuple (= 1) (= 2)) (tuple (= 3) (= 4)))))
+;; ;; Different non-intersecting tuples
+;; (assert=
+;;  '(and (tuple (= 1) (= 2)) (tuple (= 3) (= 4)))
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (tuple (= 1) (= 2)) (tuple (= 3) (= 4)))))
 
-;; The same tuples
-(assert=
- '(and (tuple (= 1) (= 2)))
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (tuple (= 1) (= 2)) (tuple (= 1) (= 2)))))
+;; ;; The same tuples
+;; (assert=
+;;  '(and (tuple (= 1) (= 2)))
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (tuple (= 1) (= 2)) (tuple (= 1) (= 2)))))
 
-;; Tuples with top expressions
-(assert=
- '(and (tuple (= 1) (= 2)))
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (tuple (= 1) (= 2)) (tuple (= 1) (= 2)) (and))))
+;; ;; Tuples with top expressions
+;; (assert=
+;;  '(and (tuple (= 1) (= 2)))
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (tuple (= 1) (= 2)) (tuple (= 1) (= 2)) (and))))
 
-;; Tuples with bottom expressions
-(assert=
- '(or)
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (tuple (= 1) (= 2)) (tuple (= 1) (= 2)) (or))))
+;; ;; Tuples with bottom expressions
+;; (assert=
+;;  '(or)
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (tuple (= 1) (= 2)) (tuple (= 1) (= 2)) (or))))
 
-;; A complex case combining '=' and 'r7rs [1]
-(assert=
- '(or)
- (labelinglogic:expression:optimize/and-assuming-nointersect
-  '(and (r7rs even?) (= 3))))
+;; ;; A complex case combining '=' and 'r7rs [1]
+;; (assert=
+;;  '(or)
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect
+;;   '(and (r7rs even?) (= 3))))
 
 ;; A complex case combining '=' and 'r7rs [2]
 (assert=
