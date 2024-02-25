22fcadf30b661a054941846e501c817c90532059
Otto Jung <otto.jung@vauplace.com>
2024-02-25 12:53:11 -0800

    save

--- test/test-list-reduce-pairwise.scm
+++ test/test-list-reduce-pairwise.scm
@@ -4,7 +4,7 @@
 (assert= (list-reduce/pairwise (lambda (direction x y) (if (equal? x y) (values 'found) (values))) '(a a b b))
          (list 'found 'found))
 
-(assert= (list-reduce/pairwise (lambda (direction x y) (if (> x 2) (values 'hit) (values))) '(1 2 3 4 5 6 7))
+(assert= (list-reduce/pairwise (lambda (direction x y) (if (and (equal? direction 'forward) (> x 2)) (values 'hit) (values))) '(1 2 3 4 5 6 7))
          (list 1 2 'hit 'hit 7))
 
 (assert= (list-reduce/pairwise (lambda (direction x y) (if (string=? x "hello") (values 'present) (values))) '("apple" "banana" "cherry" "hello" "apple"))
