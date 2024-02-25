7184f70731bea0bae158a53c94eb759e7310d5d9
Otto Jung <otto.jung@vauplace.com>
2024-02-25 14:10:19 -0800

    save

--- test/test-list-idempotent.scm
+++ test/test-list-idempotent.scm
@@ -77,8 +77,8 @@
          (list 1 2 3 1 2 3))
 
 ;; Test Case 14: Test where the predicate is always true
-(assert= (list-idempotent (lambda (x y) 'left) (list 1 2 3 1 2 3))
+(assert= (list-idempotent (lambda (x y) 'left) (list 1 2 3 4 5 6 7))
          (list 1 3 2))
 
-(assert= (list-idempotent (lambda (x y) 'right) (list 1 2 3 1 2 3))
+(assert= (list-idempotent (lambda (x y) 'right) (list 1 2 3 4 5 6 7))
          (list 1 3 2))
