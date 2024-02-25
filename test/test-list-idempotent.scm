6b04557bfbc3f861d4dd01957d9497b36e4e3713
Otto Jung <otto.jung@vauplace.com>
2024-02-25 14:05:42 -0800

    save

--- test/test-list-idempotent.scm
+++ test/test-list-idempotent.scm
@@ -32,6 +32,8 @@
 (assert= (list-idempotent greaterp (list 5 4 3 6 2 7))
          (list 5 6 7))
 
+(exit 0)
+
 ;; Test Case 5: Test with case-insensitive string equality
 (assert= (list-idempotent (lambda (x y) (string-ci=? x y))
                           (list "hello" "HELLO" "world" "WORLD" "hello"))
