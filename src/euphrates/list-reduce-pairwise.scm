cf888ffb3f5b78d74eaeb24045d0afff6aa55703
Otto Jung <otto.jung@vauplace.com>
2024-02-25 12:38:16 -0800

    save

--- src/euphrates/list-reduce-pairwise.scm
+++ src/euphrates/list-reduce-pairwise.scm
@@ -67,11 +67,13 @@
               (vector-set! output x value)
               (hashset-add! ignored x)
               (hashset-add! ignored y)
+              (hashset-add! taken x)
               (loop (+ 1 x) (+ 1 x)))
              (else
               (vector-set! output y value)
               (hashset-add! ignored x)
               (hashset-add! ignored y)
+              (hashset-add! taken y)
               (loop (+ 1 x) (+ 1 x)))))
 
           (if (< y (- n 1))
