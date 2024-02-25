f9261df4fda29d9647a0da398fbd5bd5c93da879
Otto Jung <otto.jung@vauplace.com>
2024-02-25 15:36:36 -0800

    save

--- test/test-list-reduce-pairwise.scm
+++ test/test-list-reduce-pairwise.scm
@@ -27,7 +27,7 @@
 (assert= (list-reduce/pairwise
           (lambda (d x y) (if (string=? x "hello") 'present (values)))
           '("apple" "banana" "cherry" "hello" "apple"))
-         ' (        "banana" "cherry" present "apple"))
+         '(         "banana" "cherry" present "apple"))
 
 (assert= (list-reduce/pairwise (lambda (d x y) (if (= (modulo (+ x y) 2) 0) 0 (values))) '(1 2 3 4 5 6))
          (list 0 0 5 6))
