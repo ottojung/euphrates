4a52f1e4a6bbf2c6d5169227dbe439b159ffa63c
Otto Jung <otto.jung@vauplace.com>
2024-02-25 08:22:02 -0800

    save

--- src/euphrates/labelinglogic-expression-evaluate-r7rs.scm
+++ src/euphrates/labelinglogic-expression-evaluate-r7rs.scm
@@ -1,13 +1,6 @@
 ;;;; Copyright (C) 2023  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define labelinglogic:expression:evaluate/r7rs
-  (let ()
-    (define env
-      (environment '(scheme base) '(scheme char)))
-
-    (lambda (expr input)
-      (define args (labelinglogic:expression:args expr))
-      (define code (car args))
-      (define fun (eval code env))
-      (fun input))))
+(define (labelinglogic:expression:evaluate/r7rs expr input)
+  (define fun (labelinglogic:expression:compile/r7rs expr))
+  (fun input))
