bf8a88c07a4adcb87029e02d62d54240b13f6999
Otto Jung <otto.jung@vauplace.com>
2024-02-25 12:58:28 -0800

    save

--- test/test-list-reduce-pairwise.scm
+++ test/test-list-reduce-pairwise.scm
@@ -1,4 +1,3 @@
-
 (assert= (list-reduce/pairwise (lambda (direction x y) (if (equal? x y) (values 'found) (values))) '(a a b b))
          (list 'found 'found))
 
@@ -11,8 +10,42 @@
 (assert= (list-reduce/pairwise (lambda (direction x y) (if (string=? x "hello") (values 'present) (values))) '("apple" "banana" "cherry" "hello" "apple"))
          (list 'present "banana" "cherry" "apple"))
 
+(exit 0)
+
 (assert= (list-reduce/pairwise (lambda (direction x y) (if (= (modulo x 2) 0) (values 2) (values))) '(1 2 3 4 5 6))
-         (list 2 2 2))
+         (list 1 2 2 6))
+
+(assert= (list-reduce/pairwise (lambda (direction x y) (if (null? x) (values 'nil) (values))) '(() a b c ()))
+         (list 'nil 'b 'c (list)))
+
+(assert-throw #t (list-reduce/pairwise (lambda (direction x y) (+ x y)) 0)) ;; type error
+
+(assert= (list-reduce/pairwise (lambda (direction x y) (if (and (number? x) (number? y)) (values x) (values))) '(1 2 "three" 4))
+         (list 1 "three" 4))
+
+;; Test with an empty list
+(assert= (list-reduce/pairwise (lambda (direction x y) (values)) '())
+         (list))
+
+;; Test list with a single element
+(assert= (list-reduce/pairwise (lambda (direction x y) (values)) (list 'only-ele))
+         (list 'only-ele))
+
+;; Test list with duplicates
+(assert= (list-reduce/pairwise (lambda (direction x y) (if (equal? x y) (values 'found) (values))) '(1 2 2 3 3 4 4))
+         (list 1 'found 'found 'found))
+
+;; Test list containing sublists
+(assert= (list-reduce/pairwise (lambda (direction x y) (if (equal? x y) (values 'hit) (values))) '((1 2) (3 4) (1 2)))
+         (list 'hit (list 3 4)))
+
+;; Test with elements of different data types
+(assert= (list-reduce/pairwise (lambda (direction x y) (if (char? x) (values 'yes) (values))) '(#\a 2 #\b "string" 3.14159 (2 . 3)))
+         (list 'yes 'yes 3.14159 (cons 2  3)))
+
+;; Test with a list of different types of pairs and `equal` predicate
+(assert= (list-reduce/pairwise (lambda (direction x y) (if (equal? x y) (values 'match) (values))) '((1 . 2) "pair" (1 . 2) "not pair" (1 . 2)))
+         (list 'match "pair" "not pair" (cons 1 2)))
 
 (assert= (list-reduce/pairwise (lambda (direction x y) (if (and (equal? direction 'forward) (> x 2)) (values 'hit) (values))) '(1 2 3 4 5 6 7))
          (list 1 2 'hit 'hit 7))
--- test/test-list-reduce-pairwise.sld
+++ test/test-list-reduce-pairwise.sld
@@ -3,22 +3,30 @@
   (test-list-reduce/pairwise)
   (import (only (euphrates assert-equal) assert=))
   (import
+    (only (euphrates assert-throw) assert-throw))
+  (import
     (only (euphrates list-reduce-pairwise)
           list-reduce/pairwise))
   (import
     (only (scheme base)
+          +
           =
           >
           and
           begin
+          char?
+          cons
           equal?
           if
           lambda
           list
           modulo
+          null?
+          number?
           quote
           string=?
           values))
+  (import (only (scheme process-context) exit))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
