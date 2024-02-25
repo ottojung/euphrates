1a5be2f6cff6e74d0dc74257eb4d343c7655c709
Otto Jung <otto.jung@vauplace.com>
2024-02-25 14:14:32 -0800

    save

--- src/euphrates/list-annihilate.scm
+++ src/euphrates/list-annihilate.scm
@@ -1,15 +1,14 @@
 ;;;; Copyright (C) 2024  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define (list-annihilate pred constant lst)
+(define (list-annihilate pred lst)
   ;; The `list-annihilate` function takes three arguments:
   ;;   pred: a binary predicate function that defines when two elements should be "annihilated" and replaced by `constant`
-  ;;   constant: the value to be used as a replacement for the annihilated elements
   ;;   lst: a list of elements to be processed
   ;;
   ;; It returns a new list where, starting from the head of the list,
-  ;; every time an element satisfies the predicate `pred` with any preceding element, first get replaced by `constant`, and the second one gets removed.
-  ;; The result list maintains the original structure but with elements replaced by `constant` as defined by the predicate.
+  ;; every time an element satisfies the predicate `pred` with any preceding element, they are both removed from the list.
+  ;; The result list maintains the original structure but with some elements removed.
   ;;
   ;; Example usage:
   ;; (list-annihilate equal? 'c (list 1 2 1 4 1))
