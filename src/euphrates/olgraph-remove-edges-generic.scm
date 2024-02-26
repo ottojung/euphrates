bf1996a5875dbd19e8f99b5e59447e7c6eaacb60
Otto Jung <otto.jung@vauplace.com>
2024-02-26 07:35:10 -0800

    save

--- src/euphrates/olgraph-remove-edges-generic.scm
+++ src/euphrates/olgraph-remove-edges-generic.scm
@@ -1,6 +1,7 @@
 ;;;; Copyright (C) 2024  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 (define (olnode-remove-edges/generic/aux make-check H olnode)
   (define closure
     (olnode-transitive-closure/edges olnode))
