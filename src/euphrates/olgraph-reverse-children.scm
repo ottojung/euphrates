1ce6019d47d793b8a2fcd3359d1587f0544a3d50
Otto Jung <otto.jung@vauplace.com>
2024-02-26 12:15:05 -0800

    save

--- src/euphrates/olgraph-reverse-children.scm
+++ src/euphrates/olgraph-reverse-children.scm
@@ -1,4 +1,12 @@
 ;;;; Copyright (C) 2024  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define olgraph-reverse-children "wg1pgje7r5os74s0ctqa")
+(define (olnode-reverse-children olnode)
+  (let loop ((olnode olnode))
+    (define children (olnode:children olnode))
+    (olnode:children:set! olnode (reverse children))
+    (for-each loop children)))
+
+(define (olgraph-reverse-children olgraph)
+  (for-each
+  0)
