43d1e780c97b2c7acdac151c0f00c289fee84a55
Otto Jung <otto.jung@vauplace.com>
2024-02-25 18:08:38 -0800

    save

--- src/euphrates/olgraph.scm
+++ src/euphrates/olgraph.scm
@@ -8,6 +8,19 @@
   )
 
 
+                                        ; OLNODE
+
+                                        ; Each Olnode represents a node in the graph, and includes several properties:
+                                        ; * ID: A unique identifier for the node. This is used for efficient equality checks.
+                                        ; * Value: This represents the value or data held in the node.
+                                        ; * Children: List of Olnodes, representing outgoing connections (edges) from the node. 
+                                        ; * Meta: A placeholder for additional information associated with the node,
+                                        ;   this should not be considered part of the node itself.
+
+                                        ; Olnode utilizes a counter to ensure each node has a unique ID. This counter is incremented 
+                                        ; for each new node created.
+
+
 (define-type9 olnode
   (olnode-constructor id value children meta) olnode?
   (id olnode:id)
