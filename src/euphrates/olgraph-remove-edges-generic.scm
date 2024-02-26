f2c662bab3fa130b444ec91ad8ca79912c767fa9
Otto Jung <otto.jung@vauplace.com>
2024-02-26 07:34:32 -0800

    save

--- src/euphrates/olgraph-remove-edges-generic.scm
+++ src/euphrates/olgraph-remove-edges-generic.scm
@@ -1,4 +1,62 @@
 ;;;; Copyright (C) 2024  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define olgraph-remove-edges/generic "wg1pgje7r5os74s0ctqa")
+(define (olnode-remove-edges/generic/aux make-check H olnode)
+  (define closure
+    (olnode-transitive-closure/edges olnode))
+
+  (define keep?
+    (make-check closure))
+
+  (define ret
+    (make-olnode (olnode:value olnode)))
+
+  (let loop ((olnode olnode))
+    (define existing (hashmap-ref H (olnode:id olnode) #f))
+    (or existing
+        (let ()
+          (define ret
+            (make-olnode/full
+             (olnode:value olnode)
+             '()
+             (olnode:meta olnode)))
+
+          (define _182313
+            (hashmap-set! H (olnode:id olnode) ret))
+
+          (define old-children
+            (olnode:children olnode))
+
+          (define (contains-child? child)
+            (lambda (other)
+              (and (not (olnode-eq? other child))
+                   (let ()
+                     (define key (cons (olnode:id other)
+                                       (olnode:id child)))
+                     (hashset-has? closure key)))))
+
+          (define filtered
+            (filter
+             (lambda (child)
+               (list-and-map keep? old-children))
+             old-children))
+
+          (define new-children
+            (map loop filtered))
+
+          (olnode:children:set! ret new-children)
+
+          ret))))
+
+
+(define (olnode-remove-transitive-edges olnode)
+  (define H (make-hashmap))
+  (olnode-remove-transitive-edges/aux H olnode))
+
+
+(define (olgraph-remove-transitive-edges olgraph)
+  (define H (make-hashmap))
+  (define new-initials
+    (map (lambda (node) (olnode-remove-transitive-edges/aux H node))
+         (olgraph:initial olgraph)))
+  (make-olgraph new-initials))
--- src/euphrates/olgraph-remove-edges-generic.sld
+++ src/euphrates/olgraph-remove-edges-generic.sld
@@ -1,8 +1,49 @@
+
 (define-library
   (euphrates olgraph-remove-edges-generic)
   (export olgraph-remove-edges/generic)
   (import
-    (only (scheme base) begin define make-parameter))
+    (only (euphrates hashmap)
+          hashmap-ref
+          hashmap-set!
+          make-hashmap))
+  (import (only (euphrates hashset) hashset-has?))
+  (import
+    (only (euphrates list-and-map) list-and-map))
+  (import
+    (only (euphrates olgraph-remove-transitive-edges)
+          olgraph-remove-transitive-edges))
+  (import
+    (only (euphrates olgraph-transitive-closure)
+          olnode-transitive-closure/edges))
+  (import
+    (only (euphrates olgraph)
+          make-olgraph
+          make-olnode
+          make-olnode/full
+          olgraph:initial
+          olnode:children
+          olnode:children:set!
+          olnode:id
+          olnode:meta
+          olnode:value))
+  (import
+    (only (euphrates olnode-eq-huh) olnode-eq?))
+  (import
+    (only (scheme base)
+          and
+          begin
+          cons
+          define
+          lambda
+          let
+          map
+          not
+          or
+          quote))
+  (cond-expand
+    (guile (import (only (srfi srfi-1) filter)))
+    (else (import (only (srfi 1) filter))))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
