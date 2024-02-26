802378827ed0695cd4fd84ccbb34314f79b52dbf
Otto Jung <otto.jung@vauplace.com>
2024-02-26 07:10:00 -0800

    save

--- src/euphrates/olgraph-transitive-closure.scm
+++ src/euphrates/olgraph-transitive-closure.scm
@@ -12,7 +12,7 @@
        (unless (hashset-has? ret key)
          (for-each
           (lambda (ancestor)
-            (define key (cons ancestor child))
+            (define key (cons ancestor (olnode:id child)))
             (hashset-add! ret key))
           ancestors)
          (loop child (cons (olnode:id child) ancestors))))
