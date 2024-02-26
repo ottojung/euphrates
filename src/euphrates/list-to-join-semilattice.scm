af4410b03d39c57d238e620e949a5ef424d2634b
Otto Jung <otto.jung@vauplace.com>
2024-02-26 10:53:21 -0800

    save

--- src/euphrates/list-to-join-semilattice.scm
+++ src/euphrates/list-to-join-semilattice.scm
@@ -44,7 +44,8 @@
   ;;
   ;; produces the graph `((1) (2 1) (3 1) (4 2) (5 1))` (adjacency list notation).
   ;;
-  ;; In this case, for instance, `2` and `3` have the common successor `1`, and `4` is succeeded by `2`.
+  ;; In this case, for instance,
+  ;; elements `2` and `3` have the common successor `1`, and `4` is succeeded by `2`.
   ;;
   ;; Corner Cases:
   ;;
