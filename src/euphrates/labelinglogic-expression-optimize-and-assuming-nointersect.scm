1c4d5a7d3740353da0db1338b21f6fa78319b444
Otto Jung <otto.jung@vauplace.com>
2024-02-26 21:22:15 -0800

    save

--- src/euphrates/labelinglogic-expression-optimize-and-assuming-nointersect.scm
+++ src/euphrates/labelinglogic-expression-optimize-and-assuming-nointersect.scm
@@ -122,6 +122,12 @@
           (labelinglogic:expression:evaluate/r7rs
            expr-b (car args-a)))
 
+     (and (equal? type-a '=)
+          (equal? type-b 'not)
+          (equal? inner-type-b 'r7rs)
+          (labelinglogic:expression:evaluate/r7rs
+           expr-b (car args-a)))
+
      (and (equal? type-a 'r7rs)
           (equal? type-b 'not)
           (equal? inner-type-b '=)
