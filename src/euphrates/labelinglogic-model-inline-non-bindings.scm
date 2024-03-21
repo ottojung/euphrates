57377e0e2bacd59d8fbcae81a3d6b533cd24ff7a
Otto Jung <otto.jung@vauplace.com>
2024-03-20 20:12:49 -0700

    save

--- src/euphrates/labelinglogic-model-inline-non-bindings.scm
+++ src/euphrates/labelinglogic-model-inline-non-bindings.scm
@@ -2,11 +2,19 @@
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define (labelinglogic:model:inline-non-bindings model bindings)
+  (define bindings/s (list->hashset bindings))
 
   (define (inline-vars model)
-    (labelinglogic:model:map-expressions
+    (labelinglogic:model:map-subexpressions
      (lambda (class predicate)
-       (lambda (predicate)
-         
+       (lambda (expr)
+         (define type (labelinglogic:expression:type expr))
+         (define args (labelinglogic:expression:args expr))
+         (if (and (equal? type 'constant)
+                  (not (hashset-has? expr)))
+             (assoc-or expr model 'impossible-618263163)
+             expr)))
+
+     model))
 
   (apply-until-fixpoint inline-vars model))
--- src/euphrates/labelinglogic-model-inline-non-bindings.sld
+++ src/euphrates/labelinglogic-model-inline-non-bindings.sld
@@ -3,7 +3,34 @@
   (euphrates
     labelinglogic-model-inline-non-bindings)
   (export labelinglogic:model:inline-non-bindings)
-  (import (only (scheme base) begin define))
+  (import
+    (only (euphrates apply-until-fixpoint)
+          apply-until-fixpoint))
+  (import (only (euphrates assoc-or) assoc-or))
+  (import
+    (only (euphrates hashset)
+          hashset-has?
+          list->hashset))
+  (import
+    (only (euphrates labelinglogic-expression-args)
+          labelinglogic:expression:args))
+  (import
+    (only (euphrates labelinglogic-expression-type)
+          labelinglogic:expression:type))
+  (import
+    (only (euphrates
+            labelinglogic-model-map-subexpressions)
+          labelinglogic:model:map-subexpressions))
+  (import
+    (only (scheme base)
+          and
+          begin
+          define
+          equal?
+          if
+          lambda
+          not
+          quote))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
