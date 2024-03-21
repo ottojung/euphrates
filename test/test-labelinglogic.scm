974c1829695b3fe8ba3f846b0046a11ae6561df8
Otto Jung <otto.jung@vauplace.com>
2024-03-20 20:48:59 -0700

    save

--- test/test-labelinglogic.scm
+++ test/test-labelinglogic.scm
@@ -60,9 +60,14 @@
 
   (assert=
 
-   `((t_an (or (or (r7rs char-upper-case?)
-                   (r7rs char-lower-case?))
-               (r7rs char-numeric?)))
+   ;; `((t_an (or (or (r7rs char-upper-case?)
+   ;;                 (r7rs char-lower-case?))
+   ;;             (r7rs char-numeric?)))
+   ;;   (t_3 (= #\3)))
+
+   `((t_an (or (r7rs char-upper-case?)
+               (or (r7rs char-lower-case?)
+                   (r7rs char-numeric?))))
      (t_3 (= #\3)))
 
    ;; `((t_an (or uid_1 uid_2 uid_3))
