9dfc06577b4a06a3ccc62b95f04532ce0ed642d9
Otto Jung <otto.jung@vauplace.com>
2024-03-20 20:22:33 -0700

    save

--- src/euphrates/labelinglogic-model-assoc.scm
+++ src/euphrates/labelinglogic-model-assoc.scm
@@ -1,12 +1,10 @@
 ;;;; Copyright (C) 2024  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-syntax labelinglogic:model:assoc-or
-  (syntax-rules ()
-    ((_ expression-name model default)
-     (car (assoc-or expression-name model default)))
-
-    ((_ expression-name model)
-     (let ((name expression-name)
-           (model* model))
-       (labelinglogic:model:assoc-or name model* #f)))))
+(define (labelinglogic:model:assoc expression-name model)
+  (labelinglogic:model:assoc-or
+   expression-name model
+   (raisu* :from "labelinglogic:model:assoc"
+           :type 'model-assoc-error
+           :message (stringf "Expression named ~s is abscent in the model." name)
+           :args (list name model*))))
