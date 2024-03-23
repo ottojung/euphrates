009bda450b7d405b19dff0a0352cdd93aa09d3fd
Otto Jung <otto.jung@vauplace.com>
2024-03-22 22:35:10 -0700

    save

--- src/euphrates/labelinglogic-bindings-check.scm
+++ src/euphrates/labelinglogic-bindings-check.scm
@@ -2,55 +2,7 @@
 ;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define (labelinglogic:bindings:check classes/s tokens-alist)
-
-  (define (fail-tokens-check show args)
-    (raisu* :from "labelinglogic:init"
-            :type 'tokens-alist-type-error
-            :message (stringf "Type error in ~s: ~a." (quote tokens-alist) show)
-            :args args))
-
   (labelinglogic:model:check-structure tokens-alist)
-
-  (define keys
-    (labelinglogic:model:names tokens-alist))
-
-  (define duplicates
-    (list-get-duplicates
-     (filter
-      (negate unique-identifier?)
-      keys)))
-
-  (define keys/s
-    (list->hashset keys))
-
-  (unless (null? duplicates)
-    (fail-tokens-check
-     "keys must not repeat"
-     (list duplicates)))
-
-  (for-each
-   (lambda (binding)
-     (labelinglogic:binding:check binding)
-
-     (define expr (labelinglogic:binding:expr binding))
-
-     (define constants (labelinglogic:expression:constants expr))
-
-     (define undefined-constants
-       (filter (negate
-                (lambda (x)
-                  (or (hashset-has? classes/s x)
-                      (hashset-has? keys/s x))))
-               constants))
-
-     (unless (null? undefined-constants)
-       (raisu* :from "labelinglogic"
-               :type 'undefined-reference-in-binding
-               :message "Binding references undefined class."
-               :args (list expr binding undefined-constants)))
-
-     )
-
-   tokens-alist)
+  (labelinglogic:model:check-references classes/s tokens-alist)
 
   (when #f #t))
--- src/euphrates/labelinglogic-bindings-check.sld
+++ src/euphrates/labelinglogic-bindings-check.sld
@@ -3,48 +3,12 @@
   (euphrates labelinglogic-bindings-check)
   (export labelinglogic:bindings:check)
   (import
-    (only (euphrates hashset)
-          hashset-has?
-          list->hashset))
-  (import
-    (only (euphrates labelinglogic-binding-check)
-          labelinglogic:binding:check))
-  (import
-    (only (euphrates labelinglogic-binding-expr)
-          labelinglogic:binding:expr))
-  (import
-    (only (euphrates labelinglogic-expression-constants)
-          labelinglogic:expression:constants))
+    (only (euphrates labelinglogic-model-check-references)
+          labelinglogic:model:check-references))
   (import
     (only (euphrates labelinglogic-model-check-structure)
           labelinglogic:model:check-structure))
-  (import
-    (only (euphrates labelinglogic-model-names)
-          labelinglogic:model:names))
-  (import
-    (only (euphrates list-get-duplicates)
-          list-get-duplicates))
-  (import (only (euphrates negate) negate))
-  (import (only (euphrates raisu-star) raisu*))
-  (import (only (euphrates stringf) stringf))
-  (import
-    (only (euphrates unique-identifier)
-          unique-identifier?))
-  (import
-    (only (scheme base)
-          begin
-          define
-          for-each
-          lambda
-          list
-          null?
-          or
-          quote
-          unless
-          when))
-  (cond-expand
-    (guile (import (only (srfi srfi-1) filter)))
-    (else (import (only (srfi 1) filter))))
+  (import (only (scheme base) begin define when))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
