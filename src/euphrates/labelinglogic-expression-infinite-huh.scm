dd53e3978f27fab53f47a8b76c30a1e0ac67e38b
Otto Jung <otto.jung@vauplace.com>
2024-03-23 13:26:18 -0700

    save

--- src/euphrates/labelinglogic-expression-infinite-huh.scm
+++ src/euphrates/labelinglogic-expression-infinite-huh.scm
@@ -2,15 +2,12 @@
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define (labelinglogic:expression:infinite? expr)
-  (define type (labelinglogic:expression:type expr))
-  (define args (labelinglogic:expression:args expr))
-
   (define simple
     (labelinglogic:expression:desugar
      (labelinglogic:expression:move-nots-down expr)))
 
-  (define simple-type (labelinglogic:expression:type simple))
-  (define simple-args (labelinglogic:expression:args simple))
+  (define type (labelinglogic:expression:type simple))
+  (define args (labelinglogic:expression:args simple))
 
   (when (equal? type 'constant)
     (raisu* :from "labelinglogic:expression:infinite?"
