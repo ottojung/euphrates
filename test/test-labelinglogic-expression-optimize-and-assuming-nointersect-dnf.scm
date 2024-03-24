c9e8f5ba0e128167f027a3c2550e5b4d9d3bd7e0
Otto Jung <otto.jung@vauplace.com>
2024-03-23 17:28:34 -0700

    save

--- test/test-labelinglogic-expression-optimize-and-assuming-nointersect-dnf.scm
+++ test/test-labelinglogic-expression-optimize-and-assuming-nointersect-dnf.scm
@@ -45,15 +45,15 @@
 
 ;; Tuples with top expressions
 (assert=
- (or)
+ 999
  (labelinglogic:expression:optimize/and-assuming-nointersect-dnf
   '(and (tuple (= 1) (= 2)) (tuple (= 1) (= 2)) (and))))
 
-;; Tuples with bottom expressions
-(assert=
- '(or)
- (labelinglogic:expression:optimize/and-assuming-nointersect-dnf
-  '(and (tuple (= 1) (= 2)) (tuple (= 1) (= 2)) (or))))
+;; ;; Tuples with bottom expressions
+;; (assert=
+;;  '(or)
+;;  (labelinglogic:expression:optimize/and-assuming-nointersect-dnf
+;;   '(and (tuple (= 1) (= 2)) (tuple (= 1) (= 2)) (or))))
 
 ;; Combining '=' and 'not =' [1]
 (assert=
