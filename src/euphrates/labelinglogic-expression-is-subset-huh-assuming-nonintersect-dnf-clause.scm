d1628a3fd9d7b712bd47e6ac014f2ef93406d513
Otto Jung <otto.jung@vauplace.com>
2024-03-23 18:14:21 -0700

    save

--- src/euphrates/labelinglogic-expression-is-subset-huh-assuming-nonintersect-dnf-clause.scm
+++ src/euphrates/labelinglogic-expression-is-subset-huh-assuming-nonintersect-dnf-clause.scm
@@ -1,7 +1,8 @@
 ;;;; Copyright (C) 2024  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define (labelinglogic:expression:is-subset?/assuming-nonintersect-dnf-clause expr-small expr-big)
+(define (labelinglogic:expression:is-subset?/assuming-nonintersect-dnf-clause
+         expr-small expr-big)
   (let loop ((expr-small expr-small)
              (expr-big expr-big))
 
