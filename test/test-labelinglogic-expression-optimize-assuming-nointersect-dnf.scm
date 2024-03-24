bf3dd71e5f2182c3a6064807281a27f322160394
Otto Jung <otto.jung@vauplace.com>
2024-03-23 20:15:35 -0700

    save

--- test/test-labelinglogic-expression-optimize-assuming-nointersect-dnf.scm
+++ test/test-labelinglogic-expression-optimize-assuming-nointersect-dnf.scm
@@ -540,7 +540,7 @@
 
 ;; Case with top number
 (assert-throw
- 'expression-type-error
+ 'bad-sub-expr-type
  (labelinglogic:expression:optimize/assuming-nointersect-dnf 812312))
 
 ;; Case with '((or))'
