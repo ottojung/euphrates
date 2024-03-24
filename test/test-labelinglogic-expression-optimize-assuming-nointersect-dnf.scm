0aaa9acd877067bda7a273ea377d80d8c6945d83
Otto Jung <otto.jung@vauplace.com>
2024-03-23 18:48:13 -0700

    save

--- test/test-labelinglogic-expression-optimize-assuming-nointersect-dnf.scm
+++ test/test-labelinglogic-expression-optimize-assuming-nointersect-dnf.scm
@@ -9,12 +9,12 @@
  (labelinglogic:expression:optimize/assuming-nointersect-dnf
   '(and (= 0) (= 0))))
 
-;; (assert=
-;;  '(and (= 0))
-;;  (labelinglogic:expression:optimize/assuming-nointersect-dnf
-;;   '(and (= 0) (= 0) (= 0) (= 0) (= 0))))
+(assert=
+ '(and (= 0))
+ (labelinglogic:expression:optimize/assuming-nointersect-dnf
+  '(and (= 0) (= 0) (= 0) (= 0) (= 0))))
 
-;; (assert=
-;;  '(or)
-;;  (labelinglogic:expression:optimize/assuming-nointersect-dnf
-;;   '(and (= 0) (= 1))))
+(assert=
+ '(or)
+ (labelinglogic:expression:optimize/assuming-nointersect-dnf
+  '(and (= 0) (= 1))))
