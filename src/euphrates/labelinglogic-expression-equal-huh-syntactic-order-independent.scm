10fd9a74db545906d99c4e1864badc338d5b6abc
Otto Jung <otto.jung@vauplace.com>
2024-03-24 22:19:06 -0700

    save

--- src/euphrates/labelinglogic-expression-equal-huh-syntactic-order-independent.scm
+++ src/euphrates/labelinglogic-expression-equal-huh-syntactic-order-independent.scm
@@ -4,6 +4,9 @@
 (define (labelinglogic:expression:equal?/syntactic/order-independent
          expr-1 expr-2)
 
+  (debugs expr-1)
+  (debugs expr-2)
+
   (let loop ((expr-1 expr-1)
              (expr-2 expr-2))
 
@@ -20,9 +23,6 @@
             (loop arg1 arg2))
           args-2))
        args-1))
-
-    (debugs expr-1)
-    (debugs expr-2)
 
     (and
      (equal? type-1 type-2)
