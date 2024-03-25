817ca6ed72b65d5697f412f67008ff7717f7b684
Otto Jung <otto.jung@vauplace.com>
2024-03-24 22:06:40 -0700

    save

--- test/test-list-to-join-semilattice.scm
+++ test/test-list-to-join-semilattice.scm
@@ -59,6 +59,25 @@
 
 
 (test-case
+ equal?
+ (lambda (x y)
+   (greatest-common-divisor x y))
+
+ '(2 3 4 5)
+
+ '((2 1)
+   (1)
+   (3 1)
+   (4 2)
+   (5 1))
+
+ )
+
+
+
+
+
+(test-case
  (lambda (x y)
    (hashset-equal?
     (list->hashset x)
