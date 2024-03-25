20b7821f3e291779057535a5c656d0d62a294048
Otto Jung <otto.jung@vauplace.com>
2024-03-24 20:40:31 -0700

    save

--- src/euphrates/labelinglogic-model-latticize-ands-assuming-nointersect-dnf.scm
+++ src/euphrates/labelinglogic-model-latticize-ands-assuming-nointersect-dnf.scm
@@ -3,51 +3,51 @@
 
 (define (labelinglogic:model:latticize-ands-assuming-nointersect-dnf model)
 
-  ;; (define (nicolaus? x)
-  ;;   (and (labelinglogic:expression? x)
-  ;;        (let ()
-  ;;          (define type (labelinglogic:expression:type x))
-  ;;          (define args (labelinglogic:expression:args x))
+  (define (nicolaus? x)
+    (and (labelinglogic:expression? x)
+         (let ()
+           (define type (labelinglogic:expression:type x))
+           (define args (labelinglogic:expression:args x))
 
-  ;;          (or (equal? 'r7rs type)
-  ;;              (and (equal? 'and type)
-  ;;                   (list-or-map
-  ;;                    (lambda (arg)
-  ;;                      (define type (labelinglogic:expression:type arg))
-  ;;                      (define args (labelinglogic:expression:args arg))
-  ;;                      (equal? type 'r7rs))
-  ;;                    args))))))
+           (or (equal? 'r7rs type)
+               (and (equal? 'and type)
+                    (list-or-map
+                     (lambda (arg)
+                       (define type (labelinglogic:expression:type arg))
+                       (define args (labelinglogic:expression:args arg))
+                       (equal? type 'r7rs))
+                     args))))))
 
-  ;; (define nicolaus-map (make-hashmap))
+  (define nicolaus-map (make-hashmap))
 
-  ;; (define _1237123
-  ;;   (for-each
-  ;;    (lambda (binding)
-  ;;      (define class (labelinglogic:binding:name binding))
-  ;;      (define expr (labelinglogic:binding:expr binding))
-  ;;      (define type (labelinglogic:expression:type expr))
-  ;;      (define args (labelinglogic:expression:args expr))
+  (define _1237123
+    (for-each
+     (lambda (binding)
+       (define class (labelinglogic:binding:name binding))
+       (define expr (labelinglogic:binding:expr binding))
+       (define type (labelinglogic:expression:type expr))
+       (define args (labelinglogic:expression:args expr))
 
-  ;;      (define terms
-  ;;        (if (equal? 'or type) args (list expr)))
+       (define terms
+         (if (equal? 'or type) args (list expr)))
 
-  ;;      (define r7rss
-  ;;        (filter
-  ;;         (lambda (term)
-  ;;           (define type (labelinglogic:expression:type term))
-  ;;           (define args (labelinglogic:expression:args term))
+       (define r7rss
+         (filter
+          (lambda (term)
+            (define type (labelinglogic:expression:type term))
+            (define args (labelinglogic:expression:args term))
 
-  ;;           (or (equal? 'r7rs type)
-  ;;               (and (equal? 'and type)
-  ;;                    (list-or-map
-  ;;                     (lambda (arg)
-  ;;                       (define type (labelinglogic:expression:type arg))
-  ;;                       (define args (labelinglogic:expression:args arg))
-  ;;                       (equal? type 'r7rs))
-  ;;                     args))))
-  ;;         terms))
+            (or (equal? 'r7rs type)
+                (and (equal? 'and type)
+                     (list-or-map
+                      (lambda (arg)
+                        (define type (labelinglogic:expression:type arg))
+                        (define args (labelinglogic:expression:args arg))
+                        (equal? type 'r7rs))
+                      args))))
+          terms))
 
-  ;;      0)
-  ;;    (labelinglogic:model:bindings model)))
+       0)
+     (labelinglogic:model:bindings model)))
 
   model)
--- src/euphrates/labelinglogic-model-latticize-ands-assuming-nointersect-dnf.sld
+++ src/euphrates/labelinglogic-model-latticize-ands-assuming-nointersect-dnf.sld
@@ -4,7 +4,43 @@
     labelinglogic-model-latticize-ands-assuming-nointersect-dnf)
   (export
     labelinglogic:model:latticize-ands-assuming-nointersect-dnf)
-  (import (only (scheme base) begin define))
+  (import (only (euphrates hashmap) make-hashmap))
+  (import
+    (only (euphrates labelinglogic-binding-expr)
+          labelinglogic:binding:expr))
+  (import
+    (only (euphrates labelinglogic-binding-name)
+          labelinglogic:binding:name))
+  (import
+    (only (euphrates labelinglogic-expression-args)
+          labelinglogic:expression:args))
+  (import
+    (only (euphrates labelinglogic-expression-huh)
+          labelinglogic:expression?))
+  (import
+    (only (euphrates labelinglogic-expression-type)
+          labelinglogic:expression:type))
+  (import
+    (only (euphrates labelinglogic-model-bindings)
+          labelinglogic:model:bindings))
+  (import
+    (only (euphrates list-or-map) list-or-map))
+  (import
+    (only (scheme base)
+          and
+          begin
+          define
+          equal?
+          for-each
+          if
+          lambda
+          let
+          list
+          or
+          quote))
+  (cond-expand
+    (guile (import (only (srfi srfi-1) filter)))
+    (else (import (only (srfi 1) filter))))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
