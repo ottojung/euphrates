0e51210fc5e663cb4822aabebf7e5fedd140c118
Otto Jung <otto.jung@vauplace.com>
2024-03-25 22:44:05 -0700

    save

--- src/euphrates/list-idempotent.scm
+++ src/euphrates/list-idempotent.scm
@@ -13,7 +13,7 @@
   ;; In set theory, idempotent operations such as union, intersection, or function composition do not change
   ;; the output upon repeated application. Similarly, `list-idempotent` will ensure that applying the same function
   ;; repeatedly will yield the same list after the first application, as duplicate elements will have been removed.
-  ;; This procedure is `list-deduplicate', but with a binary `pred' instead of a unary `identity', and working one step at a time.
+  ;; This procedure is like `list-deduplicate', but with a binary `pred' instead of a unary `identity', and working one step at a time.
   ;;
   ;; Example usage:
   ;; (list-idempotent equal? (list 1 2 1 4 1))
