8c295b6016df5c6c67d038f9f250c852088ab568
Otto Jung <otto.jung@vauplace.com>
2024-04-02 21:24:14 -0700

    save

--- test/test-list-to-join-semilattice.scm
+++ test/test-list-to-join-semilattice.scm
@@ -2,18 +2,19 @@
 (define-syntax test-case
   (syntax-rules ()
     ((_ equality-tester join-function lst expected)
-     (define result
-       (list->join-semilattice
-        equality-tester
-        join-function
-        lst))
+     (begin
+       (define result
+         (list->join-semilattice
+          equality-tester
+          join-function
+          lst))
 
-     (define actual
-       (olgraph->adjlist result))
+       (define actual
+         (olgraph->adjlist result))
 
-     ;; (debugs actual)
+       ;; (debugs actual)
 
-     (assert= actual expected))))
+       (assert= actual expected)))))
 
 
 
