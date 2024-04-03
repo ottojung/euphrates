23bf8100297c5b04920e8d59d0d9316d6d4bb40f
Otto Jung <otto.jung@vauplace.com>
2024-04-02 20:30:08 -0700

    save

--- test/test-labelinglogic.scm
+++ test/test-labelinglogic.scm
@@ -148,7 +148,11 @@
 
   (assert=
 
-   9999
+   '((t_an (or (= #\3)
+               (and (r7rs char-numeric?)
+                    (not (= #\5))
+                    (not (= #\3)))))
+     (t_3 (= #\3)))
 
    (labelinglogic:model:alpha-rename
     '() (labelinglogic:init
