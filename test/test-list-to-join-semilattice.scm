a1e2010d31fde0715b301dec7afbf2d4ebfe7adf
Otto Jung <otto.jung@vauplace.com>
2024-04-02 21:50:23 -0700

    save

--- test/test-list-to-join-semilattice.scm
+++ test/test-list-to-join-semilattice.scm
@@ -18,186 +18,186 @@
 
 
 
-(test-case
- equal?
- (lambda (x y)
-   (greatest-common-divisor x y))
+;; (test-case
+;;  equal?
+;;  (lambda (x y)
+;;    (greatest-common-divisor x y))
 
- '(1 2 3 4 5)
+;;  '(1 2 3 4 5)
 
- '((1)
-   (2 1)
-   (3 1)
-   (4 2)
-   (5 1))
+;;  '((1)
+;;    (2 1)
+;;    (3 1)
+;;    (4 2)
+;;    (5 1))
 
- )
-
-
-
-(test-case
- equal?
- (lambda (x y)
-   (greatest-common-divisor x y))
-
- '(1 2 3 4 5 6 7 8 9 10 11 12)
-
- '((1)
-   (2 1)
-   (3 1)
-   (4 2)
-   (5 1)
-   (6 3 2)
-   (7 1)
-   (8 4)
-   (9 3)
-   (10 2 5)
-   (11 1)
-   (12 4 6))
-
- )
+;;  )
 
 
 
+;; (test-case
+;;  equal?
+;;  (lambda (x y)
+;;    (greatest-common-divisor x y))
 
+;;  '(1 2 3 4 5 6 7 8 9 10 11 12)
 
-(test-case
- equal?
- (lambda (x y)
-   (greatest-common-divisor x y))
+;;  '((1)
+;;    (2 1)
+;;    (3 1)
+;;    (4 2)
+;;    (5 1)
+;;    (6 3 2)
+;;    (7 1)
+;;    (8 4)
+;;    (9 3)
+;;    (10 2 5)
+;;    (11 1)
+;;    (12 4 6))
 
- '(2 3 4 5)
-
- '((2 1)
-   (1)
-   (3 1)
-   (4 2)
-   (5 1))
-
- )
-
-
-
-(test-case
- equal?
- (lambda (x y)
-   (greatest-common-divisor x y))
-
- '(3 4 5)
-
- '((3 1)
-   (1)
-   (4 1)
-   (5 1))
-
- )
-
-
-
-(test-case
- equal?
- (lambda (x y)
-   (greatest-common-divisor x y))
-
- '(3 4 5 6)
-
- '((3 1)
-   (1)
-   (4 2)
-   (2 1)
-   (5 1)
-   (6 3 2))
-
- )
+;;  )
 
 
 
 
 
-(test-case
- (lambda (x y)
-   (hashset-equal?
-    (list->hashset x)
-    (list->hashset y)))
+;; (test-case
+;;  equal?
+;;  (lambda (x y)
+;;    (greatest-common-divisor x y))
 
- (lambda (x y) (list-intersect x y))
+;;  '(2 3 4 5)
 
- '((1 2 3) (1 2 6))
+;;  '((2 1)
+;;    (1)
+;;    (3 1)
+;;    (4 2)
+;;    (5 1))
 
- '(((1 2 3) (1 2))
-   ((1 2))
-   ((1 2 6) (1 2)))
-
- )
-
+;;  )
 
 
 
-(test-case
- (lambda (x y)
-   (hashset-equal?
-    (list->hashset x)
-    (list->hashset y)))
+;; (test-case
+;;  equal?
+;;  (lambda (x y)
+;;    (greatest-common-divisor x y))
 
- (lambda (x y) (list-intersect x y))
+;;  '(3 4 5)
 
- '((1 2 3) (2 6 1) (5 1 6) (7 1))
+;;  '((3 1)
+;;    (1)
+;;    (4 1)
+;;    (5 1))
 
- '(((1 2 3) (1 2))
-   ((1 2) (1))
-   ((1))
-   ((2 6 1) (1 2) (6 1))
-   ((6 1) (1))
-   ((5 1 6) (6 1))
-   ((7 1) (1)))
-
- )
+;;  )
 
 
 
-(test-case
- (lambda (x y)
-   (hashset-equal?
-    (list->hashset x)
-    (list->hashset y)))
+;; (test-case
+;;  equal?
+;;  (lambda (x y)
+;;    (greatest-common-divisor x y))
 
- (lambda (x y)
-   (let ((r (list-intersect x y)))
-     (if (null? r) (values) r)))
+;;  '(3 4 5 6)
 
- '((1 2 3) (2 6 1) (5 1 6) (7 1) (5))
+;;  '((3 1)
+;;    (1)
+;;    (4 2)
+;;    (2 1)
+;;    (5 1)
+;;    (6 3 2))
 
- '(((1 2 3) (1 2))
-   ((1 2) (1))
-   ((1))
-   ((2 6 1) (1 2) (6 1))
-   ((6 1) (1))
-   ((5 1 6) (6 1) (5))
-   ((5))
-   ((7 1) (1)))
-
- )
+;;  )
 
 
 
-(test-case
- (lambda (x y)
-   (hashset-equal?
-    (list->hashset x)
-    (list->hashset y)))
 
- (lambda (x y) (list-union x y))
 
- '((1 2) (2 1) (5 1) (7 1 5))
+;; (test-case
+;;  (lambda (x y)
+;;    (hashset-equal?
+;;     (list->hashset x)
+;;     (list->hashset y)))
 
- '(((1 2) (1 2 5))
-   ((1 2 5) (1 2 7 5))
-   ((1 2 7 5))
-   ((2 1) (1 2))
-   ((5 1) (1 2 5) (7 1 5))
-   ((7 1 5) (1 2 7 5)))
+;;  (lambda (x y) (list-intersect x y))
 
- )
+;;  '((1 2 3) (1 2 6))
+
+;;  '(((1 2 3) (1 2))
+;;    ((1 2))
+;;    ((1 2 6) (1 2)))
+
+;;  )
+
+
+
+
+;; (test-case
+;;  (lambda (x y)
+;;    (hashset-equal?
+;;     (list->hashset x)
+;;     (list->hashset y)))
+
+;;  (lambda (x y) (list-intersect x y))
+
+;;  '((1 2 3) (2 6 1) (5 1 6) (7 1))
+
+;;  '(((1 2 3) (1 2))
+;;    ((1 2) (1))
+;;    ((1))
+;;    ((2 6 1) (1 2) (6 1))
+;;    ((6 1) (1))
+;;    ((5 1 6) (6 1))
+;;    ((7 1) (1)))
+
+;;  )
+
+
+
+;; (test-case
+;;  (lambda (x y)
+;;    (hashset-equal?
+;;     (list->hashset x)
+;;     (list->hashset y)))
+
+;;  (lambda (x y)
+;;    (let ((r (list-intersect x y)))
+;;      (if (null? r) (values) r)))
+
+;;  '((1 2 3) (2 6 1) (5 1 6) (7 1) (5))
+
+;;  '(((1 2 3) (1 2))
+;;    ((1 2) (1))
+;;    ((1))
+;;    ((2 6 1) (1 2) (6 1))
+;;    ((6 1) (1))
+;;    ((5 1 6) (6 1) (5))
+;;    ((5))
+;;    ((7 1) (1)))
+
+;;  )
+
+
+
+;; (test-case
+;;  (lambda (x y)
+;;    (hashset-equal?
+;;     (list->hashset x)
+;;     (list->hashset y)))
+
+;;  (lambda (x y) (list-union x y))
+
+;;  '((1 2) (2 1) (5 1) (7 1 5))
+
+;;  '(((1 2) (1 2 5))
+;;    ((1 2 5) (1 2 7 5))
+;;    ((1 2 7 5))
+;;    ((2 1) (1 2))
+;;    ((5 1) (1 2 5) (7 1 5))
+;;    ((7 1 5) (1 2 7 5)))
+
+;;  )
 
 
 
