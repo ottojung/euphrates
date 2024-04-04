ba86ca6134e15d4dd0d1a13b1e26d9126968f49f
Otto Jung <otto.jung@vauplace.com>
2024-04-03 20:59:47 -0700

    save

--- test/test-labelinglogic-model-factor-dnf-clauses.scm
+++ test/test-labelinglogic-model-factor-dnf-clauses.scm
@@ -48,11 +48,7 @@
  ;; Simple factor out.
 
  :model
- `((alphabetic (or upcase lowercase))
-   (upcase (r7rs char-upper-case?))
-   (lowercase (r7rs char-lower-case?))
-   (numeric (r7rs char-numeric?))
-   )
+ `((rule1 (or (= 5) (= 6))))
 
  :expected
  `((alphabetic (or upcase lowercase))
