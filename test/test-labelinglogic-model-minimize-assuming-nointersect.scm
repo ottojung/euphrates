af399d10c85db82df21f832327fe2f900204297d
Otto Jung <otto.jung@vauplace.com>
2024-04-03 19:14:44 -0700

    save

--- test/test-labelinglogic-model-minimize-assuming-nointersect.scm
+++ test/test-labelinglogic-model-minimize-assuming-nointersect.scm
@@ -69,12 +69,52 @@
                   (not (= #\4))
                   (not (= #\9)))
              (= #\4)))
+
    (t_bn (or (= #\9)
              (and (r7rs char-numeric?)
                   (not (= #\3))
                   (not (= #\4))
                   (not (= #\9)))
              (= #\3)))
+
+   (t_3 (= #\9)))
+
+ )
+
+
+
+
+(test1
+
+ :model
+ `((any (or alphanum whitespace))
+   (alphanum (or alphabetic numeric))
+   (alphabetic (or upcase lowercase))
+   (upcase (r7rs char-upper-case?))
+   (lowercase (r7rs char-lower-case?))
+   (numeric (r7rs char-numeric?))
+   (whitespace (r7rs char-whitespace?)))
+
+ :bindings
+ `((t_an (and numeric (not (= #\3))))
+   (t_bn (and numeric (not (= #\4))))
+   (t_3  (= #\9)))
+
+ :expected
+ '((t_an (or (= #\9)
+             (and (r7rs char-numeric?)
+                  (not (= #\3))
+                  (not (= #\4))
+                  (not (= #\9)))
+             (= #\4)))
+
+   (t_bn (or (= #\9)
+             (and (r7rs char-numeric?)
+                  (not (= #\3))
+                  (not (= #\4))
+                  (not (= #\9)))
+             (= #\3)))
+
    (t_3 (= #\9)))
 
  )
