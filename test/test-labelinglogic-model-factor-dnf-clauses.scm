8a2c9e326172ce6162f8063464e9ae051c97a9d1
Otto Jung <otto.jung@vauplace.com>
2024-04-03 21:01:17 -0700

    save

--- test/test-labelinglogic-model-factor-dnf-clauses.scm
+++ test/test-labelinglogic-model-factor-dnf-clauses.scm
@@ -66,12 +66,36 @@
 
  :model
  `((rule1 (or (= 5) rule2 (= 6)))
-   (rule2 (or (= 3) (= 5))))
+   (rule2 (or (= 7) (= 8))))
 
  :expected
- `((rule1 (or uid_1 uid_2))
+ `((rule1 (or uid_1 rule2 uid_2))
+   (rule2 (or uid_3 uid_4))
    (uid_1 (= 5))
-   (uid_2 (= 6)))
+   (uid_2 (= 6))
+   (uid_3 (= 3))
+   (uid_4 (= 5)))
+
+ )
+
+
+
+
+(testcase
+
+ ;; Nested factor out with duplicates.
+
+ :model
+ `((rule1 (or (= 5) rule2 (= 6)))
+   (rule2 (or (= 7) (= 5))))
+
+ :expected
+ `((rule1 (or uid_1 rule2 uid_2))
+   (rule2 (or uid_3 uid_4))
+   (uid_1 (= 5))
+   (uid_2 (= 6))
+   (uid_3 (= 3))
+   (uid_4 (= 5)))
 
  )
 
