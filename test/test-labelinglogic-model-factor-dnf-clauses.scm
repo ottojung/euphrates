ffe86442035078845fa6793737fec6c1dac25ac8
Otto Jung <otto.jung@vauplace.com>
2024-04-03 21:08:38 -0700

    save

--- test/test-labelinglogic-model-factor-dnf-clauses.scm
+++ test/test-labelinglogic-model-factor-dnf-clauses.scm
@@ -195,3 +195,27 @@
 
  )
 
+
+
+(testcase
+
+ ;; Complex nested factor out with duplicates across different rules.
+
+ :model
+ '((rule1 (or (= 5) rule2 (= 6) rule3))
+   (rule2 (or (= 7) (= 5)))
+   (rule3 (or (= 5) (= 5) (= 8)))
+   (rule4 (or rule1 rule2 rule3)))
+
+ :expected
+ '((rule1 (or uid_1 rule2 uid_2 rule3))
+   (rule2 (or uid_3 uid_1))
+   (rule3 (or uid_1 uid_1 uid_4))
+   (rule4 (or rule1 rule2 rule3))
+   (uid_1 (= 5))
+   (uid_2 (= 6))
+   (uid_3 (= 7))
+   (uid_4 (= 8)))
+
+ )
+
