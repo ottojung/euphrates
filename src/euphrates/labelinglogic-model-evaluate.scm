9a74ed70f17842416b5852229e4dad2dbcf2715a
Otto Jung <otto.jung@vauplace.com>
2024-04-03 19:44:54 -0700

    save

--- src/euphrates/labelinglogic-model-evaluate.scm
+++ src/euphrates/labelinglogic-model-evaluate.scm
@@ -1,9 +1,12 @@
 ;;;; Copyright (C) 2024  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define (labelinglogic:model:evaluate model expr)
-  (map
-   labelinglogic:binding:name
-   (labelinglogic:model:filter
+(define (labelinglogic:model:evaluate model input)
+  (filter
+   identity
+   (map
     (lambda (binding)
-      
+      (define expr (labelinglogic:binding:expr bindings))
+      (and (labelinglogic:expression:evaluate model expr input)
+           (labelinglogic:binding:name binding)))
+    (labelinglogic:model:bindings model))))
--- src/euphrates/labelinglogic-model-evaluate.sld
+++ src/euphrates/labelinglogic-model-evaluate.sld
@@ -1,8 +1,25 @@
+
 (define-library
   (euphrates labelinglogic-model-evaluate)
   (export labelinglogic:model:evaluate)
+  (import (only (euphrates identity) identity))
   (import
-    (only (scheme base) begin define make-parameter))
+    (only (euphrates labelinglogic-binding-expr)
+          labelinglogic:binding:expr))
+  (import
+    (only (euphrates labelinglogic-binding-name)
+          labelinglogic:binding:name))
+  (import
+    (only (euphrates labelinglogic-expression-evaluate)
+          labelinglogic:expression:evaluate))
+  (import
+    (only (euphrates labelinglogic-model-bindings)
+          labelinglogic:model:bindings))
+  (import
+    (only (scheme base) and begin define lambda map))
+  (cond-expand
+    (guile (import (only (srfi srfi-1) filter)))
+    (else (import (only (srfi 1) filter))))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
