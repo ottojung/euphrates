decdfe911e448cce1bbe4102ad66067a963bd4a2
Otto Jung <otto.jung@vauplace.com>
2024-04-03 20:52:35 -0700

    save

--- test/test-labelinglogic-model-factor-dnf-clauses.scm
+++ test/test-labelinglogic-model-factor-dnf-clauses.scm
@@ -53,19 +53,7 @@
    )
 
  :expected
- '((any (or uid_1 uid_2))
-   (alphanum (or uid_3 uid_4))
-   (alphabetic (or uid_5 uid_6))
-   (upcase upcase)
-   (lowercase lowercase)
-   (numeric numeric)
-   (whitespace whitespace)
-   (uid_6 lowercase)
-   (uid_5 upcase)
-   (uid_4 numeric)
-   (uid_3 alphabetic)
-   (uid_2 whitespace)
-   (uid_1 alphanum))
+ 99999
 
  )
 
