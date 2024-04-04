d2b0f9f0ee3f3dae383225215decc17ef0e184df
Otto Jung <otto.jung@vauplace.com>
2024-04-03 21:07:15 -0700

    save

--- test/test-labelinglogic-model-factor-dnf-clauses.scm
+++ test/test-labelinglogic-model-factor-dnf-clauses.scm
@@ -84,12 +84,12 @@
    (rule2 (or (= 7) (= 8))))
 
  :expected
- ((rule1 (or uid_1 rule2 uid_2))
-  (rule2 (or uid_3 uid_4))
-  (uid_1 (= 5))
-  (uid_2 (= 6))
-  (uid_3 (= 7))
-  (uid_4 (= 8)))
+ '((rule1 (or uid_1 rule2 uid_2))
+   (rule2 (or uid_3 uid_4))
+   (uid_1 (= 5))
+   (uid_2 (= 6))
+   (uid_3 (= 7))
+   (uid_4 (= 8)))
 
  )
 
