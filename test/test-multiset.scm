2c7ab8681a9ee02225d975922f6b600923a7ac64
Otto Jung <otto.jung@vauplace.com>
2024-04-06 12:15:27 -0700

    save

--- test/test-multiset.scm
+++ test/test-multiset.scm
@@ -1,3 +1,20 @@
 
+(assert=
+ (multiset->list (make-multiset))
+ '())
+
+(assert=
+ (multiset->list (make-multiset (list 1 2 3 4)))
+ (multiset->list (make-multiset (list 1 2 3 4)))
+ )
+
+(assert=
+ (multiset->list (make-multiset (list 1 2 2 2 3 4)))
+ (multiset->list (make-multiset (list 1 2 2 2 3 4)))
+ )
+
 (assert
- (make-multiset
+ (not
+  (equal?
+   (multiset->list (make-multiset (list 1 2 3 4)))
+   (multiset->list (make-multiset (list 1 2 2 2 3 4))))))
