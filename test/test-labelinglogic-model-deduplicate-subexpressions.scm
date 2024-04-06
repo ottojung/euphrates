9de16b0c6131a04a078382d88918a95f10e00d0c
Otto Jung <otto.jung@vauplace.com>
2024-04-06 12:40:33 -0700

    save

---
+++ test/test-labelinglogic-model-deduplicate-subexpressions.scm
@@ -0,0 +1,328 @@
+
+
+;;
+;;
+;;  ▄▄▄▄▄▄▄                 ▄                           ▄
+;;     █     ▄▄▄    ▄▄▄   ▄▄█▄▄          ▄▄▄    ▄▄▄   ▄▄█▄▄  ▄   ▄  ▄▄▄▄
+;;     █    █▀  █  █   ▀    █           █   ▀  █▀  █    █    █   █  █▀ ▀█
+;;     █    █▀▀▀▀   ▀▀▀▄    █            ▀▀▀▄  █▀▀▀▀    █    █   █  █   █
+;;     █    ▀█▄▄▀  ▀▄▄▄▀    ▀▄▄         ▀▄▄▄▀  ▀█▄▄▀    ▀▄▄  ▀▄▄▀█  ██▄█▀
+;;                                                                  █
+;;                                                                  ▀
+
+
+
+(define-syntax testcase
+  (syntax-rules (:model :bindings :expected)
+    ((_ :model model :expected expected)
+     (let ()
+       (define result
+         (labelinglogic:model:deduplicate-subexpressions
+          model))
+
+       (define result/alpha
+         (labelinglogic:model:alpha-rename '() result))
+
+       (unless (equal? expected result/alpha)
+         (debugs result/alpha))
+
+       (assert= expected result/alpha)))))
+
+
+
+;;
+;;
+;;  ▄▄▄▄▄▄▄                 ▄
+;;     █     ▄▄▄    ▄▄▄   ▄▄█▄▄          ▄▄▄    ▄▄▄    ▄▄▄    ▄▄▄    ▄▄▄
+;;     █    █▀  █  █   ▀    █           █▀  ▀  ▀   █  █   ▀  █▀  █  █   ▀
+;;     █    █▀▀▀▀   ▀▀▀▄    █           █      ▄▀▀▀█   ▀▀▀▄  █▀▀▀▀   ▀▀▀▄
+;;     █    ▀█▄▄▀  ▀▄▄▄▀    ▀▄▄         ▀█▄▄▀  ▀▄▄▀█  ▀▄▄▄▀  ▀█▄▄▀  ▀▄▄▄▀
+;;
+;;
+
+
+
+;; (testcase
+
+;;  ;; Simple factor out.
+
+;;  :model
+;;  '((rule1 (or (= 5) (= 6))))
+
+;;  :expected
+;;  '((rule1 (or uid_1 uid_2))
+;;    (uid_1 (= 5))
+;;    (uid_2 (= 6)))
+
+;;  )
+
+
+
+
+;; (testcase
+
+;;  ;; Simple factor out of duplicates.
+
+;;  :model
+;;  '((rule1 (or (= 5) (= 6) (= 5))))
+
+;;  :expected
+;;  '((rule1 (or uid_1 uid_2 uid_1))
+;;    (uid_1 (= 5))
+;;    (uid_2 (= 6)))
+
+;;  )
+
+
+;; (testcase
+
+;;  ;; Nested factor out.
+
+;;  :model
+;;  '((rule1 (or (= 5) rule2 (= 6)))
+;;    (rule2 (or (= 7) (= 8))))
+
+;;  :expected
+;;  '((rule1 (or uid_1 rule2 uid_2))
+;;    (rule2 (or uid_3 uid_4))
+;;    (uid_1 (= 5))
+;;    (uid_2 (= 6))
+;;    (uid_3 (= 7))
+;;    (uid_4 (= 8)))
+
+;;  )
+
+
+
+
+;; (testcase
+
+;;  ;; Nested factor out with duplicates.
+
+;;  :model
+;;  '((rule1 (or (= 5) rule2 (= 6)))
+;;    (rule2 (or (= 7) (= 5))))
+
+;;  :expected
+;;  '((rule1 (or uid_1 rule2 uid_2))
+;;    (rule2 (or uid_3 uid_1))
+;;    (uid_1 (= 5))
+;;    (uid_2 (= 6))
+;;    (uid_3 (= 7)))
+
+;;  )
+
+
+
+;; (testcase
+
+;;  ;; No factoring out needed.
+
+;;  :model
+;;  '((alphabetic (or upcase lowercase))
+;;    (upcase (r7rs char-upper-case?))
+;;    (lowercase (r7rs char-lower-case?))
+;;    (numeric (r7rs char-numeric?))
+;;    )
+
+;;  :expected
+;;  '((alphabetic (or upcase lowercase))
+;;    (upcase (r7rs char-upper-case?))
+;;    (lowercase (r7rs char-lower-case?))
+;;    (numeric (r7rs char-numeric?))
+;;    )
+
+;;  )
+
+
+
+
+;; (testcase
+
+;;  ;; Duplicate bindings of equal expressions.
+
+;;  :model
+;;  '((just-5 (= 5))
+;;    (another-5 (= 5))
+;;    )
+
+;;  :expected
+;;  '((just-5 (= 5))
+;;    (another-5 just-5))
+
+;;  )
+
+
+
+;; (testcase
+
+;;  ;; Duplicate bindings of equal expressions [2].
+
+;;  :model
+;;  '((just-5 (= 5))
+;;    (another-5 (= 5))
+;;    (something-that-uses-5 (or (= 5) (= 3)))
+;;    )
+
+;;  :expected
+;;  '((just-5 (= 5))
+;;    (another-5 just-5)
+;;    (something-that-uses-5 (or just-5 uid_1))
+;;    (uid_1 (= 3)))
+
+;;  )
+
+
+
+
+;; (testcase
+
+;;  ;; Bigger nochange.
+
+;;  :model
+;;  '((any (or alphanum whitespace))
+;;    (alphanum (or alphabetic numeric))
+;;    (alphabetic (or upcase lowercase))
+;;    (upcase (r7rs char-upper-case?))
+;;    (lowercase (r7rs char-lower-case?))
+;;    (numeric (r7rs char-numeric?))
+;;    (whitespace (r7rs char-whitespace?))
+;;    )
+
+;;  :expected
+;;  '((any (or alphanum whitespace))
+;;    (alphanum (or alphabetic numeric))
+;;    (alphabetic (or upcase lowercase))
+;;    (upcase (r7rs char-upper-case?))
+;;    (lowercase (r7rs char-lower-case?))
+;;    (numeric (r7rs char-numeric?))
+;;    (whitespace (r7rs char-whitespace?)))
+
+;;  )
+
+
+
+;; (testcase
+
+;;  ;; Complex nested factor out with duplicates across different rules.
+
+;;  :model
+;;  '((rule1 (or (= 5) rule2 (= 6) rule3))
+;;    (rule2 (or (= 7) (= 5)))
+;;    (rule3 (or (= 5) (= 5) (= 8)))
+;;    (rule4 (or rule1 rule2 rule3)))
+
+;;  :expected
+;;  '((rule1 (or uid_1 rule2 uid_2 rule3))
+;;    (rule2 (or uid_3 uid_1))
+;;    (rule3 (or uid_1 uid_1 uid_4))
+;;    (rule4 (or rule1 rule2 rule3))
+;;    (uid_1 (= 5))
+;;    (uid_2 (= 6))
+;;    (uid_3 (= 7))
+;;    (uid_4 (= 8)))
+
+;;  )
+
+
+;; (testcase
+
+;;  ;; Simple tuples.
+
+;;  :model
+;;  '((just-5 (= 5))
+;;    (some-tuple (tuple (= 3) (= 5))))
+
+;;  :expected
+;;  '((just-5 (= 5))
+;;    (some-tuple (tuple uid_1 just-5))
+;;    (uid_1 (= 3)))
+
+;;  )
+
+
+
+;; (testcase
+
+;;  ;; Simple tuples [2].
+
+;;  :model
+;;  `((t_1 (= #\1))
+;;    (t_2 (= #\2))
+;;    (t_3 (= #\3))
+;;    (t_x (tuple (= #\x) (= #\3))))
+
+;;  :expected
+;;  '((t_1 (= #\1))
+;;    (t_2 (= #\2))
+;;    (t_3 (= #\3))
+;;    (t_x (tuple uid_1 t_3))
+;;    (uid_1 (= #\x)))
+
+;;  )
+
+
+
+
+;; (testcase
+
+;;  ;; Embeded tuples.
+
+;;  :model
+;;  `((t_1 (= #\1))
+;;    (t_2 (= #\2))
+;;    (t_3 (= #\3))
+;;    (t_x (or (tuple (= #\x) (= #\3))
+;;             (= #\2))))
+
+;;  :expected
+
+;;  '((t_1 (= #\1))
+;;    (t_2 (= #\2))
+;;    (t_3 (= #\3))
+;;    (t_x (or uid_1 t_2))
+;;    (uid_2 (= #\x))
+;;    (uid_1 (tuple uid_2 t_3)))
+
+;;  )
+
+
+
+;; (testcase
+
+;;  ;; Simple ands.
+
+;;  :model
+;;  '((just-5 (= 5))
+;;    (some-and (and (r7rs odd?) (not (= 5)))))
+
+;;  :expected
+;;  '((just-5 (= 5))
+;;    (some-and (and uid_1 uid_2))
+;;    (uid_1 (r7rs odd?))
+;;    (uid_2 (not just-5)))
+
+;;  )
+
+
+
+(testcase
+
+ ;; Duble ands.
+
+ :model
+ '((just-5 (= 5))
+   (some-and (and (r7rs odd?) (not (= 5))))
+   (other-and (and (r7rs odd?) (not (= 5)))))
+
+ :expected
+ '((just-5 (= 5))
+   (some-and (and uid_1 uid_2))
+   (other-and (and uid_1 uid_2))
+   (uid_1 (r7rs odd?))
+   (uid_2 (not just-5)))
+
+ )
+
+
+
---
+++ test/test-labelinglogic-model-deduplicate-subexpressions.sld
@@ -0,0 +1,34 @@
+
+(define-library
+  (test-labelinglogic-model-deduplicate-subexpressions)
+  (import (only (euphrates assert-equal) assert=))
+  (import (only (euphrates debugs) debugs))
+  (import
+    (only (euphrates labelinglogic-model-alpha-rename)
+          labelinglogic:model:alpha-rename))
+  (import
+    (only (euphrates
+            labelinglogic-model-deduplicate-subexpressions)
+          labelinglogic:model:deduplicate-subexpressions))
+  (import
+    (only (scheme base)
+          =
+          _
+          and
+          begin
+          define
+          define-syntax
+          equal?
+          let
+          not
+          odd?
+          quote
+          syntax-rules
+          unless))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin
+             (include-from-path
+               "test-labelinglogic-model-deduplicate-subexpressions.scm")))
+    (else (include
+            "test-labelinglogic-model-deduplicate-subexpressions.scm"))))
