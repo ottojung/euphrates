b28f4ad4463f5e096bd6781da22a9d89bbbdd13a
Otto Jung <otto.jung@vauplace.com>
2024-04-06 14:39:13 -0700

    save

--- test/test-labelinglogic-model-factor-dnf-clauses.scm
+++ test/test-labelinglogic-model-factor-dnf-clauses.scm
@@ -292,22 +292,22 @@
 
 
 
-;; (testcase
+(testcase
 
-;;  ;; Duble ands.
+ ;; Duble ands.
 
-;;  :model
-;;  '((just-5 (= 5))
-;;    (some-and (and (r7rs odd?) (not (= 5))))
-;;    (other-and (and (r7rs odd?) (not (= 5)))))
+ :model
+ '((just-5 (= 5))
+   (some-and (and (r7rs odd?) (not (= 5))))
+   (other-and (and (r7rs odd?) (not (= 5)))))
 
-;;  :expected
-;;  '((just-5 (= 5))
-;;    (some-and (and uid_1 uid_2))
-;;    (other-and (and uid_1 uid_2))
-;;    (uid_1 (r7rs odd?))
-;;    (uid_2 (not just-5)))
+ :expected
+ '((just-5 (= 5))
+   (some-and (and uid_1 uid_2))
+   (other-and (and uid_1 uid_2))
+   (uid_1 (r7rs odd?))
+   (uid_2 (not just-5)))
 
-;;  )
+ )
 
 
