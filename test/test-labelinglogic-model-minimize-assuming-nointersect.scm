b62697cadf92769d20f010ea893135b48d205a0b
Otto Jung <otto.jung@vauplace.com>
2024-04-06 20:05:53 -0700

    save

--- test/test-labelinglogic-model-minimize-assuming-nointersect.scm
+++ test/test-labelinglogic-model-minimize-assuming-nointersect.scm
@@ -121,8 +121,6 @@
 
 
 
-
-
 (testcase
 
  :model
@@ -136,14 +134,8 @@
    )
 
  :bindings
- `((t_an (and alphanum (not (= #\5))))
-   (t_bn (and alphanum (not (= #\7))))
+ `((t_bn (and alphanum (not (= #\7))))
    (t_cn (and (not (= #\5)) alphanum))
-   (t_dn (and alphanum (not (= #\7)) (not (= #\8))))
-   (t_en (and alphanum (not (= #\7)) (not (= #\.))))
-   (t_fn (or t_an t_bn t_3))
-   (t_3  (= #\3))
-   (t_8  (= #\8))
    )
 
  :expected
@@ -185,4 +177,71 @@
    (uid_5 (= #\7)))
 
  )
+
+
+
+
+
+;; (testcase
+
+;;  :model
+;;  `((any (or alphanum whitespace))
+;;    (alphanum (or alphabetic numeric))
+;;    (alphabetic (or upcase lowercase))
+;;    (upcase (r7rs char-upper-case?))
+;;    (lowercase (r7rs char-lower-case?))
+;;    (numeric (r7rs char-numeric?))
+;;    (whitespace (r7rs char-whitespace?))
+;;    )
+
+;;  :bindings
+;;  `((t_an (and alphanum (not (= #\5))))
+;;    (t_bn (and alphanum (not (= #\7))))
+;;    (t_cn (and (not (= #\5)) alphanum))
+;;    (t_dn (and alphanum (not (= #\7)) (not (= #\8))))
+;;    (t_en (and alphanum (not (= #\7)) (not (= #\.))))
+;;    (t_fn (or t_an t_bn t_3))
+;;    (t_3  (= #\3))
+;;    (t_8  (= #\8))
+;;    )
+
+;;  :expected
+;;  '((t_an (or uid_1 uid_2 t_8 t_3 uid_3 uid_5))
+;;    (t_bn (or uid_1 uid_2 t_8 t_3 uid_3 uid_4))
+;;    (t_cn (or (r7rs char-upper-case?)
+;;              (r7rs char-lower-case?)
+;;              (= #\8)
+;;              (= #\3)
+;;              (and (r7rs char-numeric?)
+;;                   (not (= #\5))
+;;                   (not (= #\7))
+;;                   (not (= #\8))
+;;                   (not (= #\3)))
+;;              (= #\7)))
+;;    (t_dn (or uid_1 uid_2 t_3 uid_3 uid_4))
+;;    (t_en (or (r7rs char-upper-case?)
+;;              (r7rs char-lower-case?)
+;;              (= #\8)
+;;              (= #\3)
+;;              (and (r7rs char-numeric?)
+;;                   (not (= #\5))
+;;                   (not (= #\7))
+;;                   (not (= #\8))
+;;                   (not (= #\3)))
+;;              (= #\5)))
+;;    (t_fn (or t_8 t_3 uid_3 uid_5 uid_1 uid_2 uid_4))
+;;    (t_3 (= #\3))
+;;    (t_8 (= #\8))
+
+;;    (uid_1 (r7rs char-upper-case?))
+;;    (uid_2 (r7rs char-lower-case?))
+;;    (uid_3 (and (r7rs char-numeric?)
+;;                (not (= #\5))
+;;                (not (= #\7))
+;;                (not (= #\8))
+;;                (not (= #\3))))
+;;    (uid_4 (= #\5))
+;;    (uid_5 (= #\7)))
+
+;;  )
 
