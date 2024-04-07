c0a5b73227226c2fb467df8788351455e824791c
Otto Jung <otto.jung@vauplace.com>
2024-04-06 23:18:41 -0700

    save

--- src/euphrates/parselynn-singlechar-result-as-iterator.scm
+++ src/euphrates/parselynn-singlechar-result-as-iterator.scm
@@ -12,12 +12,6 @@
   (define input
     (parselynn/singlechar-result-struct:input result))
 
-  ;; (define singleton-map
-  ;;   (parselynn/singlechar-struct:singleton-map this))
-
-  ;; (define categories
-  ;;   (parselynn/singlechar-struct:categories this))
-
   (define lexer-model
     (parselynn/singlechar:lexer-model this))
 
