23bb59a777f5739b7842cd14e4e7359b50b84279
Otto Jung <otto.jung@vauplace.com>
2024-04-06 19:30:25 -0700

    save

--- src/euphrates/labelinglogic-model-reduce-to-names.scm
+++ src/euphrates/labelinglogic-model-reduce-to-names.scm
@@ -1,10 +1,10 @@
 ;;;; Copyright (C) 2024  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define (labelinglogic:model:reduce-to-names names/set model)
+(define (labelinglogic:model:reduce-to-names names/stack model)
   (define names/set/full
     (labelinglogic:model:reachable-from
-     names/set model))
+     names/stack model))
 
   (labelinglogic:model:reduce-to-names/unsafe
    names/set/full model))
