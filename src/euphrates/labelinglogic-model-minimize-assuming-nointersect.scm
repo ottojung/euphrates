30388f49203334c0f2e5be397d9650c89c1c200f
Otto Jung <otto.jung@vauplace.com>
2024-04-06 19:32:43 -0700

    save

--- src/euphrates/labelinglogic-model-minimize-assuming-nointersect.scm
+++ src/euphrates/labelinglogic-model-minimize-assuming-nointersect.scm
@@ -23,8 +23,12 @@
   (define latticised-opt
     (labelinglogic:model:optimize/or/just-idempotency latticised-reduced))
 
+  (debugs latticised-opt)
+
   (define dedup-model
     (labelinglogic:model:deduplicate-subexpressions latticised-opt))
+
+  (debugs dedup-model)
 
   (define factored-model
     (labelinglogic:model:factor-dnf-clauses dedup-model))
--- src/euphrates/labelinglogic-model-minimize-assuming-nointersect.sld
+++ src/euphrates/labelinglogic-model-minimize-assuming-nointersect.sld
@@ -4,6 +4,7 @@
     labelinglogic-model-minimize-assuming-nointersect)
   (export
     labelinglogic:model:minimize/assuming-nointersect)
+  (import (only (euphrates debugs) debugs))
   (import
     (only (euphrates
             labelinglogic-model-deduplicate-subexpressions)
