642c46ac7faa9fd545f51b7041a49c3f3047141e
Otto Jung <otto.jung@vauplace.com>
2024-04-07 21:48:50 -0700

    upload benchmarks to permanent store

---
+++ scripts/update-benchmarks-git-branch.sh
@@ -0,0 +1,60 @@
+#! /bin/sh
+
+#################
+## This script is supposed to be run on CI.
+## It requires REPOLOGKEY variable to be set to the contents of the SSH key.
+#################
+
+set -e
+
+if test -z "$REPOLOGKEY"
+then
+    echo 'Variable $REPOLOGKEY is not set' 1>&2
+    exit 1
+fi
+
+SSHKEYFILE="$PWD/sshkey~"
+export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i $SSHKEYFILE"
+rm -f "$SSHKEYFILE"
+echo "$REPOLOGKEY" > "$SSHKEYFILE"
+chmod u=r,g=,o= "$SSHKEYFILE"
+
+set -x
+
+git clone \
+    --branch euphrates-master \
+    pubgit@vau.place:repolog.git \
+    benchmarks-repo \
+
+
+rm   -fr benchmarks-repo/data/
+mkdir -p benchmarks-repo/data/
+
+cp -T -r dist/benchmarks/ benchmarks-repo/data/
+
+cd benchmarks-repo/
+
+git submodule update --init
+
+cd euphrates/
+
+git fetch origin master
+git reset --hard origin/master
+
+cd -
+
+git config user.name "Otto Jung (bot)"
+git config user.email "otto.jung@vauplace.com"
+git config gpg.format ssh
+git config commit.gpgsign true
+git config user.signingkey "$SSHKEYFILE"
+git remote set-url --add origin pubgit@vau.place:repolog.git
+git remote set-url --add origin git@github.com:ottojung/repolog.git
+git remote set-url --add origin git@codeberg.org:otto/repolog.git
+
+git add --all
+git commit --message 'update benchmarks'
+git push origin HEAD
+
+rm -f "$SSHKEYFILE"
+rm -fr benchmarks-repo
