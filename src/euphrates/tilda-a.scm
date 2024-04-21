2e9d1bc7446c145a925edd1de6b702db4df4e36f
Otto Jung <otto.jung@vauplace.com>
2024-04-21 13:36:10 -0700

    improve ~a implementation

--- src/euphrates/tilda-a.scm
+++ src/euphrates/tilda-a.scm
@@ -1,4 +1,4 @@
-;;;; Copyright (C) 2020, 2021, 2023  Otto Jung
+;;;; Copyright (C) 2020, 2021, 2023, 2024  Otto Jung
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define (~a x)
@@ -6,4 +6,8 @@
    ((string? x) x)
    ((number? x) (number->string x))
    ((symbol? x) (symbol->string x))
-   (else (stringf "~a" x))))
+   ((char? x) (string x))
+   (else
+    (call-with-output-string
+     (lambda (port)
+       (display x port))))))
--- src/euphrates/tilda-a.sld
+++ src/euphrates/tilda-a.sld
@@ -2,18 +2,24 @@
 (define-library
   (euphrates tilda-a)
   (export ~a)
-  (import (only (euphrates stringf) stringf))
+  (import
+    (only (euphrates call-with-output-string)
+          call-with-output-string))
   (import
     (only (scheme base)
           begin
+          char?
           cond
           define
           else
+          lambda
           number->string
           number?
+          string
           string?
           symbol->string
           symbol?))
+  (import (only (scheme write) display))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
