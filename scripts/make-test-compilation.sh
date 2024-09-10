e047c6f2c417df7d0d521d7ad4a7871e9afcff60
Otto Jung <otto.jung@vauplace.com>
2024-09-10 16:02:28 -0700

    improve compilation script

--- scripts/make-test-compilation.sh
+++ scripts/make-test-compilation.sh
@@ -1,5 +1,19 @@
 #! /bin/sh
 
-exec my-fix-imports \
+if ! command -v my-fix-imports 1>/dev/null 2>/dev/null
+then
+	echo "Expect 'my-fix-imports- command to be available." 1>&2
+	exit 1
+fi
+
+#
+# Do not compile the super large, generated files.
+#
+TMPDIR="$(mktemp -d)"
+mv -- test/data/*large* "$TMPDIR"
+
+my-fix-imports \
 	--import-everything \
 	"test/test-compilation.scm"
+
+mv -- "$TMPDIR"/* test/data/
