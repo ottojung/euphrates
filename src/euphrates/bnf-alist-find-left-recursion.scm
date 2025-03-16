e70994fba15ac0146fc0ea4c1ed81893df3e35af
Otto Jung <otto.jung@vauplace.com>
2025-03-15 17:58:08 -0700

    implement bnf-alist:find-left-recursion

--- src/euphrates/bnf-alist-find-left-recursion.scm
+++ src/euphrates/bnf-alist-find-left-recursion.scm
@@ -2,4 +2,37 @@
 ;;;; This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define (bnf-alist:find-left-recursion bnf-alist)
-  'TODO)
+  (define reachability
+    (bnf-alist:compute-reachability bnf-alist))
+
+  (define nonterminals
+    (bnf-alist:nonterminals bnf-alist))
+
+  (define (recursive? nonterminal)
+    (define reachable
+      (hashmap-ref reachability nonterminal))
+
+    (hashset-has? reachable nonterminal))
+
+  (define recursive-nonterminals
+    (filter recursive? nonterminals))
+
+  (define (recursive-path? path)
+    (member (car path) (cdr path)))
+
+  (define (make-recursion-structs nonterminal)
+    (define all-paths
+      (bnf-alist:compute-all-paths-from bnf-alist nonterminal))
+
+    (define cycles
+      (filter recursive-path? all-paths))
+
+    (define (make-single-recursion-struct cycle)
+      (bnf-alist:left-recursion:make nonterminal cycle))
+
+    (map make-single-recursion-struct cycles))
+
+  (define structures
+    (list-map/flatten make-recursion-structs recursive-nonterminals))
+
+  structures)
--- src/euphrates/bnf-alist-find-left-recursion.sld
+++ src/euphrates/bnf-alist-find-left-recursion.sld
@@ -2,7 +2,34 @@
 (define-library
   (euphrates bnf-alist-find-left-recursion)
   (export bnf-alist:find-left-recursion)
-  (import (only (scheme base) begin define quote))
+  (import
+    (only (euphrates bnf-alist-compute-all-paths-from)
+          bnf-alist:compute-all-paths-from))
+  (import
+    (only (euphrates bnf-alist-compute-reachability)
+          bnf-alist:compute-reachability))
+  (import
+    (only (euphrates bnf-alist-left-recursion)
+          bnf-alist:left-recursion:make))
+  (import
+    (only (euphrates bnf-alist-nonterminals)
+          bnf-alist:nonterminals))
+  (import (only (euphrates hashmap) hashmap-ref))
+  (import (only (euphrates hashset) hashset-has?))
+  (import
+    (only (euphrates list-map-flatten)
+          list-map/flatten))
+  (import
+    (only (scheme base)
+          begin
+          car
+          cdr
+          define
+          map
+          member))
+  (cond-expand
+    (guile (import (only (srfi srfi-1) filter)))
+    (else (import (only (srfi 1) filter))))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
